dad0140e7339a15536555323d053b6a1bf64bb36
Ellis Bigelow <ellisbigelow@gmail.com>
2019-04-21 22:23:26 -0700

    Fixes JSONPatch merge issues maps

--- pkg/webhook/admission/kfservice/kfservice_defaulter.go
+++ pkg/webhook/admission/kfservice/kfservice_defaulter.go
@@ -2,9 +2,12 @@ package kfservice
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 
 	kfservingv1alpha1 "github.com/kubeflow/kfserving/pkg/apis/serving/v1alpha1"
+	"github.com/mattbaird/jsonpatch"
+	"k8s.io/api/admission/v1beta1"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -27,7 +30,29 @@ func (defaulter *Defaulter) Handle(ctx context.Context, req admissiontypes.Reque
 		return admission.ErrorResponse(http.StatusBadRequest, err)
 	}
 
-	patch := kfsvc.DeepCopy()
-	patch.Default()
-	return admission.PatchResponse(kfsvc, patch)
+	kfsvc.Default()
+
+	patch, err := json.Marshal(kfsvc)
+	if err != nil {
+		return admission.ErrorResponse(http.StatusInternalServerError, err)
+	}
+
+	return patchResponseFromRaw(req.AdmissionRequest.Object.Raw, patch)
+}
+
+// Use the controller-runtime version of this in 1.13. This is temporarily ported from:
+// https://github.com/kubernetes-sigs/controller-runtime/blob/58a08d8098290a173ef143bd28820f4308916948/pkg/webhook/admission/response.go#L81
+func patchResponseFromRaw(original, current []byte) admissiontypes.Response {
+	patches, err := jsonpatch.CreatePatch(original, current)
+	if err != nil {
+		return admission.ErrorResponse(http.StatusBadRequest, err)
+	}
+
+	return admissiontypes.Response{
+		Patches: patches,
+		Response: &v1beta1.AdmissionResponse{
+			Allowed:   true,
+			PatchType: func() *v1beta1.PatchType { pt := v1beta1.PatchTypeJSONPatch; return &pt }(),
+		},
+	}
 }
