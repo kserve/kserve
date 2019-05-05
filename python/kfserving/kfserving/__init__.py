e4fd4f45b91be819f34d841cf1d47e5799bfec10
Clive Cox <cc@seldon.io>
2019-05-05 15:14:20 +0100

    merge

--- python/kfserving/kfserving/__init__.py
+++ python/kfserving/kfserving/__init__.py
@@ -1,3 +1,4 @@
 from .server import KFServer
 from .model import KFModel
 from .storage import Storage
+
--- python/kfserving/kfserving/server.py
+++ python/kfserving/kfserving/server.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 
 import tornado.ioloop
 import tornado.web
+import tornado.httpserver
 import argparse
 import os
 import logging
@@ -9,13 +10,17 @@ import json
 
 DEFAULT_HTTP_PORT = 8080
 DEFAULT_GRPC_PORT = 8081
-DEFAULT_PROTOCOL = "tensorflow/HTTP"
+
+DEFAULT_PROTOCOL = "tensorflow.http"
+PROTOCOLS = ["tensorflow.http","seldon.http"]
 
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('--http_port', default=DEFAULT_HTTP_PORT,
                     help='The HTTP Port listened to by the model server.')
 parser.add_argument('--grpc_port', default=DEFAULT_GRPC_PORT,
                     help='The GRPC Port listened to by the model server.')
+parser.add_argument('--protocol', default=DEFAULT_PROTOCOL, choices=PROTOCOLS,
+                    help='The protocol served by the model server')
 args, _ = parser.parse_known_args()
 
 KFSERVER_LOGLEVEL = os.environ.get('KFSERVER_LOGLEVEL', 'INFO').upper()
@@ -31,28 +36,33 @@ class KFServer(object):
 
         self._http_server = None
 
+    def createApplication(self):
+        standard_handlers = [
+            # Server Liveness API returns 200 if server is alive.
+            (r"/", LivenessHandler),
+            # Protocol Discovery API that returns the serving protocol supported by this server.
+            (r"/protocol", ProtocolHandler, dict(protocol=self.protocol)),
+            # Prometheus Metrics API that returns metrics for model servers
+            (r"/metrics", MetricsHandler, dict(models=self.registered_models)),
+            # Model Health API returns 200 if model is ready to serve.
+            (r"/models/([a-zA-Z0-9_-]+)",
+             ModelHealthHandler, dict(models=self.registered_models))]
+
+        return tornado.web.Application(standard_handlers + [
+        # Predict API executes executes predict on input tensors
+        (r"/models/([a-zA-Z0-9_-]+)",
+         ModelPredictHandler, dict(models=self.registered_models)),
+        # Optional Custom Predict Verb for Tensorflow compatibility
+        (r"/models/([a-zA-Z0-9_-]+):predict",
+         ModelPredictHandler, dict(models=self.registered_models)),
+        ])
+
     def start(self, models=[]):
         # TODO add a GRPC server
         for model in models:
             self.register_model(model)
 
-        self._http_server = tornado.httpserver.HTTPServer(tornado.web.Application([
-            # Server Liveness API returns 200 if server is alive.
-            (r"/", LivenessHandler),
-            # Protocol Discovery API that returns the serving protocol supported by this server.
-            (r"/protocol", ProtocolHandler),
-            # Prometheus Metrics API that returns metrics for model servers
-            (r"/metrics", MetricsHandler, dict(models=self.registered_models)),
-            # Model Health API returns 200 if model is ready to serve.
-            (r"/models/([a-zA-Z0-9_-]+)",
-             ModelHealthHandler, dict(models=self.registered_models)),
-            # Predict API executes executes predict on input tensors
-            (r"/models/([a-zA-Z0-9_-]+)",
-             ModelPredictHandler, dict(models=self.registered_models)),
-            # Optional Custom Predict Verb for Tensorflow compatibility
-            (r"/models/([a-zA-Z0-9_-]+):predict",
-             ModelPredictHandler, dict(models=self.registered_models)),
-        ]))
+        self._http_server = tornado.httpserver.HTTPServer(self.createApplication())
 
         logging.info("Listening on port %s" % self.http_port)
         self._http_server.bind(self.http_port)
@@ -63,42 +73,6 @@ class KFServer(object):
         if not model.name:
             raise Exception("Failed to register model, model.name must be provided.")
         self.registered_models[model.name] = model
-
-
-class LivenessHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Alive")
-
-
-class ProtocolHandler(tornado.web.RequestHandler):
-    def initialize(self, protocol):
-        self.protocol = protocol
-
-    def get(self):
-        self.write(self.protocol)
-
-
-class MetricsHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Not Implemented")
-
-
-class ModelHealthHandler(tornado.web.RequestHandler):
-    def initialize(self, models):
-        self.models = models
-
-    def get(self, name):
-        if name not in self.models:
-            raise tornado.web.HTTPError(
-                status_code=404,
-                reason="Model with name %s does not exist." % name
-            )
-
-        model = self.models[name]
-        self.write(json.dumps({
-            "name": model.name,
-            "ready": model.ready
-        }))
 
 
 class ModelPredictHandler(tornado.web.RequestHandler):
@@ -138,3 +112,43 @@ class ModelPredictHandler(tornado.web.RequestHandler):
         self.write(str({
             "predictions": outputs
         }))
+
+class LivenessHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Alive")
+
+
+class ProtocolHandler(tornado.web.RequestHandler):
+    def initialize(self, protocol):
+        self.protocol = protocol
+
+    def get(self):
+        self.write(self.protocol)
+
+
+class MetricsHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Not Implemented")
+
+
+class ModelHealthHandler(tornado.web.RequestHandler):
+    def initialize(self, models):
+        self.models = models
+
+    def get(self, name):
+        if name not in self.models:
+            raise tornado.web.HTTPError(
+                status_code=404,
+                reason="Model with name %s does not exist." % name
+            )
+
+        model = self.models[name]
+        self.write(json.dumps({
+            "name": model.name,
+            "ready": model.ready
+        }))
+
+
+if __name__ == "__main__":
+    s = KFServer()
+    s.start()
