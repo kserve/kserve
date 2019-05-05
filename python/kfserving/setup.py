2c8da3323fdaceecca22952be5d4d5047d04930d
Clive Cox <cc@seldon.io>
2019-05-05 15:07:19 +0100

    Update project structure

---
+++ python/kfserving/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='kfserver',
+    version='0.1.0',
+    author_email='ellisbigelow@google.com',
+    license='../../LICENSE.txt',
+    url='https://github.com/kubeflow/kfserving/python/kfserving/kfserving',
+    description='Model Server for arbitrary python ML frameworks.',
+    long_description=open('README.md').read(),
+    python_requires='>3.4',
+    packages=find_packages("kfserving"),
+    install_requires=[
+        "tornado >= 1.4.1",
+        "argparse >= 1.4.0"
+    ],
+)
