dc406a98b71d1ffa7b83269aaf5ef5a2e37f44c1
Tommy Li <Tommy.chaoping.li@ibm.com>
2019-05-14 10:33:17 -0700

    Made python kfserving.storage return uri if local path exists (#92)
    * made storage return uri if path exists and added test cases
    
    * Update Storage with file:// prefix
    
    * update docs regarding relative and absolute local path
    
    * make local path also works without prefix
    
    * add note for relative path without prefix as recommened way

--- python/kfserving/kfserving/storage.py
+++ python/kfserving/kfserving/storage.py
@@ -1,17 +1,18 @@
 import logging
 import tempfile
+import os
 
 _GCS_PREFIX = "gs://"
 _S3_PREFIX = "s3://"
-_LOCAL_PREFIX = "/"
+_LOCAL_PREFIX = "file://"
 
 
 class Storage(object):
     @staticmethod
     def download(uri: str) -> str:
         logging.info("Copying contents of %s to local" % uri)
-        if uri.startswith(_LOCAL_PREFIX):
-            return uri
+        if uri.startswith(_LOCAL_PREFIX) or os.path.exists(uri):
+            return Storage._download_local(uri)
 
         temp_dir = tempfile.mkdtemp()
         if uri.startswith(_GCS_PREFIX):
@@ -19,7 +20,9 @@ class Storage(object):
         elif uri.startswith(_S3_PREFIX):
             Storage._download_s3(uri, temp_dir)
         else:
-            raise Exception("Cannot recognize storage type for " + uri)
+            raise Exception("Cannot recognize storage type for " + uri +
+                            "\n'%s', '%s', and '%s' are the current available storage type." %
+                            (_GCS_PREFIX, _S3_PREFIX, _LOCAL_PREFIX))
 
         logging.info("Successfully copied %s to %s" % (uri, temp_dir))
         return temp_dir
@@ -31,3 +34,10 @@ class Storage(object):
     @staticmethod
     def _download_gcs(uri, temp_dir: str):
         raise NotImplementedError
+
+    @staticmethod
+    def _download_local(uri):
+        local_path = uri.replace(_LOCAL_PREFIX, "", 1)
+        if not os.path.exists(local_path):
+            raise Exception("Local path %s does not exist." % (uri))
+        return local_path
---
+++ python/kfserving/kfserving/test_storage.py
@@ -0,0 +1,22 @@
+import pytest
+import kfserving
+
+
+def test_storage_local_path():
+    abs_path = 'file:///'
+    relative_path = 'file://.'
+    assert kfserving.Storage.download(abs_path) == abs_path.replace("file://", "", 1)
+    assert kfserving.Storage.download(relative_path) == relative_path.replace("file://", "", 1)
+
+
+def test_storage_local_path_exception():
+    not_exist_path = 'file:///some/random/path'
+    with pytest.raises(Exception):
+        kfserving.Storage.download(not_exist_path)
+
+
+def test_no_prefix_local_path():
+    abs_path = '/'
+    relative_path = '.'
+    assert kfserving.Storage.download(abs_path) == abs_path
+    assert kfserving.Storage.download(relative_path) == relative_path
