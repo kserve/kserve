688bad0ebc2e36df1af2f1ce105371ae53832494
Hougang Liu <liuhoug@cn.ibm.com>
2019-05-15 22:35:42 -0500

    Make deploy and deploy-dev rule of Makefile work (#95)

--- image_patch_dev.sh
+++ image_patch_dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-IMG=$(ko resolve -f config/manager/manager.yaml | grep 'image:' | awk '{print $2}')
-if [ -z IMG ]; then exit; fi
+IMG=$(ko resolve -f config/default/manager/manager.yaml | grep 'image:' | awk '{print $2}')
+if [ -z ${IMG} ]; then exit; fi
 cat > config/overlays/development/manager_image_patch.yaml << EOF 
 apiVersion: apps/v1
 kind: StatefulSet 
