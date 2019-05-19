a047136cd9605e2290ee08c113ebd3488ee2c36a
Ellis Bigelow <ellisbigelow@gmail.com>
2019-05-19 10:21:00 -0700

    Adds a mutating admission controller to inject GPU functionality into deployments

--- pkg/webhook/admission/kfservice/kfservice_defaulter.go
+++ pkg/webhook/admission/kfservice/kfservice_defaulter.go
@@ -22,8 +22,7 @@ import (
 	"net/http"
 
 	kfservingv1alpha1 "github.com/kubeflow/kfserving/pkg/apis/serving/v1alpha1"
-	"github.com/mattbaird/jsonpatch"
-	"k8s.io/api/admission/v1beta1"
+	"github.com/kubeflow/kfserving/pkg/webhook/third_party"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -53,22 +52,5 @@ func (defaulter *Defaulter) Handle(ctx context.Context, req admissiontypes.Reque
 		return admission.ErrorResponse(http.StatusInternalServerError, err)
 	}
 
-	return patchResponseFromRaw(req.AdmissionRequest.Object.Raw, patch)
-}
-
-// Use the controller-runtime version of this in 1.13. This is temporarily ported from:
-// https://github.com/kubernetes-sigs/controller-runtime/blob/58a08d8098290a173ef143bd28820f4308916948/pkg/webhook/admission/response.go#L81
-func patchResponseFromRaw(original, current []byte) admissiontypes.Response {
-	patches, err := jsonpatch.CreatePatch(original, current)
-	if err != nil {
-		return admission.ErrorResponse(http.StatusBadRequest, err)
-	}
-
-	return admissiontypes.Response{
-		Patches: patches,
-		Response: &v1beta1.AdmissionResponse{
-			Allowed:   true,
-			PatchType: func() *v1beta1.PatchType { pt := v1beta1.PatchTypeJSONPatch; return &pt }(),
-		},
-	}
+	return third_party.PatchResponseFromRaw(req.AdmissionRequest.Object.Raw, patch)
 }
