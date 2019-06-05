b6238801ad34d3114fd0ba714197818eda763d66
Clive Cox <cc@seldon.io>
2019-06-05 08:05:50 +0100

    lint update

--- pkg/apis/serving/v1alpha1/kfservice_framework.go
+++ pkg/apis/serving/v1alpha1/kfservice_framework.go
@@ -16,9 +16,9 @@ package v1alpha1
 import (
 	"fmt"
 
-	"k8s.io/klog"
 	v1 "k8s.io/api/core/v1"
 	resource "k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/klog"
 )
 
 type FrameworkHandler interface {
