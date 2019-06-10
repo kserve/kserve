b557728f953d9f6fa7302688f04b32c49febb60d
Ellis Bigelow <ellisbigelow@gmail.com>
2019-06-09 23:41:00 -0700

    Catching missed files

---
+++ pkg/reconciler/knative/route_reconciler.go
@@ -0,0 +1,78 @@
+package knative
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/knative/pkg/kmp"
+	knservingv1alpha1 "github.com/knative/serving/pkg/apis/serving/v1alpha1"
+	"github.com/kubeflow/kfserving/pkg/apis/serving/v1alpha1"
+	"github.com/kubeflow/kfserving/pkg/reconciler/knative/resources"
+	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+type RouteReconciler struct {
+	client client.Client
+	scheme *runtime.Scheme
+}
+
+func NewRouteReconciler(client client.Client, scheme *runtime.Scheme) *RouteReconciler {
+	return &RouteReconciler{
+		client: client,
+		scheme: scheme,
+	}
+}
+
+func (r *RouteReconciler) Reconcile(kfsvc *v1alpha1.KFService) error {
+	desiredRoute := resources.NewRouteBuilder().CreateKnativeRoute(kfsvc)
+	if err := controllerutil.SetControllerReference(kfsvc, &desiredRoute, r.scheme); err != nil {
+		return err
+	}
+
+	// Create route if does not exist
+	route := knservingv1alpha1.Route{}
+	err := r.client.Get(context.TODO(), types.NamespacedName{Name: desiredRoute.Name, Namespace: desiredRoute.Namespace}, &route)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Info("Creating Knative Serving route", "namespace", desiredRoute.Namespace, "name", desiredRoute.Name)
+			err = r.client.Create(context.TODO(), &desiredRoute)
+			return err
+		}
+		return err
+	}
+
+	// Return if no differences to reconcile.
+	if routeSemanticEquals(desiredRoute, route) {
+		return nil
+	}
+
+	// Reconcile differences and update
+	diff, err := kmp.SafeDiff(desiredRoute.Spec, route.Spec)
+	if err != nil {
+		return fmt.Errorf("failed to diff route: %v", err)
+	}
+	log.Info("Reconciling route diff (-desired, +observed):", "diff", diff)
+	log.Info("Updating route", "namespace", route.Namespace, "name", route.Name)
+	route.Spec = desiredRoute.Spec
+	route.ObjectMeta.Labels = desiredRoute.ObjectMeta.Labels
+	route.ObjectMeta.Annotations = desiredRoute.ObjectMeta.Annotations
+	err = r.client.Update(context.TODO(), &route)
+	if err != nil {
+		return err
+	}
+
+	// Update parent object's status
+	kfsvc.Status.PropagateRouteStatus(&route.Status)
+	return nil
+}
+
+func routeSemanticEquals(desiredRoute, route knservingv1alpha1.Route) bool {
+	return equality.Semantic.DeepEqual(desiredRoute.Spec, route.Spec) &&
+		equality.Semantic.DeepEqual(desiredRoute.ObjectMeta.Labels, route.ObjectMeta.Labels) &&
+		equality.Semantic.DeepEqual(desiredRoute.ObjectMeta.Annotations, route.ObjectMeta.Annotations)
+}
