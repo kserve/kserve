e1b7942ed0a4d8684e985a44e57713be4e50ac8c
Janice Chan <janicechan@google.com>
2019-06-13 16:05:26 -0700

    Start visiting nodes

--- tools/tf2openapi/tfmodel/openapivisitor.go
+++ tools/tf2openapi/tfmodel/openapivisitor.go
@@ -1,12 +1,82 @@
 package tfmodel
 
-import "fmt"
+import (
+	"github.com/getkin/kin-openapi/openapi3"
+	"log"
+)
 
 /** Implements the Visitor interface **/
 type OpenAPIVisitor struct {
+	savedModelRep *openapi3.Schema
+	metaGraphRep  *openapi3.Schema
+	sigDefRep     *openapi3.Schema
+	tensorRep     *openapi3.Schema
 }
 
-func (w *OpenAPIVisitor) VisitSavedModel(node *TFSavedModel)     { fmt.Println("Saved Model") }
-func (w *OpenAPIVisitor) VisitMetaGraph(node *TFMetaGraph)       { fmt.Println("Meta Graph") }
-func (w *OpenAPIVisitor) VisitSignatureDef(node *TFSignatureDef) { fmt.Println("Sig Def") }
-func (w *OpenAPIVisitor) VisitTensor(node *TFTensor)             { fmt.Println("Tensor") }
+func OpenAPIPrint(v OpenAPIVisitor) {
+
+	swagger := &openapi3.Swagger{
+		//Servers: openapi3.Servers{
+		//	{
+		//		URL: "http://example.com/api/",
+		//	},
+		//},
+		Paths: openapi3.Paths{
+			"models/${MODEL_NAME}[/versions/${MODEL_VERSION}]:predict": &openapi3.PathItem{
+				Post: &openapi3.Operation{
+					Parameters: openapi3.Parameters{
+						{
+							Value: &openapi3.Parameter{
+								In:   "query",
+								Name: "contentArg2",
+								Content: openapi3.Content{
+									"application/something_funny":
+									openapi3.NewMediaType().WithSchema(v.savedModelRep),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		Info: openapi3.Info{
+			Title: "TFServing Predict Request API",
+			Version: "3.0",
+		},
+	}
+	jsonSerialized, _ := (*swagger).MarshalJSON()
+
+	log.Println("************************")
+	log.Println(string(jsonSerialized))
+}
+
+func (w *OpenAPIVisitor) VisitSavedModel(node *TFSavedModel) {
+	w.savedModelRep = openapi3.NewOneOfSchema()
+}
+
+func (w *OpenAPIVisitor) VisitMetaGraph(node *TFMetaGraph) {
+	w.metaGraphRep = openapi3.NewOneOfSchema()
+	w.savedModelRep.OneOf = append(w.savedModelRep.OneOf, w.metaGraphRep.NewRef())
+	log.Println("Metagraph")
+	log.Println(w.metaGraphRep)
+}
+
+func (w *OpenAPIVisitor) VisitSignatureDef(node *TFSignatureDef) {
+	log.Println("SigDef")
+	multiTensorSchema := *openapi3.NewObjectSchema().WithProperties(make(map[string]*openapi3.Schema))
+
+	w.sigDefRep = openapi3.NewArraySchema().WithItems(&multiTensorSchema)
+	// TODO may change for columnar format
+	rowSchemaRef := openapi3.NewObjectSchema().WithProperty("instances", w.sigDefRep)
+
+	w.metaGraphRep.OneOf = append(w.metaGraphRep.OneOf, rowSchemaRef.NewRef())
+	log.Println(w.metaGraphRep)
+}
+
+func (w *OpenAPIVisitor) VisitTensor(node *TFTensor) {
+	log.Println("Tensor")
+	tensorConstraints := openapi3.NewStringSchema().NewRef()
+	w.sigDefRep.Items.Value.Properties[node.Key] = tensorConstraints
+
+	log.Println("metagraphrep", w.metaGraphRep)
+}
