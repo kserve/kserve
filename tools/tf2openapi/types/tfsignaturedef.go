b88ec58eb6dd1a7c782fc628b31948e405adbe5f
Janice Chan <jc2729@cornell.edu>
2019-06-19 11:24:32 -0700

    Add types (#164)
    * Add types
    
    * Move constants to avoid namespace clash
    
    * Create top level generator
    
    * Fix formatting
    
    * Update README.md
    
    * Distribute constants

---
+++ tools/tf2openapi/types/tfsignaturedef.go
@@ -0,0 +1,29 @@
+package types
+
+/**
+TFSignatureDef defines the signature of supported computations in the TensorFlow graph, including their inputs and
+outputs. It is the internal model representation for the SignatureDef defined in the TensorFlow repository
+[tensorflow/core/protobuf/meta_graph.proto]
+*/
+import (
+	"github.com/getkin/kin-openapi/openapi3"
+	pb "github.com/tensorflow/tensorflow/tensorflow/go/core/protobuf"
+)
+
+type TFSignatureDef struct {
+	Name    string
+	Inputs  [] TFTensor
+	Outputs [] TFTensor
+}
+
+func NewTFSignatureDef(key string, inputs map[string]*pb.TensorInfo, outputs map[string]*pb.TensorInfo) TFSignatureDef {
+	return TFSignatureDef{}
+}
+
+func extractTensors(tensors map[string]*pb.TensorInfo) []TFTensor {
+	return []TFTensor{}
+}
+
+func (t *TFSignatureDef) Schema() *openapi3.Schema {
+	return &openapi3.Schema{}
+}
