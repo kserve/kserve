8f578b77f5f61cbe07ae744f1c75cb122130869a
Janice Chan <janicechan@google.com>
2019-06-19 13:32:09 -0700

    Add pb conversion for scalars

--- tools/tf2openapi/types/tftensor.go
+++ tools/tf2openapi/types/tftensor.go
@@ -7,8 +7,8 @@ tensor and not an actual tensor).
  */
 
 import (
-	fw "github.com/tensorflow/tensorflow/tensorflow/go/core/framework"
 	"github.com/getkin/kin-openapi/openapi3"
+	fw "github.com/tensorflow/tensorflow/tensorflow/go/core/framework"
 	pb "github.com/tensorflow/tensorflow/tensorflow/go/core/protobuf"
 	"strings"
 )
@@ -65,7 +65,6 @@ func NewTFTensor(key string, tensor pb.TensorInfo) TFTensor {
 		return TFTensor{
 			Key:   key,
 			DType: NewTFDType(tensor.Dtype.String(), key),
-			// TODO add what happens to scalars
 			Shape: tfShape,
 			Rank:  int64(len(tfShape)),
 		}
@@ -74,7 +73,8 @@ func NewTFTensor(key string, tensor pb.TensorInfo) TFTensor {
 
 func NewTFShape(dimensions []*fw.TensorShapeProto_Dim) TFShape {
 	tfShape := TFShape{}
-	for _, d := range dimensions {
+	// There will always be -1 in dimensions[0] for batch size, so ignore
+	for _, d := range dimensions[1:] {
 		tfShape = append(tfShape, d.Size)
 	}
 	return tfShape
