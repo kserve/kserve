679f3613bb607167c372df92a9405e6c61d27398
Janice Chan <jc2729@cornell.edu>
2019-06-25 17:35:15 -0700

    Add types for TF2OpenAPI (#186)
    * Revert "Revert "Add types (#164)" (#169)"
    
    This reverts commit ef9e8b60e26c4dff660ef314a7d1ad010e45a3bb.
    
    * Change imports to use generated dir

---
+++ tools/tf2openapi/types/tfmetagraph.go
@@ -0,0 +1,29 @@
+package types
+
+/**
+TFMetaGraph contains meta information about the TensorFlow graph, i.e. the signature definitions of the graph.
+It is the internal model representation for the MetaGraph defined in the TensorFlow repository
+[tensorflow/core/protobuf/meta_graph.proto]
+*/
+import (
+	"github.com/getkin/kin-openapi/openapi3"
+	pb "github.com/kubeflow/kfserving/tools/tf2openapi/generated/protobuf"
+)
+
+const PredictReqSigDefMethod string = "tensorflow/serving/predict"
+
+type TFMetaGraph struct {
+	SignatureDefs [] TFSignatureDef
+}
+
+func NewTFMetaGraph(metaGraph pb.MetaGraphDef) TFMetaGraph {
+	return TFMetaGraph{}
+}
+
+func extractSigDefs(sigDefs map[string]*pb.SignatureDef) []TFSignatureDef {
+	return []TFSignatureDef{}
+}
+
+func (t *TFMetaGraph) Schema() *openapi3.Schema {
+	return &openapi3.Schema{}
+}
