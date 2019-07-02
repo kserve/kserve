d624f0de4be320e8d2646f0072c768568db48ce6
Ellis Bigelow <ellisbigelow@gmail.com>
2019-07-02 12:19:56 -0700

    Attach release tag to generate-install

--- hack/generate-install.sh
+++ hack/generate-install.sh
@@ -33,4 +33,4 @@ INSTALL_DIR=./install/$TAG
 INSTALL_PATH=$INSTALL_DIR/kfserving.yaml
 
 mkdir -p $INSTALL_DIR
-kustomize build config/default > $INSTALL_PATH
+kustomize build config/default | sed s/:latest/:$TAG/ > $INSTALL_PATH
