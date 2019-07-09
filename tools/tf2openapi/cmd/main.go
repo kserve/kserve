9b91e1a6030ebf75d9ee33946405d2e42dd803ea
Janice Chan <jc2729@cornell.edu>
2019-07-09 14:50:24 -0700

    Tf2OpenAPI: Add CLI documentation (#220)
    * Add CLI documentation
    
    * Add cobra as explicit dependency
    
    * update README with correct command

--- tools/tf2openapi/cmd/main.go
+++ tools/tf2openapi/cmd/main.go
@@ -1,43 +1,61 @@
 package main
 
 import (
-	"flag"
 	"github.com/golang/protobuf/proto"
-	"github.com/kubeflow/kfserving/tools/tf2openapi/generator"
 	pb "github.com/kubeflow/kfserving/tools/tf2openapi/generated/protobuf"
+	"github.com/kubeflow/kfserving/tools/tf2openapi/generator"
+	"github.com/spf13/cobra"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
 var (
-	model     = flag.String("model", "", "Absolute path of SavedModel file")
-	outFile   = flag.String("output-file", "", "Absolute path of file to write OpenAPI spec to")
-	sigDefKey = flag.String("signature-def", "", "Serving Signature Def Key ")
+	modelBasePath string
+	modelName     string
+	modelVersion  string
+	sigDefKey     string
+	outFile       string
 )
 
 /** Convert SavedModel to OpenAPI. Note that the SavedModel must have at least one signature defined**/
 func main() {
-	flag.Parse()
-	if *model == "" {
-		log.Fatalln("Please specify the absolute path of the SavedModel file")
+	rootCmd := &cobra.Command{
+		Use:   "tf2openapi",
+		Short: "tf2openapi is an OpenAPI generator for TensorFlow SavedModels",
+		Long: `This tool takes TensorFlow SavedModel files as inputs and generates OpenAPI 3.0 
+			specifications for HTTP prediction requests. The SavedModel files must 
+			contain signature definitions (SignatureDefs) for models.`,
+		Run: viewAPI,
 	}
 
-	modelPb, err := ioutil.ReadFile(*model)
+	rootCmd.Flags().StringVarP(&modelBasePath, "model_base_path", "m", "", "Absolute path of SavedModel file")
+	rootCmd.MarkFlagRequired("model_base_path")
+	rootCmd.Flags().StringVarP(&modelName, "name", "n", "model", "Name of model")
+	rootCmd.Flags().StringVarP(&modelVersion, "version", "v", "1", "Model version")
+	rootCmd.Flags().StringVarP(&outFile, "output_file", "o", "", "Absolute path of file to write OpenAPI spec to")
+	rootCmd.Flags().StringVarP(&sigDefKey, "signature_def", "s", "", "Serving Signature Def Key")
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func viewAPI(cmd *cobra.Command, args []string) {
+	modelPb, err := ioutil.ReadFile(modelBasePath)
 	if err != nil {
-		log.Fatalf("Error reading file %s \n%s", *model, err.Error())
+		log.Fatalf("Error reading file %s \n%s", modelBasePath, err.Error())
 	}
 
 	/** Convert Go struct to inner model */
 	model := UnmarshalSavedModelPb(modelPb)
 
 	/** Schema generation example **/
-	spec, err := generator.GenerateOpenAPI(model, *sigDefKey)
+	spec, err := generator.GenerateOpenAPI(model, sigDefKey)
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-	if *outFile != "" {
-		f, err := os.Create(*outFile)
+	if outFile != "" {
+		f, err := os.Create(outFile)
 		if err != nil {
 			panic(err)
 		}
