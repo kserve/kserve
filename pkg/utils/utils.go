06bae5ddd37e50f6a65943c602b2d945786289ef
Janice Chan <janicechan@google.com>
2019-07-18 15:01:26 -0700

    Organize utils types into separate file

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -50,11 +50,3 @@ func Includes(slice []string, value string) bool {
 	}
 	return false
 }
-
-func Bool(b bool) *bool {
-	return &b
-}
-
-func UInt64(u uint64) *uint64 {
-	return &u
-}
--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -118,21 +118,3 @@ func TestContainsUtil(t *testing.T) {
 		}
 	}
 }
-
-func TestBool(t *testing.T) {
-	input := true
-	expected := &input
-	result := Bool(input)
-	if diff := cmp.Diff(expected, result); diff != "" {
-		t.Errorf("Test %q unexpected result (-want +got): %v", t.Name(), diff)
-	}
-}
-
-func TestUInt64(t *testing.T) {
-	input := uint64(63)
-	expected := &input
-	result := UInt64(input)
-	if diff := cmp.Diff(expected, result); diff != "" {
-		t.Errorf("Test %q unexpected result (-want +got): %v", t.Name(), diff)
-	}
-}
