f998d50fb779c2452374fdeaff50502b8e51f7be
Janice Chan <janicechan@google.com>
2019-07-19 16:40:16 -0700

    Fix typo in cloudbuild command

--- hack/generate-cloudbuild-triggers.sh
+++ hack/generate-cloudbuild-triggers.sh
@@ -4,6 +4,6 @@ CONFIGS=$(find $DIR/../release | grep yaml)
 
 # Not currently idempotent! Use with care. Will be resolve in July 2019
 for CONFIG in $CONFIGS; do
-    gcloud alpha builds triggers create github --project kfserving --trigger_config $CONFIG
+    gcloud alpha builds triggers create github --project kfserving --trigger-config $CONFIG
 done
 
