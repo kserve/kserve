042e859c5d4fe979ffd33e79a8de5b662ebc6b00
Dan Sun <dsun20@bloomberg.net>
2019-08-21 22:22:31 -0400

    Set default resource limits for KFService (#293)
    * Default cpu/memory limits
    
    * Add resource limit tests
    
    * Use same variable for requests/limits
    
    * Fix limit tests

--- pkg/apis/serving/v1alpha2/framework.go
+++ pkg/apis/serving/v1alpha2/framework.go
@@ -37,8 +37,8 @@ const (
 )
 
 var (
-	DefaultMemoryRequests = resource.MustParse("2Gi")
-	DefaultCPURequests    = resource.MustParse("1")
+	DefaultMemory = resource.MustParse("2Gi")
+	DefaultCPU    = resource.MustParse("1")
 )
 
 // Returns a URI to the model. This URI is passed to the model-initializer via the ModelInitializerSourceUriInternalAnnotationKey
@@ -81,10 +81,21 @@ func setResourceRequirementDefaults(requirements *v1.ResourceRequirements) {
 	}
 
 	if _, ok := requirements.Requests[v1.ResourceCPU]; !ok {
-		requirements.Requests[v1.ResourceCPU] = DefaultCPURequests
+		requirements.Requests[v1.ResourceCPU] = DefaultCPU
 	}
 	if _, ok := requirements.Requests[v1.ResourceMemory]; !ok {
-		requirements.Requests[v1.ResourceMemory] = DefaultMemoryRequests
+		requirements.Requests[v1.ResourceMemory] = DefaultMemory
+	}
+
+	if requirements.Limits == nil {
+		requirements.Limits = v1.ResourceList{}
+	}
+
+	if _, ok := requirements.Limits[v1.ResourceCPU]; !ok {
+		requirements.Limits[v1.ResourceCPU] = DefaultCPU
+	}
+	if _, ok := requirements.Limits[v1.ResourceMemory]; !ok {
+		requirements.Limits[v1.ResourceMemory] = DefaultMemory
 	}
 }
 
