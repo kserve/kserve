f0c3956f0e297394680eb2f16b52d01da5459646
Dan Sun <dsun20@bloomberg.net>
2019-09-08 07:24:31 -0400

    fix lint

--- python/kfserving/kfserving/transformer.py
+++ python/kfserving/kfserving/transformer.py
@@ -1,11 +1,24 @@
+# Copyright 2019 kubeflow.org.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import logging
 from typing import List
 import requests
 import numpy as np
-
-from kfserving.server import Protocol
-from kfserving.server import KFModel
-from kfserving.server import KFSERVER_LOGLEVEL
+from .server import Protocol
+from .server import KFModel
+from .server import KFSERVER_LOGLEVEL
 import kfserving.protocols.seldon_http as seldon
 from kfserving.protocols.seldon_http import SeldonRequestHandler
 
@@ -19,17 +32,19 @@ class Transformer(KFModel):
         super().__init__(name)
         self.predict_url = predict_url
         self.protocol = protocol
+        self.ready = False
 
     def load(self):
         if not self.ready:
-            logging.info("Loading transformer")
             self.ready = True
 
+    # subclass of Transformer should implement preprocess
     def preprocess(self, inputs: List) -> List:
-        return inputs
+        raise NotImplementedError
 
+    # subclass of Transformer should implment postprocess
     def postprocess(self, inputs: List) -> List:
-        return inputs
+        raise NotImplementedError
 
     def predict(self, inputs: List) -> List:
         if self.protocol == Protocol.seldon_http:
@@ -40,7 +55,8 @@ class Transformer(KFModel):
                 response_list = rh.extract_request()
                 return response_list
             else:
-                raise Exception("Failed to get response from model return_code:%d" % response_raw.status_code)
+                raise Exception("Failed to get response from model return_code:%d" %
+                                response_raw.status_code)
         elif self.protocol == Protocol.tensorflow_http:
             payload = {"instances": inputs}
             logging.info(payload.items())
@@ -50,7 +66,8 @@ class Transformer(KFModel):
                 logging.info(response_raw.json())
                 return response_raw.json()["predictions"]
             else:
-                raise Exception("Failed to get response from model return_code:%d" % response_raw.status_code)
+                raise Exception("Failed to get response from model return_code:%d" %
+                                response_raw.status_code)
         else:
             raise NotImplementedError
 
