9d95bba7e6306a2c520f2b645c08a3b31cdf2d85
Rakesh Kelkar <rakelkar@microsoft.com>
2019-09-16 22:02:32 -0700

    use knative istio instead

--- vendor/knative.dev/pkg/apis/istio/register.go
+++ vendor/knative.dev/pkg/apis/istio/register.go
@@ -1,10 +1,12 @@
 /*
-Portions Copyright 2017 The Kubernetes Authors.
-Portions Copyright 2018 Aspen Mesh Authors.
+Copyright 2018 The Knative Authors
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -12,8 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +k8s:deepcopy-gen=package
+package istio
 
-// Package v1alpha3 is the v1alpha3 version of the networking.istio.io API.
-// +groupName=networking.istio.io
-package v1alpha3
+const (
+	GroupName = "networking.istio.io"
+)
--- vendor/knative.dev/pkg/apis/istio/v1alpha3/register.go
+++ vendor/knative.dev/pkg/apis/istio/v1alpha3/register.go
@@ -1,10 +1,12 @@
 /*
-Portions Copyright 2017 The Kubernetes Authors.
-Portions Copyright 2018 Aspen Mesh Authors.
+Copyright 2018 The Knative Authors
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,20 +20,18 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-
-	"github.com/aspenmesh/istio-client-go/pkg/apis/networking"
+	"knative.dev/pkg/apis/istio"
 )
 
 // SchemeGroupVersion is group version used to register these objects
-var SchemeGroupVersion = schema.GroupVersion{Group: networkingcontroller.GroupName, Version: "v1alpha3"}
+var SchemeGroupVersion = schema.GroupVersion{Group: istio.GroupName, Version: "v1alpha3"}
 
 // Kind takes an unqualified kind and returns back a Group qualified GroupKind
 func Kind(kind string) schema.GroupKind {
 	return SchemeGroupVersion.WithKind(kind).GroupKind()
 }
 
-// Resource takes an unqualified resource and returns a Group qualified
-// GroupResource
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
 func Resource(resource string) schema.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
 }
@@ -45,15 +45,11 @@ var (
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&VirtualService{},
-		&VirtualServiceList{},
-		&DestinationRule{},
-		&DestinationRuleList{},
 		&Gateway{},
+		&DestinationRule{},
+		&VirtualServiceList{},
 		&GatewayList{},
-		&ServiceEntry{},
-		&ServiceEntryList{},
-		&EnvoyFilter{},
-		&EnvoyFilterList{},
+		&DestinationRuleList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
