8fd0c20b2d461abfbdc70b5220fcf629f733d1b5
cliveseldon <cc@seldon.io>
2019-09-19 17:25:05 +0100

    Fix lint errors

--- python/alibiexplainer/alibiexplainer/__main__.py
+++ python/alibiexplainer/alibiexplainer/__main__.py
@@ -31,7 +31,7 @@ ENV_STORAGE_URI = "STORAGE_URI"
 
 
 class GroupedAction(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
+    def __call__(self, theparser, namespace, values, option_string=None):
         group, dest = self.dest.split('.', 2)
         groupspace = getattr(namespace, group, argparse.Namespace())
         setattr(groupspace, dest, values)
