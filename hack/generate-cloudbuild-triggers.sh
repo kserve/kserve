1d8a3cc0fda31c077305cb5c0a449ef8edadf512
Ellis Bigelow <ellisbigelow@google.com>
2019-09-23 13:12:33 -0700

    Adds build triggers for latest

--- hack/generate-cloudbuild-triggers.sh
+++ hack/generate-cloudbuild-triggers.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CONFIGS=$(find $DIR/../release | grep yaml)
+PROJECT=kfserving
 
-# Not currently idempotent! Use with care. Will be resolve in July 2019
+for i in $(gcloud alpha builds triggers list --project kfserving | grep ^id | cut -d ':' -f 2); do gcloud alpha builds triggers delete $i --project $PROJECT --quiet; done
+
 for CONFIG in $CONFIGS; do
-    gcloud alpha builds triggers create github --project kfserving --trigger-config $CONFIG
+    gcloud alpha builds triggers create github --project $PROJECT --trigger-config $CONFIG
 done
-
