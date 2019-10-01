147237e69ab2970b37af3aee5822075a991fc4af
Gabriel Wen <gabrielwen@google.com>
2019-10-01 16:53:17 +0800

    add optional param

--- python/kfserving/kfserving/api/kf_serving_client.py
+++ python/kfserving/kfserving/api/kf_serving_client.py
@@ -27,15 +27,16 @@ class KFServingClient(object):
     '''KFServing Client Apis.'''
 
     def __init__(self, config_file=None, context=None,
-                 client_configuration=None, persist_config=True):
-        if utils.is_running_in_k8s():
-            config.load_incluster_config()
-        else:
+                 client_configuration=None, persist_config=True,
+                 force_kubeconfig=False):
+        if force_kubeconfig or utils.is_running_in_k8s() == False:
             config.load_kube_config(
                 config_file=config_file,
                 context=context,
                 client_configuration=client_configuration,
                 persist_config=persist_config)
+        else:
+            config.load_incluster_config()
 
         self.api_instance = client.CustomObjectsApi()
 
