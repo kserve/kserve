1de8aa365b2600d39adad67e91d451d0f580ad96
Gang Pu <pugangxa@cn.ibm.com>
2019-10-25 13:47:52 +0800

    Refactor and add unit test

--- pkg/apis/serving/v1alpha2/predictor.go
+++ pkg/apis/serving/v1alpha2/predictor.go
@@ -15,16 +15,11 @@ package v1alpha2
 
 import (
 	"fmt"
-	"regexp"
-	"strings"
 
 	"github.com/kubeflow/kfserving/pkg/constants"
 	v1 "k8s.io/api/core/v1"
 	resource "k8s.io/apimachinery/pkg/api/resource"
-	field "k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/klog"
-	core "k8s.io/kubernetes/pkg/apis/core"
-	validation "k8s.io/kubernetes/pkg/apis/core/validation"
 )
 
 type Predictor interface {
@@ -74,71 +69,19 @@ func (p *PredictorSpec) Validate(config *InferenceServicesConfig) error {
 	if err != nil {
 		return err
 	}
-	if err := predictor.Validate(config); err != nil {
-		return err
+	errs := []error{
+		predictor.Validate(config),
+		validateStorageURI(p.GetStorageUri()),
+		validateReplicas(p.MinReplicas, p.MaxReplicas),
+		validateResourceRequirements(predictor.GetResourceRequirements()),
 	}
-	if err := validateStorageURI(p.GetStorageUri()); err != nil {
-		return err
-	}
-	if err := validateReplicas(p.MinReplicas, p.MaxReplicas); err != nil {
-		return err
-	}
-	if err := validateResourceRequirements(predictor.GetResourceRequirements()); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func transferToCoreResourceRequirements(rr *v1.ResourceRequirements) *core.ResourceRequirements {
-	resourceRequirements := &core.ResourceRequirements{
-		Limits:   make(core.ResourceList),
-		Requests: make(core.ResourceList),
-	}
-
-	for k, v := range rr.Requests {
-		resourceName := core.ResourceName(string(k))
-		resourceRequirements.Requests[resourceName] = v
-	}
-	for k, v := range rr.Limits {
-		resourceName := core.ResourceName(string(k))
-		resourceRequirements.Limits[resourceName] = v
-	}
-
-	return resourceRequirements
-}
-
-// validate the ResourceRequirements of the model.
-func validateResourceRequirements(rr *v1.ResourceRequirements) error {
-	if errs := validation.ValidateResourceRequirements(transferToCoreResourceRequirements(rr), field.NewPath("resources")); len(errs) != 0 {
-		return fmt.Errorf("Unexpected error: %v", errs)
-	}
-	return nil
-}
-
-func validateStorageURI(storageURI string) error {
-	if storageURI == "" {
-		return nil
-	}
-
-	// local path (not some protocol?)
-	if !regexp.MustCompile("\\w+?://").MatchString(storageURI) {
-		return nil
-	}
-
-	// one of the prefixes we know?
-	for _, prefix := range SupportedStorageURIPrefixList {
-		if strings.HasPrefix(storageURI, prefix) {
-			return nil
+	for _, err := range errs {
+		if err != nil {
+			return err
 		}
 	}
 
-	azureURIMatcher := regexp.MustCompile(AzureBlobURIRegEx)
-	if parts := azureURIMatcher.FindStringSubmatch(storageURI); parts != nil {
-		return nil
-	}
-
-	return fmt.Errorf(UnsupportedStorageURIFormatError, strings.Join(SupportedStorageURIPrefixList, ", "), storageURI)
+	return nil
 }
 
 func validateReplicas(minReplicas int, maxReplicas int) error {
@@ -152,30 +95,6 @@ func validateReplicas(minReplicas int, maxReplicas int) error {
 		return fmt.Errorf(MinReplicasShouldBeLessThanMaxError)
 	}
 	return nil
-}
-
-func setResourceRequirementDefaults(requirements *v1.ResourceRequirements) {
-	if requirements.Requests == nil {
-		requirements.Requests = v1.ResourceList{}
-	}
-
-	if _, ok := requirements.Requests[v1.ResourceCPU]; !ok {
-		requirements.Requests[v1.ResourceCPU] = DefaultCPU
-	}
-	if _, ok := requirements.Requests[v1.ResourceMemory]; !ok {
-		requirements.Requests[v1.ResourceMemory] = DefaultMemory
-	}
-
-	if requirements.Limits == nil {
-		requirements.Limits = v1.ResourceList{}
-	}
-
-	if _, ok := requirements.Limits[v1.ResourceCPU]; !ok {
-		requirements.Limits[v1.ResourceCPU] = DefaultCPU
-	}
-	if _, ok := requirements.Limits[v1.ResourceMemory]; !ok {
-		requirements.Limits[v1.ResourceMemory] = DefaultMemory
-	}
 }
 
 func isGPUEnabled(requirements v1.ResourceRequirements) bool {
---
+++ pkg/apis/serving/v1alpha2/utils.go
@@ -0,0 +1,87 @@
+package v1alpha2
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/kubernetes/pkg/apis/core"
+	"k8s.io/kubernetes/pkg/apis/core/validation"
+)
+
+func setResourceRequirementDefaults(requirements *v1.ResourceRequirements) {
+	if requirements.Requests == nil {
+		requirements.Requests = v1.ResourceList{}
+	}
+
+	if _, ok := requirements.Requests[v1.ResourceCPU]; !ok {
+		requirements.Requests[v1.ResourceCPU] = DefaultCPU
+	}
+	if _, ok := requirements.Requests[v1.ResourceMemory]; !ok {
+		requirements.Requests[v1.ResourceMemory] = DefaultMemory
+	}
+
+	if requirements.Limits == nil {
+		requirements.Limits = v1.ResourceList{}
+	}
+
+	if _, ok := requirements.Limits[v1.ResourceCPU]; !ok {
+		requirements.Limits[v1.ResourceCPU] = DefaultCPU
+	}
+	if _, ok := requirements.Limits[v1.ResourceMemory]; !ok {
+		requirements.Limits[v1.ResourceMemory] = DefaultMemory
+	}
+}
+
+func toCoreResourceRequirements(rr *v1.ResourceRequirements) *core.ResourceRequirements {
+	resourceRequirements := &core.ResourceRequirements{
+		Limits:   make(core.ResourceList),
+		Requests: make(core.ResourceList),
+	}
+
+	for k, v := range rr.Requests {
+		resourceName := core.ResourceName(string(k))
+		resourceRequirements.Requests[resourceName] = v
+	}
+	for k, v := range rr.Limits {
+		resourceName := core.ResourceName(string(k))
+		resourceRequirements.Limits[resourceName] = v
+	}
+
+	return resourceRequirements
+}
+
+// validate the ResourceRequirements of the model.
+func validateResourceRequirements(rr *v1.ResourceRequirements) error {
+	if errs := validation.ValidateResourceRequirements(toCoreResourceRequirements(rr), field.NewPath("resources")); len(errs) != 0 {
+		return fmt.Errorf("Unexpected error: %v", errs)
+	}
+	return nil
+}
+
+func validateStorageURI(storageURI string) error {
+	if storageURI == "" {
+		return nil
+	}
+
+	// local path (not some protocol?)
+	if !regexp.MustCompile("\\w+?://").MatchString(storageURI) {
+		return nil
+	}
+
+	// one of the prefixes we know?
+	for _, prefix := range SupportedStorageURIPrefixList {
+		if strings.HasPrefix(storageURI, prefix) {
+			return nil
+		}
+	}
+
+	azureURIMatcher := regexp.MustCompile(AzureBlobURIRegEx)
+	if parts := azureURIMatcher.FindStringSubmatch(storageURI); parts != nil {
+		return nil
+	}
+
+	return fmt.Errorf(UnsupportedStorageURIFormatError, strings.Join(SupportedStorageURIPrefixList, ", "), storageURI)
+}
