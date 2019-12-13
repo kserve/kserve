aa828bc10a39e2e8c39053905a1cb70bb59deb30
Alejandro Saucedo <axsauze@gmail.com>
2019-12-13 09:32:54 +0000

    Added default amazon endpoint string (#589)

--- python/kfserving/kfserving/storage.py
+++ python/kfserving/kfserving/storage.py
@@ -209,7 +209,7 @@ The path or model %s does not exist." % (uri))
     @staticmethod
     def _create_minio_client():
         # Remove possible http scheme for Minio
-        url = urlparse(os.getenv("AWS_ENDPOINT_URL", ""))
+        url = urlparse(os.getenv("AWS_ENDPOINT_URL", "s3.amazonaws.com"))
         use_ssl = url.scheme == 'https' if url.scheme else bool(os.getenv("S3_USE_HTTPS", "true"))
         return Minio(url.netloc,
                      access_key=os.getenv("AWS_ACCESS_KEY_ID", ""),
