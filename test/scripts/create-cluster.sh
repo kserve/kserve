9943c4c442970b7a48e68ddde6de9e6be4074996
Jin Chi He <hejinchi@cn.ibm.com>
2020-01-08 13:52:57 +0800

    Debug CI problem

--- test/scripts/create-cluster.sh
+++ test/scripts/create-cluster.sh
@@ -31,7 +31,7 @@ gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS
 echo "Creating cluster ${CLUSTER_NAME} ... "
 gcloud --project ${PROJECT} beta container clusters create ${CLUSTER_NAME} \
     --addons=HorizontalPodAutoscaling,HttpLoadBalancing \
-    --machine-type=n1-standard-4 \
+    --machine-type=n1-standard-8 \
     --cluster-version 1.14 --zone ${ZONE} \
     --enable-stackdriver-kubernetes --enable-ip-alias \
     --enable-autoscaling --min-nodes=3 --max-nodes=10 \
