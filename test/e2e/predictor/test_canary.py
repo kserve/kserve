4cfcc53061d56183308f6aabc0eee3205e68d383
Jane Man <janeman@us.ibm.com>
2020-02-06 11:29:25 -0800

    Added e2e test for canary rollout (#658)
    * Added e2e test for canary rollout
    
    * remove config_file pt to default, add wait_for_isvc_ready
    
    * Revert "remove config_file pt to default, add wait_for_isvc_ready"
    
    This reverts commit 755a8960a939c14ae8f80de57d034d3ef236feb2.
    
    Reverts commit to restore test_tensorflow.py and constants.go
    
    * address review feedback: remove default config_file, add wait_for_isvc_ready, restore test_tensorflow.py, constants.go
    
    * added config_file
    
    * verify promote is successful

---
+++ test/e2e/predictor/test_canary.py
@@ -0,0 +1,73 @@
+# Copyright 2019 kubeflow.org.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+from kubernetes import client
+
+from kfserving import KFServingClient
+from kfserving import constants
+from kfserving import V1alpha2EndpointSpec
+from kfserving import V1alpha2PredictorSpec
+from kfserving import V1alpha2TensorflowSpec
+from kfserving import V1alpha2InferenceServiceSpec
+from kfserving import V1alpha2InferenceService
+from kubernetes.client import V1ResourceRequirements
+
+from ..common.utils import predict
+from ..common.utils import KFSERVING_TEST_NAMESPACE
+
+
+# Setting config_file is required since SDK is running in a different cluster than KFServing
+KFServing = KFServingClient(config_file="~/.kube/config")
+
+
+def test_canary_rollout():
+    service_name = 'isvc-canary'
+    default_endpoint_spec = V1alpha2EndpointSpec(
+        predictor=V1alpha2PredictorSpec(
+            min_replicas=1,
+            tensorflow=V1alpha2TensorflowSpec(
+                storage_uri='gs://kfserving-samples/models/tensorflow/flowers',
+                resources=V1ResourceRequirements(
+                    requests={'cpu': '100m', 'memory': '256Mi'},
+                    limits={'cpu': '100m', 'memory': '256Mi'}))))
+
+    isvc = V1alpha2InferenceService(api_version=constants.KFSERVING_API_VERSION,
+                                    kind=constants.KFSERVING_KIND,
+                                    metadata=client.V1ObjectMeta(
+                                        name=service_name, namespace=KFSERVING_TEST_NAMESPACE),
+                                    spec=V1alpha2InferenceServiceSpec(default=default_endpoint_spec))
+
+    KFServing.create(isvc)
+    KFServing.wait_isvc_ready(service_name, namespace=KFSERVING_TEST_NAMESPACE)
+
+    # define canary endpoint spec, and then rollout 10% traffic to the canary version
+    canary_endpoint_spec = V1alpha2EndpointSpec(
+        predictor=V1alpha2PredictorSpec(
+            tensorflow=V1alpha2TensorflowSpec(
+                storage_uri='gs://kfserving-samples/models/tensorflow/flowers-2',
+                resources=V1ResourceRequirements(
+                    requests={'cpu':'100m','memory':'256Mi'},
+                    limits={'cpu':'100m', 'memory':'256Mi'}))))
+
+    KFServing.rollout_canary(service_name, canary=canary_endpoint_spec, percent=10,
+       namespace=KFSERVING_TEST_NAMESPACE, watch=True, timeout_seconds=120)
+    KFServing.wait_isvc_ready(service_name, namespace=KFSERVING_TEST_NAMESPACE)
+
+    # Promote Canary to Default
+    KFServing.promote(service_name, namespace=KFSERVING_TEST_NAMESPACE, watch=True, timeout_seconds=120)
+    KFServing.wait_isvc_ready(service_name, namespace=KFSERVING_TEST_NAMESPACE)
+
+    # Delete the InferenceService
+    KFServing.delete(service_name, namespace=KFSERVING_TEST_NAMESPACE)
