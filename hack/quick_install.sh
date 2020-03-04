040c501c94079e793f0a6e8e960071c6cf717cec
Dan Sun <dsun20@bloomberg.net>
2020-03-03 22:07:01 -0500

    Use version variable

--- hack/quick_install.sh
+++ hack/quick_install.sh
@@ -2,6 +2,7 @@ set -e
 
 export ISTIO_VERSION=1.3.6
 export KNATIVE_VERSION=v0.12.0
+export KFSERVING_VERSION=0.2.2
 curl -L https://git.io/getLatestIstio | sh -
 cd istio-${ISTIO_VERSION}
 
@@ -74,7 +75,7 @@ kubectl apply --filename https://github.com/knative/serving/releases/download/${
 
 cd ..
 # Install KFServing
-kubectl apply -f install/0.2.2/kfserving.yaml
+kubectl apply -f install/${KFSERVING_VERSION}/kfserving.yaml
 
 # Clean up
 rm -rf istio-${ISTIO_VERSION} 
