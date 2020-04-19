f4911e261cd77f5475567413a0e241ab21fbf003
Dan Sun <dsun20@bloomberg.net>
2020-04-18 23:03:36 -0400

    Fix test resource

--- test/e2e/predictor/test_triton.py
+++ test/e2e/predictor/test_triton.py
@@ -40,28 +40,12 @@ def test_triton():
         predictor=V1alpha2PredictorSpec(
             min_replicas=1,
             triton=V1alpha2TritonSpec(
-                storage_uri='gs://kfserving-samples/models/triton/bert',
-                resources=V1ResourceRequirements(
-                    requests={'cpu': '100m', 'memory': '1Gi'},
-                    limits={'cpu': '100m', 'memory': '1Gi'}))),
-        transformer=V1alpha2TransformerSpec(
-            min_replicas=1,
-            custom=V1alpha2CustomSpec(
-                container=V1Container(
-                  image='gcr.io/kubeflow-ci/kfserving/bert-transformer:latest',
-                  name='kfserving-container',
-                  env=[
-                      V1EnvVar(name="STORAGE_URI", value="gs://kfserving-samples/models/triton/bert-transformer")
-                  ],
-                  resources=V1ResourceRequirements(
-                    requests={'cpu': '1', 'memory': '16Gi', 'nvidia.com/gpu': '1'},
-                    limits={'cpu': '1', 'memory': '16Gi', 'nvidia.com/gpu': '1'})))))
+                storage_uri='gs://kfserving-samples/models/tensorrt')))
 
     isvc = V1alpha2InferenceService(api_version=api_version,
                                     kind=constants.KFSERVING_KIND,
                                     metadata=client.V1ObjectMeta(
-                                        name=service_name, namespace=KFSERVING_TEST_NAMESPACE,
-                                        annotations={'serving.kubeflow.org/gke-accelerator': 'nvidia-tesla-k80'}),
+                                        name=service_name, namespace=KFSERVING_TEST_NAMESPACE),
                                     spec=V1alpha2InferenceServiceSpec(default=default_endpoint_spec))
 
     KFServing.create(isvc)
@@ -70,10 +54,11 @@ def test_triton():
     except RuntimeError as e:
         print(KFServing.api_instance.get_namespaced_custom_object("serving.knative.dev", "v1", KFSERVING_TEST_NAMESPACE,
                                                                   "services", service_name + "-predictor-default"))
-        print(KFServing.api_instance.get_namespaced_custom_object("serving.knative.dev", "v1", KFSERVING_TEST_NAMESPACE,
-                                                                  "services", service_name + "-transformer-default"))
+        deployments = KFServing.app_api.list_namespaced_deployment(KFSERVING_TEST_NAMESPACE,
+                                                                   label_selector='serving.kubeflow.org/inferenceservice={}'.
+                                                                   format(service_name))
+        for deployment in deployments.items:
+            print(deployment)
         raise e
-    prediction = predict(service_name, './data/qa.json')
-    assert(prediction["predictions"] == "John F. Kennedy")
     KFServing.delete(service_name, KFSERVING_TEST_NAMESPACE)
 
