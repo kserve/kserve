0567d03e23621b5a5f3911ff9de7ea7451ae506f
Dan Sun <dsun20@bloomberg.net>
2020-05-01 21:29:10 -0400

    Skip tag resolution for nvcr.io

--- test/e2e/predictor/test_triton.py
+++ test/e2e/predictor/test_triton.py
@@ -34,8 +34,6 @@ api_version = constants.KFSERVING_GROUP + '/' + constants.KFSERVING_VERSION
 KFServing = KFServingClient(config_file="~/.kube/config")
 
 
-@pytest.mark.skip(reason="Unable to fetch image nvcr.io/nvidia/tritonserver:20.03-py3: "
-                         "UNAUTHORIZED: authentication required")
 def test_triton():
     service_name = 'isvc-triton'
     default_endpoint_spec = V1alpha2EndpointSpec(
