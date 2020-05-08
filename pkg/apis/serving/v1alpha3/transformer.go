2e02b69792042c40d7febe425d01837e6e9c4467
Ellis Bigelow <ellisbigelow@google.com>
2020-05-08 14:33:40 -0700

    Switched to pod template spec

--- pkg/apis/serving/v1alpha3/transformer.go
+++ pkg/apis/serving/v1alpha3/transformer.go
@@ -5,7 +5,7 @@ import v1 "k8s.io/api/core/v1"
 // TransformerSpec defines transformer service for pre/post processing
 type TransformerSpec struct {
 	// Passthrough to underlying Pods
-	*v1.PodSpec `json:",inline"`
+	*v1.PodTemplateSpec `json:",inline"`
 	// Extensions available in all components
 	*ComponentExtensionSpec `json:",inline"`
 }
