8bd0870e8b75b8f359a24381f4217e34907841f0
zhangrongguo <zhangrongguo@inspur.com>
2020-05-22 19:40:37 +0800

    Fix batcher issue.

--- python/kfserving/kfserving/models/__init__.py
+++ python/kfserving/kfserving/models/__init__.py
@@ -44,6 +44,7 @@ from kfserving.models.v1alpha2_inference_service_list import V1alpha2InferenceSe
 from kfserving.models.v1alpha2_inference_service_spec import V1alpha2InferenceServiceSpec
 from kfserving.models.v1alpha2_inference_service_status import V1alpha2InferenceServiceStatus
 from kfserving.models.v1alpha2_logger import V1alpha2Logger
+from kfserving.models.v1alpha2_batcher import V1alpha2Batcher
 from kfserving.models.v1alpha2_onnx_spec import V1alpha2ONNXSpec
 from kfserving.models.v1alpha2_predictor_spec import V1alpha2PredictorSpec
 from kfserving.models.v1alpha2_py_torch_spec import V1alpha2PyTorchSpec
