34715caab281c7ac3936cecbc16231ccecf41ce5
Ellis Tarn <ellistarn@gmail.com>
2020-06-05 16:23:32 -0700

    Prefixed with inference. Renamed types

--- pkg/apis/serving/v1alpha3/inference_router_validation.go
+++ pkg/apis/serving/v1alpha3/inference_router_validation.go
@@ -3,5 +3,5 @@ package v1alpha3
 import "sigs.k8s.io/controller-runtime/pkg/client"
 
 // Validate the resource
-func (r *Router) Validate(client client.Client) {
+func (i *InferenceRouter) Validate(client client.Client) {
 }
