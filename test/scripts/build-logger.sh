bfcceacda5dab3b8f57ee48aa43bab3e76e71d3d
Dan Sun <dsun20@bloomberg.net>
2020-06-06 21:18:33 -0400

    Fix dockerfile

--- test/scripts/build-logger.sh
+++ test/scripts/build-logger.sh
@@ -33,7 +33,6 @@ echo "Copy source to GOPATH"
 mkdir -p ${GO_DIR}
 cp -r cmd ${GO_DIR}/cmd
 cp -r pkg ${GO_DIR}/pkg
-cp -r vendor ${GO_DIR}/vendor
 cp -r third_party ${GO_DIR}/third_party
 cp logger.Dockerfile ${GO_DIR}/Dockerfile
 
