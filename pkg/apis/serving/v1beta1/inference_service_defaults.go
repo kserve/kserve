474f855ebce305e2e29429a5ebc45ed1099e11a7
Dan Sun <dsun20@bloomberg.net>
2020-06-27 18:12:25 -0400

    v1beta1 codegen

--- pkg/apis/serving/v1beta1/inference_service_defaults.go
+++ pkg/apis/serving/v1beta1/inference_service_defaults.go
@@ -1,8 +1,26 @@
 package v1beta1
 
-import "sigs.k8s.io/controller-runtime/pkg/client"
+import (
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+)
 
-// Default the resource
-func (i *InferenceService) Default(client client.Client) {
-	i.GetPredictor().Default()
+var logger = logf.Log.WithName("kfserving-v1beta1-defaulter")
+
+func (isvc *InferenceService) Default(client client.Client) {
+	logger.Info("Defaulting InferenceService", "namespace", isvc.Namespace, "name", isvc.Name)
+	/*configMap, err := GetInferenceServicesConfig(client)
+	if err != nil {
+		logger.Error(err, "Failed to get configmap")
+		return
+	}
+	isvc.Spec.Predictor.ApplyDefaults(configMap)
+
+	if isvc.Spec.Transformer != nil {
+		isvc.Spec.Transformer.ApplyDefaults(configMap)
+	}
+
+	if isvc.Spec.Explainer != nil {
+		isvc.Spec.Explainer.ApplyDefaults(configMap)
+	}*/
 }
