d03c91da5e892e035199e87dc866c642196427b2
yuzliu <yliu989@bloomberg.net>
2020-06-30 18:00:29 -0400

    update comments for address

--- pkg/apis/serving/v1beta1/trained_model_status.go
+++ pkg/apis/serving/v1beta1/trained_model_status.go
@@ -8,6 +8,6 @@ type TrainedModelStatus struct {
 	// Condition for "Deployed"
 	duckv1beta1.Status `json:",inline"`
 	// Addressable endpoint for the deployed trained model
-	// http://inferenceservice.metadata.name/v1/models/trainedmodel.metadata.name
+	// http://<inferenceservice.metadata.name>/v1/models/<trainedmodel>.metadata.name
 	Address *duckv1beta1.Addressable `json:"address,omitempty"`
 }
