4d45e8c32fb003cd66fcc8ecff55e7626670e19a
yuzliu <yliu989@bloomberg.net>
2020-06-30 17:55:46 -0400

    remove model version policy

--- pkg/apis/serving/v1beta1/trained_model.go
+++ pkg/apis/serving/v1beta1/trained_model.go
@@ -22,17 +22,9 @@ type TrainedModelSpec struct {
 type ModelSpec struct {
 	// Storage URI for the model repository
 	StorageURI string `json:"storageUri"`
-	// Default to latest
-	ModelVersionPolicy ModelVersionPolicy `json:"modelVersionPolicy,omitempty"`
 	// Machine Learning <framework name>:<git tag>
 	// The values could be: "tensorflow:v2.2.0","pytorch:v1.5.1","sklearn:0.23.1","onnx:v1.7.0","xgboost:v1.1.1", "myawesomeinternalframework:1.1.0" etc.
 	Framework string `json:"framework"`
 	// Defaults to requests and limits of 1CPU, 2Gb MEM.
 	Resources v1.ResourceRequirements `json:"resources,omitempty"`
-}
-type ModelVersionPolicy struct {
-	// +kubebuilder:webhooks:Enum={"all","latest","specific"}
-	ModelVersionPolicy string `json:"modelVersionPolicy"`
-	// A list of specific model versions to serve, it is required when model version policy is "specific"
-	ModelVersions []string `json:"modelVersions,omitempty"`
 }
