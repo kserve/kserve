79975d465d95b6e45059f2a9ead4471568fa3eca
Peter Salanki <peter@salanki.st>
2020-07-06 23:22:03 -0400

    PRIVATE: Order matters

--- python/kfserving/kfserving/handlers/http.py
+++ python/kfserving/kfserving/handlers/http.py
@@ -42,7 +42,7 @@ class PredictHandler(HTTPHandler):
             )
         request = model.preprocess(body)
         request = self.validate(request)
-        response = await model.predict(request) if inspect.iscoroutinefunction(model.predict) else model.predict(request)
+        response = (await model.predict(request)) if inspect.iscoroutinefunction(model.predict) else model.predict(request)
         response = model.postprocess(response)
         self.write(response)
 
@@ -59,6 +59,6 @@ class ExplainHandler(HTTPHandler):
             )
         request = model.preprocess(body)
         request = self.validate(request)
-        response = await model.explain(request) if inspect.iscoroutinefunction(model.explain) else model.explain(request)
+        response = (await model.explain(request)) if inspect.iscoroutinefunction(model.explain) else model.explain(request)
         response = model.postprocess(response)
         self.write(response)
