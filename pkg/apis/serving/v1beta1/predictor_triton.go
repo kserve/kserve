6d76c3ebe12d3e88578372f0c2ff79d35342b098
Dan Sun <dsun20@bloomberg.net>
2020-07-19 13:50:01 -0400

    Add triton container

--- pkg/apis/serving/v1beta1/predictor_triton.go
+++ pkg/apis/serving/v1beta1/predictor_triton.go
@@ -1,6 +1,16 @@
 package v1beta1
 
-import v1 "k8s.io/api/core/v1"
+import (
+	"fmt"
+	"github.com/kubeflow/kfserving/pkg/constants"
+	v1 "k8s.io/api/core/v1"
+)
+
+var (
+	// For versioning see https://github.com/NVIDIA/triton-inference-server/releases
+	TritonISGRPCPort = int32(9000)
+	TritonISRestPort = int32(8080)
+)
 
 // TritonSpec defines arguments for configuring Triton model serving.
 type TritonSpec struct {
@@ -18,7 +28,23 @@ func (t *TritonSpec) Default() {}
 
 // GetContainers transforms the resource into a container spec
 func (t *TritonSpec) GetContainer(modelName string, config *InferenceServicesConfig) *v1.Container {
-	return &v1.Container{}
+	if t.Container.Image == "" {
+		t.Container.Image = config.Predictors.Triton.ContainerImage + ":" + *t.RuntimeVersion
+	}
+	return &v1.Container{
+		Name:      constants.InferenceServiceContainerName,
+		Image:     t.Container.Image,
+		Resources: t.Resources,
+		Args: []string{
+			"trtserver",
+			"--model-store=" + constants.DefaultModelLocalMountPath,
+			"--allow-poll-model-repository=false",
+			"--allow-grpc=true",
+			"--allow-http=true",
+			"--grpc-port=" + fmt.Sprint(TritonISGRPCPort),
+			"--http-port=" + fmt.Sprint(TritonISRestPort),
+		},
+	}
 }
 
 func (t *TritonSpec) GetStorageUri() *string {
