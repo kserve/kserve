ed125ba13499fd15d3b13074099fb60b37bbd4b2
Dan Sun <dsun20@bloomberg.net>
2020-07-26 16:35:41 -0400

    Conversion between v1alpha2 and v1beta1

--- pkg/apis/serving/v1alpha2/inferenceservice_conversion.go
+++ pkg/apis/serving/v1alpha2/inferenceservice_conversion.go
@@ -2,89 +2,143 @@ package v1alpha2
 
 import (
 	"github.com/kubeflow/kfserving/pkg/apis/serving/v1beta1"
+	v1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 )
 
-// Convert to hub version v1beta1
+// Convert to hub version from v1alpha2 to v1beta1
 func (src *InferenceService) ConvertTo(dstRaw conversion.Hub) error {
 	dst := dstRaw.(*v1beta1.InferenceService)
 	dst.ObjectMeta = src.ObjectMeta
 	if src.Spec.Default.Predictor.Tensorflow != nil {
-		dst.Spec.Predictor.Tensorflow.RuntimeVersion = src.Spec.Default.Predictor.Tensorflow.RuntimeVersion
-		dst.Spec.Predictor.Tensorflow.StorageURI = &src.Spec.Default.Predictor.Tensorflow.StorageURI
-		dst.Spec.Predictor.Tensorflow.Resources = src.Spec.Default.Predictor.Tensorflow.Resources
+		dst.Spec.Predictor.Tensorflow = &v1beta1.TensorflowSpec{
+			PredictorExtensionSpec: v1beta1.PredictorExtensionSpec{
+				RuntimeVersion: src.Spec.Default.Predictor.Tensorflow.RuntimeVersion,
+				StorageURI: &src.Spec.Default.Predictor.Tensorflow.StorageURI,
+				Container: v1.Container{
+					Resources: src.Spec.Default.Predictor.Tensorflow.Resources,
+				},
+			},
+		}
 	} else if src.Spec.Default.Predictor.SKLearn != nil {
-		dst.Spec.Predictor.SKLearn.RuntimeVersion = src.Spec.Default.Predictor.SKLearn.RuntimeVersion
-		dst.Spec.Predictor.SKLearn.StorageURI = &src.Spec.Default.Predictor.SKLearn.StorageURI
-		dst.Spec.Predictor.SKLearn.Resources = src.Spec.Default.Predictor.SKLearn.Resources
+		dst.Spec.Predictor.SKLearn = &v1beta1.SKLearnSpec{
+			PredictorExtensionSpec: v1beta1.PredictorExtensionSpec{
+				RuntimeVersion: src.Spec.Default.Predictor.SKLearn.RuntimeVersion,
+				StorageURI: &src.Spec.Default.Predictor.SKLearn.StorageURI,
+				Container: v1.Container{
+					Resources: src.Spec.Default.Predictor.SKLearn.Resources,
+				},
+			},
+		}
 	} else if src.Spec.Default.Predictor.XGBoost != nil {
-		dst.Spec.Predictor.XGBoost.RuntimeVersion = src.Spec.Default.Predictor.XGBoost.RuntimeVersion
-		dst.Spec.Predictor.XGBoost.StorageURI = &src.Spec.Default.Predictor.XGBoost.StorageURI
-		dst.Spec.Predictor.XGBoost.Resources = src.Spec.Default.Predictor.XGBoost.Resources
+		dst.Spec.Predictor.XGBoost = &v1beta1.XGBoostSpec{
+			PredictorExtensionSpec: v1beta1.PredictorExtensionSpec{
+				RuntimeVersion: src.Spec.Default.Predictor.XGBoost.RuntimeVersion,
+				StorageURI: &src.Spec.Default.Predictor.XGBoost.StorageURI,
+				Container: v1.Container{
+					Resources: src.Spec.Default.Predictor.XGBoost.Resources,
+				},
+			},
+		}
 	} else if src.Spec.Default.Predictor.Triton != nil {
-		dst.Spec.Predictor.Triton.RuntimeVersion = src.Spec.Default.Predictor.Triton.RuntimeVersion
-		dst.Spec.Predictor.Triton.StorageURI = &src.Spec.Default.Predictor.Triton.StorageURI
-		dst.Spec.Predictor.Triton.Resources = src.Spec.Default.Predictor.Triton.Resources
+		dst.Spec.Predictor.Triton = &v1beta1.TritonSpec{
+			PredictorExtensionSpec: v1beta1.PredictorExtensionSpec{
+				RuntimeVersion: src.Spec.Default.Predictor.Triton.RuntimeVersion,
+				StorageURI: &src.Spec.Default.Predictor.Triton.StorageURI,
+				Container: v1.Container{
+					Resources: src.Spec.Default.Predictor.Triton.Resources,
+				},
+			},
+		}
 	} else if src.Spec.Default.Predictor.ONNX != nil {
-		dst.Spec.Predictor.ONNXRuntime.RuntimeVersion = src.Spec.Default.Predictor.ONNX.RuntimeVersion
-		dst.Spec.Predictor.ONNXRuntime.StorageURI = &src.Spec.Default.Predictor.ONNX.StorageURI
-		dst.Spec.Predictor.ONNXRuntime.Resources = src.Spec.Default.Predictor.ONNX.Resources
+		dst.Spec.Predictor.ONNXRuntime = &v1beta1.ONNXRuntimeSpec{
+			PredictorExtensionSpec: v1beta1.PredictorExtensionSpec{
+				RuntimeVersion: src.Spec.Default.Predictor.ONNX.RuntimeVersion,
+				StorageURI: &src.Spec.Default.Predictor.ONNX.StorageURI,
+				Container: v1.Container{
+					Resources: src.Spec.Default.Predictor.ONNX.Resources,
+				},
+			},
+		}
 	} else if src.Spec.Default.Predictor.PyTorch != nil {
-		dst.Spec.Predictor.PyTorch.RuntimeVersion = src.Spec.Default.Predictor.PyTorch.RuntimeVersion
-		dst.Spec.Predictor.PyTorch.StorageURI = &src.Spec.Default.Predictor.PyTorch.StorageURI
-		dst.Spec.Predictor.PyTorch.Resources = src.Spec.Default.Predictor.PyTorch.Resources
+		dst.Spec.Predictor.PyTorch = &v1beta1.TorchServeSpec{
+			PredictorExtensionSpec: v1beta1.PredictorExtensionSpec{
+				RuntimeVersion: src.Spec.Default.Predictor.PyTorch.RuntimeVersion,
+				StorageURI:     &src.Spec.Default.Predictor.PyTorch.StorageURI,
+				Container: v1.Container{
+					Resources: src.Spec.Default.Predictor.PyTorch.Resources,
+				},
+			},
+		}
 	}
 	dst.Spec.Predictor.MinReplicas = src.Spec.Default.Predictor.MinReplicas
 	dst.Spec.Predictor.MaxReplicas = src.Spec.Default.Predictor.MaxReplicas
 	dst.Spec.Predictor.ContainerConcurrency = src.Spec.Default.Predictor.Parallelism
-	dst.Spec.Predictor.Spec.ServiceAccountName = src.Spec.Default.Predictor.ServiceAccountName
+	if src.Spec.Default.Predictor.ServiceAccountName != "" {
+		dst.Spec.Predictor.Spec = v1.PodSpec{}
+		dst.Spec.Predictor.Spec.ServiceAccountName = src.Spec.Default.Predictor.ServiceAccountName
+	}
 	return nil
 }
 
-// Convert from hub version
+// Convert from hub version v1beta1 to v1alpha2
 func (dst *InferenceService) ConvertFrom(srcRaw conversion.Hub) error {
 	src := srcRaw.(*v1beta1.InferenceService)
 	dst.ObjectMeta = src.ObjectMeta
 	if src.Spec.Predictor.Tensorflow != nil {
-		dst.Spec.Default.Predictor.Tensorflow.RuntimeVersion = src.Spec.Predictor.Tensorflow.RuntimeVersion
+		dst.Spec.Default.Predictor.Tensorflow = &TensorflowSpec{
+			RuntimeVersion: src.Spec.Predictor.Tensorflow.RuntimeVersion,
+			Resources: src.Spec.Predictor.Tensorflow.Resources,
+		}
 		if src.Spec.Predictor.PyTorch.StorageURI != nil {
 			dst.Spec.Default.Predictor.Tensorflow.StorageURI = *src.Spec.Predictor.Tensorflow.StorageURI
 		}
-		dst.Spec.Default.Predictor.Tensorflow.Resources = src.Spec.Predictor.Tensorflow.Resources
 	} else if src.Spec.Predictor.SKLearn != nil {
-		dst.Spec.Default.Predictor.SKLearn.RuntimeVersion = src.Spec.Predictor.SKLearn.RuntimeVersion
+		dst.Spec.Default.Predictor.SKLearn = &SKLearnSpec{
+			RuntimeVersion: src.Spec.Predictor.SKLearn.RuntimeVersion,
+			Resources: src.Spec.Predictor.SKLearn.Resources,
+		}
 		if src.Spec.Predictor.SKLearn.StorageURI != nil {
 			dst.Spec.Default.Predictor.SKLearn.StorageURI = *src.Spec.Predictor.SKLearn.StorageURI
 		}
-		dst.Spec.Default.Predictor.SKLearn.Resources = src.Spec.Predictor.SKLearn.Resources
 	} else if src.Spec.Predictor.XGBoost != nil {
-		dst.Spec.Default.Predictor.XGBoost.RuntimeVersion = src.Spec.Predictor.XGBoost.RuntimeVersion
+		dst.Spec.Default.Predictor.XGBoost = &XGBoostSpec{
+			RuntimeVersion: src.Spec.Predictor.XGBoost.RuntimeVersion,
+			Resources: src.Spec.Predictor.XGBoost.Resources,
+		}
 		if src.Spec.Predictor.XGBoost.StorageURI != nil {
 			dst.Spec.Default.Predictor.XGBoost.StorageURI = *src.Spec.Predictor.XGBoost.StorageURI
 		}
-		dst.Spec.Default.Predictor.XGBoost.Resources = src.Spec.Predictor.XGBoost.Resources
 	} else if src.Spec.Predictor.Triton != nil {
-		dst.Spec.Default.Predictor.Triton.RuntimeVersion = src.Spec.Predictor.Triton.RuntimeVersion
+		dst.Spec.Default.Predictor.Triton = &TritonSpec{
+			RuntimeVersion: src.Spec.Predictor.Triton.RuntimeVersion,
+			Resources: src.Spec.Predictor.Triton.Resources,
+		}
 		if src.Spec.Predictor.Triton.StorageURI != nil {
 			dst.Spec.Default.Predictor.Triton.StorageURI = *src.Spec.Predictor.Triton.StorageURI
 		}
-		dst.Spec.Default.Predictor.Triton.Resources = src.Spec.Predictor.Triton.Resources
 	} else if src.Spec.Predictor.ONNXRuntime != nil {
-		dst.Spec.Default.Predictor.ONNX.RuntimeVersion = src.Spec.Predictor.ONNXRuntime.RuntimeVersion
+		dst.Spec.Default.Predictor.ONNX = &ONNXSpec{
+			RuntimeVersion: src.Spec.Predictor.ONNXRuntime.RuntimeVersion,
+			Resources: src.Spec.Predictor.ONNXRuntime.Resources,
+		}
 		if src.Spec.Predictor.ONNXRuntime.StorageURI != nil {
 			dst.Spec.Default.Predictor.ONNX.StorageURI = *src.Spec.Predictor.ONNXRuntime.StorageURI
 		}
-		dst.Spec.Default.Predictor.ONNX.Resources = src.Spec.Predictor.ONNXRuntime.Resources
 	} else if src.Spec.Predictor.PyTorch != nil {
-		dst.Spec.Default.Predictor.PyTorch.RuntimeVersion = src.Spec.Predictor.PyTorch.RuntimeVersion
+		dst.Spec.Default.Predictor.PyTorch = &PyTorchSpec{
+			RuntimeVersion: src.Spec.Predictor.PyTorch.RuntimeVersion,
+			Resources: src.Spec.Predictor.PyTorch.Resources,
+		}
 		if src.Spec.Predictor.PyTorch.StorageURI != nil {
 			dst.Spec.Default.Predictor.PyTorch.StorageURI = *src.Spec.Predictor.PyTorch.StorageURI
 		}
-		dst.Spec.Default.Predictor.PyTorch.Resources = src.Spec.Predictor.PyTorch.Resources
 	}
 	dst.Spec.Default.Predictor.MinReplicas = src.Spec.Predictor.MinReplicas
 	dst.Spec.Default.Predictor.MaxReplicas = src.Spec.Predictor.MaxReplicas
 	dst.Spec.Default.Predictor.Parallelism = src.Spec.Predictor.ContainerConcurrency
-	dst.Spec.Default.Predictor.ServiceAccountName = src.Spec.Predictor.Spec.ServiceAccountName
+	if src.Spec.Predictor.CustomPredictor != nil {
+		dst.Spec.Default.Predictor.ServiceAccountName = src.Spec.Predictor.CustomPredictor.Spec.ServiceAccountName
+	}
 	return nil
 }
