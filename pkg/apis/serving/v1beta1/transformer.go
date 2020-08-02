ba3b498285b68db3ec7627c2fb1595309a93d838
Dan Sun <dsun20@bloomberg.net>
2020-08-01 20:01:08 -0400

    Update transformer and explainer

--- pkg/apis/serving/v1beta1/transformer.go
+++ pkg/apis/serving/v1beta1/transformer.go
@@ -16,12 +16,70 @@ limitations under the License.
 
 package v1beta1
 
-import v1 "k8s.io/api/core/v1"
+import (
+	"fmt"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Constants
+const (
+	ExactlyOneTransformerViolatedError = "Exactly one of [Custom] must be specified in TransformerSpec"
+)
 
 // TransformerSpec defines transformer service for pre/post processing
 type TransformerSpec struct {
-	// Passthrough to underlying Pods
-	*v1.PodTemplateSpec `json:",inline"`
+	// Pass through Pod fields or specify a custom container spec
+	*CustomTransformer `json:",inline"`
 	// Extensions available in all components
 	*ComponentExtensionSpec `json:",inline"`
+}
+
+// Transformer interface is implemented by all Transformers
+// +kubebuilder:object:generate=false
+type Transformer interface {
+	GetContainer(metadata metav1.ObjectMeta) *v1.Container
+	GetStorageUri() *string
+	Default(config *InferenceServicesConfig)
+	Validate(config *InferenceServicesConfig) error
+}
+
+func getTransformer(t *TransformerSpec) (Transformer, error) {
+	transformers := []Transformer{}
+	if t.CustomTransformer != nil {
+		transformers = append(transformers, t.CustomTransformer)
+	}
+	// Fail if not exactly one
+	if len(transformers) != 1 {
+		err := fmt.Errorf(ExactlyOneTransformerViolatedError)
+		return nil, err
+	}
+	return transformers[0], nil
+}
+
+// Validate the TransformerSpec
+func (t *TransformerSpec) Validate(config *InferenceServicesConfig) error {
+	transformer, err := getTransformer(t)
+	if err != nil {
+		return err
+	}
+	for _, err := range []error{
+		validateContainerConcurrency(t.ContainerConcurrency),
+		validateReplicas(t.MinReplicas, t.MaxReplicas),
+		transformer.Validate(config),
+		validateLogger(t.LoggerSpec),
+	} {
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Apply Defaults to the TransformerSpec
+func (t *TransformerSpec) Default(config *InferenceServicesConfig) {
+	transformer, err := getTransformer(t)
+	if err == nil {
+		transformer.Default(config)
+	}
 }
