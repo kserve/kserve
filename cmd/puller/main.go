738598ddd32069c8d8c4115d6dd2f68f89dfebba
Ilan Filonenko <ifilonenko@bloomberg.net>
2020-08-04 10:42:54 -0700

    add downloader and requester logic

--- cmd/puller/main.go
+++ cmd/puller/main.go
@@ -8,13 +8,15 @@ import (
 
 var (
 	modelDir = flag.String("model_dir", "/tmp","directory for multi-model config files")
+	numWorkers = flag.Int("num_workers", 1,"number of workers for parallel downloads")
 )
 
 func main() {
 	flag.Parse()
+	puller.InitiatePullers(*numWorkers)
 	puller.OnConfigChange(func(e puller.EventWrapper) {
-		log.Println("Send a request to:", e.LoadState)
-		log.Println("for model", e.ModelName)
+		log.Println("Send a request to:", e.LoadState, "for model", e.ModelName)
+		puller.AddModelToChannel(e)
 	})
 	puller.WatchConfig(*modelDir)
 }
