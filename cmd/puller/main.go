9ed663530bd92f29b7a804dd493b160c2a52e156
Ilan Filonenko <ifilonenko@bloomberg.net>
2020-08-04 12:46:51 -0700

    add retry logic to donwload

--- cmd/puller/main.go
+++ cmd/puller/main.go
@@ -9,13 +9,14 @@ import (
 var (
 	modelDir = flag.String("model_dir", "/tmp","directory for multi-model config files")
 	numWorkers = flag.Int("num_workers", 1,"number of workers for parallel downloads")
+	numRetries = flag.Int("num_retries", 3, "number of retries for downloading a model")
 )
 
 func main() {
 	flag.Parse()
-	puller.InitiatePullers(*numWorkers)
+	puller.InitiatePullers(*numWorkers, *numRetries)
 	puller.OnConfigChange(func(e puller.EventWrapper) {
-		log.Println("Send a request to:", e.LoadState, "for model", e.ModelName)
+		log.Println("Send a request to:", e.LoadState, "model:", e.ModelName)
 		puller.AddModelToChannel(e)
 	})
 	puller.WatchConfig(*modelDir)
