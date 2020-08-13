07c038223e4ff3859982432056de2cc00222f1e0
yuzliu <yliu989@bloomberg.net>
2020-08-12 20:58:26 -0400

    update modelconfig when reconciling TrainedModel

--- pkg/controller/v1beta1/trainedmodel/reconcilers/modelconfig/configmap_reconciler.go
+++ pkg/controller/v1beta1/trainedmodel/reconcilers/modelconfig/configmap_reconciler.go
@@ -19,6 +19,7 @@ package modelconfig
 import (
 	"context"
 	v1beta1api "github.com/kubeflow/kfserving/pkg/apis/serving/v1beta1"
+	"github.com/kubeflow/kfserving/pkg/modelconfig"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -39,13 +40,24 @@ func NewConfigMapReconciler(client client.Client, scheme *runtime.Scheme) *Confi
 	}
 }
 
-func (c *ConfigMapReconciler) Reconcile(desired *corev1.ConfigMap, trainedModel *v1beta1api.TrainedModel) error {
-	if trainedModel.DeletionTimestamp != nil {
+func (c *ConfigMapReconciler) Reconcile(desired *corev1.ConfigMap, tm *v1beta1api.TrainedModel) error {
+	if tm.DeletionTimestamp != nil {
 		//A Trainedmodel is being deleted, remove the model from multi-model configmap
-		//TODO call modelconfig handler once https://github.com/kubeflow/kfserving/pull/992 is merged
+		deletedConfigs := []string{tm.Name}
+		mConfig := modelconfig.NewConfigsDelta(nil, deletedConfigs)
+		err := mConfig.Process(desired)
+		if err != nil {
+			return err
+		}
 	} else {
 		//A Trainedmodel is created or updated, add or update the model from multi-model configmap
-		//TODO call modelconfig handler once https://github.com/kubeflow/kfserving/pull/992 is merged
+		modelConfig := modelconfig.ModelConfig{Name: tm.Name, Spec: tm.Spec.Inference}
+		updatedConfigs := []modelconfig.ModelConfig{modelConfig}
+		mConfig := modelconfig.NewConfigsDelta(updatedConfigs, nil)
+		err := mConfig.Process(desired)
+		if err != nil {
+			return err
+		}
 	}
 	err := c.client.Create(context.TODO(), desired)
 	if err != nil {
