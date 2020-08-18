0aaedcf79b0ece204904d7ff7dd8243b4b6f0b91
Ilan Filonenko <ifilonenko@bloomberg.net>
2020-08-18 09:32:22 -0400

    resolve comments

--- cmd/puller/main.go
+++ cmd/puller/main.go
@@ -6,14 +6,14 @@ import (
 )
 
 var (
-	configDir  = flag.String("config-dir", "/mnt/configs", "directory for multi-model config files")
-	modelDir   = flag.String("model-dir", "/mnt/models", "directory for multi-model models")
+	configDir  = flag.String("config-dir", "/mnt/configs", "directory for model config files")
+	modelDir   = flag.String("model-dir", "/mnt/models", "directory for model files")
 	numWorkers = flag.Int("num-workers", 1, "number of workers, per model")
 	numRetries = flag.Int("num-retries", 3, "number of retries for downloading a model")
 )
 
 func main() {
 	flag.Parse()
-	puller.SetEnvs(*numRetries, *modelDir)
+	puller.SetDownloadSettings(*numRetries, *modelDir)
 	puller.WatchConfig(*configDir, *numWorkers)
 }
