8bb2a6b74d98f6265da3d9eea2738e5c934cb77b
yuzliu <yliu989@bloomberg.net>
2020-08-19 23:58:25 -0400

    refactor sharding strategy

--- pkg/controller/v1beta1/trainedmodel/sharding/memory/strategy.go
+++ pkg/controller/v1beta1/trainedmodel/sharding/memory/strategy.go
@@ -1,12 +1,8 @@
-package sharding
+package memory
 
 import (
 	v1beta1api "github.com/kubeflow/kfserving/pkg/apis/serving/v1beta1"
 )
-
-type Strategy interface {
-	GetOrAssignShard(trainedModel *v1beta1api.TrainedModel) int
-}
 
 //TODO MemoryStrategy will be implemented in another PR
 type MemoryStrategy struct {
