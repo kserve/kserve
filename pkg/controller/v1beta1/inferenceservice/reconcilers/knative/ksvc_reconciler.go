bdb68d78937c12ada3354947f14830df50e414bf
Dan Sun <dsun20@bloomberg.net>
2020-08-22 21:22:29 -0400

    Implement v1beta1 controller

---
+++ pkg/controller/v1beta1/inferenceservice/reconcilers/knative/ksvc_reconciler.go
@@ -0,0 +1,162 @@
+/*
+Copyright 2020 kubeflow.org.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package knative
+
+import (
+	"context"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"github.com/kubeflow/kfserving/pkg/apis/serving/v1beta1"
+	"github.com/kubeflow/kfserving/pkg/constants"
+	"github.com/kubeflow/kfserving/pkg/utils"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"knative.dev/pkg/kmp"
+	"knative.dev/serving/pkg/apis/autoscaling"
+	knservingv1 "knative.dev/serving/pkg/apis/serving/v1"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+)
+
+var log = logf.Log.WithName("KsvcReconciler")
+
+type KsvcReconciler struct {
+	client  client.Client
+	scheme  *runtime.Scheme
+	Service *knservingv1.Service
+}
+
+func NewKsvcReconciler(client client.Client, scheme *runtime.Scheme, isvc *metav1.ObjectMeta,
+	component v1beta1.ComponentType, deploymentSpec *v1beta1.ComponentExtensionSpec, podSpec *corev1.PodSpec) *KsvcReconciler {
+	return &KsvcReconciler{
+		client:  client,
+		scheme:  scheme,
+		Service: createKnativeService(isvc, component, deploymentSpec, podSpec),
+	}
+}
+
+func createKnativeService(isvc *metav1.ObjectMeta,
+	component v1beta1.ComponentType, deploymentSpec *v1beta1.ComponentExtensionSpec, podSpec *corev1.PodSpec) *knservingv1.Service {
+	annotations := isvc.GetAnnotations()
+
+	if deploymentSpec.MinReplicas == nil {
+		annotations[autoscaling.MinScaleAnnotationKey] = fmt.Sprint(constants.DefaultMinReplicas)
+	} else {
+		annotations[autoscaling.MinScaleAnnotationKey] = fmt.Sprint(*deploymentSpec.MinReplicas)
+	}
+
+	if deploymentSpec.MaxReplicas != 0 {
+		annotations[autoscaling.MaxScaleAnnotationKey] = fmt.Sprint(deploymentSpec.MaxReplicas)
+	}
+
+	// User can pass down scaling class annotation to overwrite the default scaling KPA
+	if _, ok := annotations[autoscaling.ClassAnnotationKey]; !ok {
+		annotations[autoscaling.ClassAnnotationKey] = autoscaling.KPA
+	}
+
+	return &knservingv1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      isvc.Name + "-" + string(component),
+			Namespace: isvc.Namespace,
+			Labels:    isvc.Labels,
+		},
+		Spec: knservingv1.ServiceSpec{
+			ConfigurationSpec: knservingv1.ConfigurationSpec{
+				Template: knservingv1.RevisionTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: utils.Union(isvc.Labels, map[string]string{
+							constants.InferenceServicePodLabelKey: isvc.Name,
+							constants.KServiceComponentLabel:      string(component),
+						}),
+						Annotations: annotations,
+					},
+					Spec: knservingv1.RevisionSpec{
+						TimeoutSeconds:       deploymentSpec.TimeoutSeconds,
+						ContainerConcurrency: deploymentSpec.ContainerConcurrency,
+						PodSpec:              *podSpec,
+					},
+				},
+			},
+			RouteSpec: knservingv1.RouteSpec{
+				Traffic: []knservingv1.TrafficTarget{
+					{
+						Tag:     "default",
+						Percent: proto.Int64(100),
+					},
+				},
+			},
+		},
+	}
+}
+
+func (r *KsvcReconciler) finalizeService(serviceName, namespace string) error {
+	existing := &knservingv1.Service{}
+	if err := r.client.Get(context.TODO(), types.NamespacedName{Name: serviceName, Namespace: namespace}, existing); err != nil {
+		if !errors.IsNotFound(err) {
+			return err
+		}
+	} else {
+		log.Info("Deleting Knative Service", "namespace", namespace, "name", serviceName)
+		if err := r.client.Delete(context.TODO(), existing, client.PropagationPolicy(metav1.DeletePropagationBackground)); err != nil {
+			if !errors.IsNotFound(err) {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (r *KsvcReconciler) Reconcile() (*knservingv1.ServiceStatus, error) {
+	// Create service if does not exist
+	desired := r.Service
+	existing := &knservingv1.Service{}
+	err := r.client.Get(context.TODO(), types.NamespacedName{Name: desired.Name, Namespace: desired.Namespace}, existing)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Info("Creating Knative Service", "namespace", desired.Namespace, "name", desired.Name)
+			return &desired.Status, r.client.Create(context.TODO(), desired)
+		}
+		return nil, err
+	}
+
+	// Return if no differences to reconcile.
+	if semanticEquals(desired, existing) {
+		return &existing.Status, nil
+	}
+
+	// Reconcile differences and update
+	diff, err := kmp.SafeDiff(desired.Spec.ConfigurationSpec, existing.Spec.ConfigurationSpec)
+	if err != nil {
+		return &existing.Status, fmt.Errorf("failed to diff Knative Service: %v", err)
+	}
+	log.Info("Reconciling Knative Service diff (-desired, +observed):", "diff", diff)
+	log.Info("Updating Knative Service", "namespace", desired.Namespace, "name", desired.Name)
+	existing.Spec.ConfigurationSpec = desired.Spec.ConfigurationSpec
+	existing.ObjectMeta.Labels = desired.ObjectMeta.Labels
+	if err := r.client.Update(context.TODO(), existing); err != nil {
+		return &existing.Status, err
+	}
+
+	return &existing.Status, nil
+}
+
+func semanticEquals(desiredService, service *knservingv1.Service) bool {
+	return equality.Semantic.DeepEqual(desiredService.Spec.ConfigurationSpec, service.Spec.ConfigurationSpec) &&
+		equality.Semantic.DeepEqual(desiredService.ObjectMeta.Labels, service.ObjectMeta.Labels)
+}
