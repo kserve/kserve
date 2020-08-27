17b6c04bac13e99ee1830a320bc93e388d3a0237
Ilan Filonenko <ifilonenko@bloomberg.net>
2020-08-27 14:05:52 -0400

    [POC] Multi-Model Puller (#989)
    * [WIP] Beginning logic for multi-model puller
    
    * change version to memory
    
    * moving puller to cmd
    
    * add downloader and requester logic
    
    * add retry logic to donwload
    
    * resolve intial comments
    
    * resolve comments and rebase
    
    * resolve comments
    
    * resolve comments and add s3 logic
    
    * have downloaded models be under a modelname
    
    * resolve comments
    
    * add unload logic
    
    * added retry logic and further hardening for failures
    
    * resolve comments and handle on-start
    
    * resolve comments and reorganize
    
    * fmt
    
    * resolve comments
    
    * inline puller
    
    * update go mod
    
    * move comment
    
    * remove unnnecessary comment

---
+++ pkg/agent/storage/utils.go
@@ -0,0 +1,42 @@
+package storage
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func FileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	return !os.IsNotExist(err) && !info.IsDir()
+}
+
+func Create(fileName string) (*os.File, error) {
+	if err := os.MkdirAll(filepath.Dir(fileName), 0770); err != nil {
+		return nil, err
+	}
+	return os.Create(fileName)
+}
+
+func RemoveDir(dir string) error {
+	d, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(dir, name))
+		if err != nil {
+			return err
+		}
+	}
+	// Remove empty dir
+	if err := os.Remove(dir); err != nil {
+		return fmt.Errorf("dir is unable to be deleted: %v", err)
+	}
+	return nil
+}
