1543cd8fcb3c47cc64b56b92f959932ec0a61a93
Dan Sun <dsun20@bloomberg.net>
2020-09-05 11:58:05 -0400

    More conversion test

--- pkg/apis/serving/v1alpha2/inferenceservice_conversion_test.go
+++ pkg/apis/serving/v1alpha2/inferenceservice_conversion_test.go
@@ -15,6 +15,78 @@ func TestInferenceServiceConversion(t *testing.T) {
 		v1alpha2spec *InferenceService
 		v1beta1Spec  *v1beta1.InferenceService
 	}{
+		"customPredictor": {
+			v1alpha2spec: &InferenceService{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "custom-predictor",
+					Namespace: "default",
+				},
+				Spec: InferenceServiceSpec{
+					Default: EndpointSpec{
+						Predictor: PredictorSpec{
+							DeploymentSpec: DeploymentSpec{
+								MinReplicas: GetIntReference(1),
+								MaxReplicas: 3,
+								Parallelism: 1,
+							},
+							Custom: &CustomSpec{
+								Container: v1.Container{
+									Name:  "kfserving-container",
+									Image: "custom-predictor:v1",
+									Resources: v1.ResourceRequirements{
+										Requests: v1.ResourceList{
+											v1.ResourceCPU:    resource.MustParse("1"),
+											v1.ResourceMemory: resource.MustParse("2Gi"),
+										},
+										Limits: v1.ResourceList{
+											v1.ResourceCPU:    resource.MustParse("1"),
+											v1.ResourceMemory: resource.MustParse("2Gi"),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			v1beta1Spec: &v1beta1.InferenceService{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "custom-predictor",
+					Namespace: "default",
+				},
+				Spec: v1beta1.InferenceServiceSpec{
+					Predictor: v1beta1.PredictorSpec{
+						ComponentExtensionSpec: v1beta1.ComponentExtensionSpec{
+							MinReplicas:          GetIntReference(1),
+							MaxReplicas:          3,
+							ContainerConcurrency: proto.Int64(1),
+						},
+						CustomPredictor: &v1beta1.CustomPredictor{
+							PodTemplateSpec: v1.PodTemplateSpec{
+								Spec: v1.PodSpec{
+									Containers: []v1.Container{
+										{
+											Name:  "kfserving-container",
+											Image: "custom-predictor:v1",
+											Resources: v1.ResourceRequirements{
+												Requests: v1.ResourceList{
+													v1.ResourceCPU:    resource.MustParse("1"),
+													v1.ResourceMemory: resource.MustParse("2Gi"),
+												},
+												Limits: v1.ResourceList{
+													v1.ResourceCPU:    resource.MustParse("1"),
+													v1.ResourceMemory: resource.MustParse("2Gi"),
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 		"transformer": {
 			v1alpha2spec: &InferenceService{
 				ObjectMeta: metav1.ObjectMeta{
@@ -103,6 +175,96 @@ func TestInferenceServiceConversion(t *testing.T) {
 												},
 											},
 										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"explainer": {
+			v1alpha2spec: &InferenceService{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "explainerConversionTest",
+					Namespace: "default",
+				},
+				Spec: InferenceServiceSpec{
+					Default: EndpointSpec{
+						Predictor: PredictorSpec{
+							DeploymentSpec: DeploymentSpec{
+								MinReplicas: GetIntReference(1),
+								MaxReplicas: 3,
+								Parallelism: 1,
+							},
+							Tensorflow: &TensorflowSpec{
+								StorageURI:     "s3://test/mnist/export",
+								RuntimeVersion: "1.13.0",
+							},
+						},
+						Explainer: &ExplainerSpec{
+							DeploymentSpec: DeploymentSpec{
+								MinReplicas: GetIntReference(1),
+								MaxReplicas: 3,
+								Parallelism: 1,
+							},
+							Alibi: &AlibiExplainerSpec{
+								Type: AlibiAnchorsTabularExplainer,
+								StorageURI: "s3://test/mnist/explainer",
+								RuntimeVersion: "0.4.0",
+								Resources: v1.ResourceRequirements{
+									Requests: v1.ResourceList{
+										v1.ResourceCPU:    resource.MustParse("1"),
+										v1.ResourceMemory: resource.MustParse("2Gi"),
+									},
+									Limits: v1.ResourceList{
+										v1.ResourceCPU:    resource.MustParse("1"),
+										v1.ResourceMemory: resource.MustParse("2Gi"),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			v1beta1Spec: &v1beta1.InferenceService{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "explainerConversionTest",
+					Namespace: "default",
+				},
+				Spec: v1beta1.InferenceServiceSpec{
+					Predictor: v1beta1.PredictorSpec{
+						ComponentExtensionSpec: v1beta1.ComponentExtensionSpec{
+							MinReplicas:          GetIntReference(1),
+							MaxReplicas:          3,
+							ContainerConcurrency: proto.Int64(1),
+						},
+						Tensorflow: &v1beta1.TFServingSpec{
+							PredictorExtensionSpec: v1beta1.PredictorExtensionSpec{
+								StorageURI:     proto.String("s3://test/mnist/export"),
+								RuntimeVersion: proto.String("1.13.0"),
+							},
+						},
+					},
+					Explainer: &v1beta1.ExplainerSpec{
+						ComponentExtensionSpec: v1beta1.ComponentExtensionSpec{
+							MinReplicas:          GetIntReference(1),
+							MaxReplicas:          3,
+							ContainerConcurrency: proto.Int64(1),
+						},
+						Alibi: &v1beta1.AlibiExplainerSpec{
+							Type: v1beta1.AlibiAnchorsTabularExplainer,
+							StorageURI: "s3://test/mnist/explainer",
+							RuntimeVersion: proto.String("0.4.0"),
+							Container: v1.Container{
+								Resources: v1.ResourceRequirements{
+									Requests: v1.ResourceList{
+										v1.ResourceCPU:    resource.MustParse("1"),
+										v1.ResourceMemory: resource.MustParse("2Gi"),
+									},
+									Limits: v1.ResourceList{
+										v1.ResourceCPU:    resource.MustParse("1"),
+										v1.ResourceMemory: resource.MustParse("2Gi"),
 									},
 								},
 							},
