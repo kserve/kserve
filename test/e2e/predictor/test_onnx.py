fe4a9c427ab4c4ed5d1f3421a40753f0d4cbdc97
Thomas Duffy <tduffy000@citymail.cuny.edu>
2020-09-25 14:09:28 -0400

    draft version of e2e test for ONNX

---
+++ test/e2e/predictor/test_onnx.py
@@ -0,0 +1,72 @@
+# Copyright 2019 kubeflow.org.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import numpy as np
+from kubernetes import client
+
+from kfserving import KFServingClient
+from kfserving import constants
+from kfserving import V1alpha2EndpointSpec
+from kfserving import V1alpha2PredictorSpec
+from kfserving import V1alpha2ONNXSpec
+from kfserving import V1alpha2InferenceServiceSpec
+from kfserving import V1alpha2InferenceService
+from kubernetes.client import V1ResourceRequirements
+
+from ..common.utils import predict
+from ..common.utils import KFSERVING_TEST_NAMESPACE
+
+from PIL import Image
+
+api_version = constants.KFSERVING_GROUP + '/' + constants.KFSERVING_VERSION
+KFServing = KFServingClient(config_file="~/.kube/config")
+ONNX_EXAMPLE_IMAGE_FILE = './data/onnx_input.jpg'
+ONNX_EXAMPLE_JSON_FILE = './data/onnx_input.json'
+
+def prepare_onnx_example(path):
+    im = Image.open(path)
+    return np.array(im.resize((300, 400))).astype('float32') / 255
+
+def test_onnx_kfserving():
+    service_name = 'isvc-onnx'
+    default_endpoint_spec = V1alpha2EndpointSpec(
+        predictor=V1alpha2PredictorSpec(
+            min_replicas=1,
+            onnx=V1alpha2ONNXSpec(
+                storage_uri='https://github.com/onnx/models/blob/master/vision/style_transfer/fast_neural_style/model/mosaic-9.onnx?raw=true',
+                resources=V1ResourceRequirements(
+                    requests={'cpu': '1', 'memory': '2Gi'},
+                    limits={'cpu': '1', 'memory': '2Gi'}))))
+
+    isvc = V1alpha2InferenceService(api_version=api_version,
+                                    kind=constants.KFSERVING_KIND,
+                                    metadata=client.V1ObjectMeta(
+                                        name=service_name, namespace=KFSERVING_TEST_NAMESPACE),
+                                    spec=V1alpha2InferenceServiceSpec(default=default_endpoint_spec))
+
+    KFServing.create(isvc)
+    KFServing.wait_isvc_ready(service_name, namespace=KFSERVING_TEST_NAMESPACE)
+
+    arr = prepare_onnx_example(ONNX_EXAMPLE_IMAGE_FILE)
+    with open(ONNX_EXAMPLE_JSON_FILE, 'w') as stream:
+        payload = {'instances': [arr]}
+        json.dump(payload, stream)
+        stream.close()
+
+    res = predict(service_name, ONNX_EXAMPLE_IMAGE_FILE)
+    assert(arr.shape == res.shape)
+
+    # Delete the InferenceService
+    KFServing.delete(service_name, namespace=KFSERVING_TEST_NAMESPACE)
