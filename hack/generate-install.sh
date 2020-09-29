6feb8f6cbb8cb31e51733b48aa8c9445facd9cee
Hanbae Seo <jazzsir@gmail.com>
2020-09-29 06:43:18 +0000

    Add kubeflow overlays Use kustomize embedded in kubectl to support env

--- hack/generate-install.sh
+++ hack/generate-install.sh
@@ -38,4 +38,4 @@ INSTALL_DIR=./install/$TAG
 INSTALL_PATH=$INSTALL_DIR/kfserving.yaml
 
 mkdir -p $INSTALL_DIR
-kustomize build config/default | sed s/:latest/:$TAG/ > $INSTALL_PATH
+kubectl kustomize config/default | sed s/:latest/:$TAG/ > $INSTALL_PATH
