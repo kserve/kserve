000812c2fe8bb6cd2e210023616795f7e176bda4
Adrian Gonzalez-Martin <agm@seldon.io>
2020-10-23 10:22:14 +0100

    Add defaults for missing model-settings.json

--- pkg/constants/mlserver.go
+++ pkg/constants/mlserver.go
@@ -5,9 +5,13 @@ const (
 	MLServerGRPCPortEnv            = "MLSERVER_GRPC_PORT"
 	MLServerModelsDirEnv           = "MODELS_DIR"
 	MLServerModelImplementationEnv = "MLSERVER_MODEL_IMPLEMENTATION"
+	MLServerModelNameEnv           = "MLSERVER_MODEL_NAME"
+	MLServerModelVersionEnv        = "MLSERVER_MODEL_VERSION"
 
 	MLServerSKLearnImplementation = "mlserver.models.SKLearnModel"
 	MLServerXGBoostImplementation = "mlserver.models.XGBoostModel"
+
+	MLServerModelVersionDefault = "v1"
 )
 
 var (
