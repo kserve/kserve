de5580c8a853153e1ea311e3da6465adde46ab94
Andrew-Butler <Andrew.Butler@ibm.com>
2020-10-26 15:26:06 -0700

    Update example script

--- docs/samples/explanation/art/mnist/query_explain.py
+++ docs/samples/explanation/art/mnist/query_explain.py
@@ -18,7 +18,6 @@ test_num = 349
 plt_image = data.test_data[test_num]
 input_image = np.array([data.test_data[test_num]])
 input_label = data.test_labels[test_num]
-print(input_image.shape)
 input_image = input_image.tolist()
 payload = {
     "instances": [input_image, [input_label.argmax().item()]]
@@ -27,38 +26,34 @@ payload = {
 x = time.time()
 
 res = requests.post(endpoint, json=payload, headers=headers)
-print("TIME TAKEN: ", x - time.time())
+print("TIME TAKEN: ", time.time() - x)
 
 print(res)
 res_json = res.json()
 
-print(res_json["explanations"].keys())
 label = input_label.argmax()
-pred = res_json["explanations"]["prediction"]
-adv_pred = res_json["explanations"]["adversarial_prediction"]
-print("Label (", label, ") : Prediction (", pred, ") : Adv Pred (", adv_pred,")")
-
-
 adv_im = np.asarray(res_json["explanations"]["adversarial_example"])
-adv_class = res_json["explanations"]["adversarial_prediction"]
+adv_class = res_json["explanations"]["adversarial_prediction"] 
 image_class = res_json["explanations"]["prediction"]
 
-print("Checking difference...")
 is_diff = False
 for x in range(0, len(input_image[0])):
     for y in range(0, len(input_image[0][0])):
         for z in range(0, len(input_image[0][0][0])):
             if(input_image[0][x][y][z] != adv_im[0][x][y][z]):
                 is_diff = True
-print("is different: ", is_diff)
 
-fig0 = (plt_image[:,:,0] + 0.5) * 255
-fig_adv = (adv_im[0,:,:,0] + 0.5) * 255
+if (is_diff):
+    print("Found an adversarial example.")
+    fig0 = (plt_image[:,:,0] + 0.5) * 255
+    fig_adv = (adv_im[0,:,:,0] + 0.5) * 255
 
-f, axarr = plt.subplots(1, 2, figsize=(10,10))
-axarr[0].set_title("Original (" + str(input_label) + ")")
-axarr[1].set_title("Adversarial (" + str(adv_class) + ")")
+    f, axarr = plt.subplots(1, 2, figsize=(10,10))
+    axarr[0].set_title("Original (" + str(input_label) + ")")
+    axarr[1].set_title("Adversarial (" + str(adv_class) + ")")
 
-axarr[0].imshow(fig0, cmap="gray")
-axarr[1].imshow(fig_adv, cmap="gray")
-plt.show()
+    axarr[0].imshow(fig0, cmap="gray")
+    axarr[1].imshow(fig_adv, cmap="gray")
+    plt.show()
+else:
+    print("Unable to find an adversarial example.")
