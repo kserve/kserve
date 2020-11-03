aab92159094d45b0503f8db07e916c283829571b
yliu989 <yliu989@bloomberg.net>
2020-11-02 19:31:49 -0500

    fix unit test

--- pkg/credentials/service_account_credentials_test.go
+++ pkg/credentials/service_account_credentials_test.go
@@ -133,10 +133,6 @@ func TestS3CredentialBuilder(t *testing.T) {
 													Name:  s3.AWSEndpointUrl,
 													Value: "https://s3.aws.com",
 												},
-												{
-													Name:  s3.S3UseVirtualBucket,
-													Value: "true",
-												},
 											},
 										},
 									},
