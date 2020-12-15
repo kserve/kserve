f8128498b44668531ababaddf6ac8c6361d8b175
jagadeesh <jagadeeshj@ideas2it.com>
2020-12-15 18:30:47 +0530

    Fix: image converter for torchserve

--- docs/samples/v1beta1/torchserve/imgconv/img2bytearray.py
+++ docs/samples/v1beta1/torchserve/imgconv/img2bytearray.py
@@ -11,12 +11,10 @@ image = open(args.filename, 'rb') #open binary file in read mode
 image_read = image.read()
 image_64_encode = base64.b64encode(image_read)
 bytes_array = image_64_encode.decode('utf-8')
-request = {  
-  "instances":[  
-    {  
-      "data":{
-      "b64": bytes_array          
-      }
+request = {
+  "instances":[
+    {
+      "data": bytes_array
     }
   ]
 }
