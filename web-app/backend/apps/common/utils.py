d85e54ff6329fc14bb9c4f3bca03fa5c88182648
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2021-01-29 16:01:16 +0200

    mwa(back): initial commit
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

---
+++ web-app/backend/apps/common/utils.py
@@ -0,0 +1,90 @@
+import os
+
+from kubeflow.kubeflow.crud_backend import api, helpers, logging
+
+log = logging.getLogger(__name__)
+
+FILE_ABS_PATH = os.path.abspath(os.path.dirname(__file__))
+
+INFERENCESERVICE_TEMPLATE_YAML = os.path.join(
+    FILE_ABS_PATH, "yaml", "inference_service_template.yaml")
+
+
+def load_inference_service_template(**kwargs):
+    """
+    kwargs: the parameters to be replaced in the yaml
+
+    Reads the yaml for the web app's custom resource, replaces the variables
+    and returns it as a python dict.
+    """
+    return helpers.load_param_yaml(INFERENCESERVICE_TEMPLATE_YAML, **kwargs)
+
+
+# helper functions for accessing the logs of an InferenceService
+def get_inference_service_pods(svc, components=[]):
+    """
+    Return a dictionary with (endpoint, component) keys,
+    i.e. ("default", "predictor") and a list of pod names as values
+    """
+    namespace = svc["metadata"]["namespace"]
+
+    # dictionary{revisionName: (endpoint, component)}
+    revisions_dict = get_components_revisions_dict(components, svc)
+
+    if len(revisions_dict.keys()) == 0:
+        return {}
+
+    pods = api.list_pods(namespace, auth=False).items
+    component_pods_dict = {}
+    for pod in pods:
+        for revision in revisions_dict:
+            if pod.metadata.generate_name is None:
+                continue
+
+            if not pod.metadata.generate_name.startswith(revision):
+                continue
+
+            component = revisions_dict[revision]
+            curr_pod_names = component_pods_dict.get(component, [])
+            curr_pod_names.append(pod.metadata.name)
+            component_pods_dict[component] = curr_pod_names
+
+    if len(component_pods_dict.keys()) == 0:
+        log.info("No pods are found for inference service: %s",
+                 svc["metadata"]["name"])
+
+    return component_pods_dict
+
+
+# FIXME(elikatsis,kimwnasptd): Change the logic of this function according to
+# https://github.com/arrikto/dev/issues/867
+def get_components_revisions_dict(components, svc):
+    """
+    Return a dictionary{revisionId: component}
+    """
+    status = svc["status"]
+    revisions_dict = {}
+
+    for component in components:
+        if "components" not in status:
+            log.info("Component '%s' not in inference service '%s'",
+                     component, svc["metadata"]["name"])
+            continue
+
+        if component not in status["components"]:
+            log.info("Component '%s' not in inference service '%s'",
+                     component, svc["metadata"]["name"])
+            continue
+
+        if "latestReadyRevision" in status["components"][component]:
+            revision = status["components"][component]["latestReadyRevision"]
+
+            revisions_dict[revision] = component
+
+    if len(revisions_dict.keys()) == 0:
+        log.info(
+            "No revisions found for the inference service's components: %s",
+            svc["metadata"]["name"],
+        )
+
+    return revisions_dict
---
+++ web-app/backend/apps/common/versions.py
@@ -0,0 +1,27 @@
+from flask import current_app
+
+KNATIVE_ROUTE = {"group": "serving.knative.dev",
+                 "version": "v1",
+                 "kind": "routes"}
+KNATIVE_REVISION = {"group": "serving.knative.dev",
+                    "version": "v1",
+                    "kind": "revisions"}
+KNATIVE_CONF = {"group": "serving.knative.dev",
+                "version": "v1",
+                "kind": "configurations"}
+KNATIVE_SERVICE = {"group": "serving.knative.dev",
+                   "version": "v1",
+                   "kind": "services"}
+
+
+def inference_service_gvk():
+    try:
+        version = current_app.config["APP_VERSION"]
+        if version not in ['v1alpha2', 'v1beta2']:
+            version = 'v1beta1'
+    except RuntimeError:
+        version = "v1alpha2"
+
+    return {"group": "serving.kubeflow.org",
+            "version": version,
+            "kind": "inferenceservices"}
