377a7cd922c901ccc9a0b08e4fc809ba80efba13
achoo30 <achoo30@bloomberg.net>
2021-02-01 20:25:11 -0500

    Passed correct parameter to IsMMSPredictor

--- pkg/controller/v1beta1/inferenceservice/reconcilers/modelconfig/modelconfig_reconciler.go
+++ pkg/controller/v1beta1/inferenceservice/reconcilers/modelconfig/modelconfig_reconciler.go
@@ -47,7 +47,11 @@ func NewModelConfigReconciler(client client.Client, scheme *runtime.Scheme) *Mod
 }
 
 func (c *ModelConfigReconciler) Reconcile(isvc *v1beta1api.InferenceService) error {
-	if v1beta1utils.IsMMSPredictor(&isvc.Spec.Predictor, c.client) {
+	isvcConfig, err := v1beta1api.NewInferenceServicesConfig(c.client)
+	if err != nil {
+		return err
+	}
+	if v1beta1utils.IsMMSPredictor(&isvc.Spec.Predictor, isvcConfig) {
 		// Create an empty modelConfig for every InferenceService shard
 		// An InferenceService without storageUri is an empty model server with for multi-model serving so a modelConfig configmap should be created
 		// An InferenceService with storageUri is considered as multi-model InferenceService with only one model, a modelConfig configmap should be created as well
