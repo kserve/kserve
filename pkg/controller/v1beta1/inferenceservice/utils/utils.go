8e073754b928384dd957e6739bce1bebd8ad3c21
achoo30 <achoo30@bloomberg.net>
2021-02-01 22:15:26 -0500

    Comment change for clarity

--- pkg/controller/v1beta1/inferenceservice/utils/utils.go
+++ pkg/controller/v1beta1/inferenceservice/utils/utils.go
@@ -20,7 +20,7 @@ import (
 	v1beta1api "github.com/kubeflow/kfserving/pkg/apis/serving/v1beta1"
 )
 
-// Only enable MMS predictor for sklearn and xgboost model server
+// Only enable MMS predictor when predictor config sets MMS to true and storage uri is not set
 func IsMMSPredictor(predictor *v1beta1api.PredictorSpec, isvcConfig *v1beta1api.InferenceServicesConfig) bool {
 	return predictor.GetImplementation().IsMMS(isvcConfig) && predictor.GetImplementation().GetStorageUri() == nil
 }
