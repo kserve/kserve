59ef1254c67115c8b7b39d4bf89cd1c3699220fc
Heqin Wang <heqin.wang@ibm.com>
2021-02-15 18:06:22 -0800

    adapt the aixserver to accept inputs on the explainer's parameters

--- docs/samples/explanation/aix/mnist/query_explain.py
+++ docs/samples/explanation/aix/mnist/query_explain.py
@@ -2,6 +2,7 @@ import os
 import sys
 import requests
 import json
+import math
 from matplotlib import pyplot as plt
 import numpy as np
 from aix360.datasets import MNISTDataset
@@ -22,6 +23,7 @@ endpoint = sys.argv[1]
 headers = {
     'Host': sys.argv[2]
 }
+parameters = {}
 test_num = 1002
 is_file = False
 if len(sys.argv) > 3:
@@ -30,6 +32,11 @@ if len(sys.argv) > 3:
     except:
         is_file = True
 
+    if len(sys.argv) > 4:
+        try:
+            parameters = json.loads(sys.argv[4]) 
+        except:
+            raise Exception("Failed to convert to json format. ")
 if is_file:
     inputs = open(sys.argv[2])
     inputs = json.load(inputs)
@@ -45,6 +52,7 @@ else:
     inputs = gray2rgb(inputs.reshape((-1, 28, 28)))
     inputs = np.reshape(inputs, (28,28,3))
 input_image = {"instances": [inputs.tolist()]}
+input_image.update(parameters)
 print("Sending Explain Query")
 
 x = time.time()
@@ -61,8 +69,11 @@ temp = np.array(res_json["explanations"]["temp"])
 masks = np.array(res_json["explanations"]["masks"])
 top_labels = np.array(res_json["explanations"]["top_labels"])
 
-fig, m_axs = plt.subplots(2,5, figsize = (12,6))
+fig, m_axs = plt.subplots(math.ceil(len(top_labels)/5),5, figsize = (12,6))
 for i, c_ax in enumerate(m_axs.flatten()):
+    if i >= len(top_labels):
+        c_ax.axis('off')
+        continue
     mask = masks[i]
     c_ax.imshow(label2rgb(mask, temp, bg_label = 0), interpolation = 'nearest')
     c_ax.set_title('Positive for {}\nActual {}'.format(top_labels[i], actual))
