c81e9ca52f8670805915ece6a7c3ab1954f382b2
abchoo <77467773+abchoo@users.noreply.github.com>
2021-02-18 19:10:24 -0500

    Feature to include headers in http/https service requests (non-MMS) (#1372)
    * Added http(s) header support extracted from env var + tar format
    
    * Created http(s) uri secret support to extract headers
    
    * Created tests for http(s) secret extraction
    
    * Added condition to extract secret volume from uri
    
    * Added how to set headers for http(s) uri request in secret
    
    * Added minor note on formatting
    
    * Added minor note on how to convert to base64
    
    * refactored variable name and introduced constant
    
    * Secret Data Key Header -> Headers
    
    * Changed base-uri -> host-uri for consistency
    
    * Checked secret data for https-host-uri instead of annotation
    
    * Moved host uri to secret data
    
    * Added parsing logic for headers/host uri in secret data
    
    * Fixed test to match host uri in secret data
    
    * Updated variables for clarity
    
    * Stored header as a json string in env
    
    * Parse header as json
    
    * Update headers to store json
    
    * Removed unnecessary code

---
+++ pkg/credentials/https/https_secret.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2021 kubeflow.org.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package https
+
+import (
+	v1 "k8s.io/api/core/v1"
+)
+
+// Create constants -- baseURI
+const (
+	HTTPSHost = "https-host"
+	HEADERS   = "headers"
+	NEWLINE   = "\n"
+)
+
+var (
+	HeadersSuffix  = "-" + HEADERS
+	ColonSeparator = ": "
+)
+
+// Can be used for http and https uris
+func BuildSecretEnvs(secret *v1.Secret) []v1.EnvVar {
+	envs := []v1.EnvVar{}
+	uriHost, ok := secret.Data[HTTPSHost]
+
+	if !ok {
+		return envs
+	}
+
+	headers, ok := secret.Data[HEADERS]
+
+	if !ok {
+		return envs
+	}
+
+	envs = append(envs, v1.EnvVar{
+		Name:  string(uriHost) + HeadersSuffix,
+		Value: string(headers),
+	})
+
+	return envs
+}
