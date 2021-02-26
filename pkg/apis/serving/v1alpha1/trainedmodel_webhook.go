fe6d6447e4cb57209fd2527d33d3923015152ea1
abchoo <77467773+abchoo@users.noreply.github.com>
2021-02-26 18:12:38 -0500

    Validation for storage uri in webhook (#1407)
    * add validation for storageUri
    
    * add test for invalid storageURIs
    
    * add function to get protocols as an array of string
    
    * generate regex dynamically from list of protocol prefixes
    
    * updated storage uri error to contain protocols
    
    * Reduced repeated regex
    
    * Fixed regex
    
    * Added exported function to check if Prefix is supported given a list of prefixes
    
    * Updated storageURI validation to use same prefix check as inference service
    
    * Removed test cases that are invalid and updated error message

--- pkg/apis/serving/v1alpha1/trainedmodel_webhook.go
+++ pkg/apis/serving/v1alpha1/trainedmodel_webhook.go
@@ -18,25 +18,32 @@ package v1alpha1
 
 import (
 	"fmt"
+	"github.com/kubeflow/kfserving/pkg/agent/storage"
+	"github.com/kubeflow/kfserving/pkg/apis/serving/v1beta1"
 	"github.com/kubeflow/kfserving/pkg/utils"
 	"k8s.io/apimachinery/pkg/runtime"
 	"regexp"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"strings"
 )
 
 // regular expressions for validation of isvc name
 const (
-	TmNameFmt                   string = "[a-z]([-a-z0-9]*[a-z0-9])?"
-	InvalidTmNameFormatError           = "the Trained Model \"%s\" is invalid: a Trained Model name must consist of lower case alphanumeric characters or '-', and must start with alphabetical character. (e.g. \"my-name\" or \"abc-123\", regex used for validation is '%s')"
-	InvalidTmMemoryModification        = "the Trained Model \"%s\" memory field is immutable. The memory was \"%s\" but it is updated to \"%s\""
+	CommaSpaceSeparator                 = ", "
+	TmNameFmt                    string = "[a-z]([-a-z0-9]*[a-z0-9])?"
+	InvalidTmNameFormatError            = "the Trained Model \"%s\" is invalid: a Trained Model name must consist of lower case alphanumeric characters or '-', and must start with alphabetical character. (e.g. \"my-name\" or \"abc-123\", regex used for validation is '%s')"
+	InvalidStorageUriFormatError        = "the Trained Model \"%s\" storageUri field is invalid. The storage uri must start with one of the prefixes: %s. (the storage uri given is \"%s\")"
+	InvalidTmMemoryModification         = "the Trained Model \"%s\" memory field is immutable. The memory was \"%s\" but it is updated to \"%s\""
 )
 
 var (
 	// log is for logging in this package.
 	tmLogger = logf.Log.WithName("trainedmodel-alpha1-validator")
-	// regular expressions for validation of isvc name
+	// regular expressions for validation of tm name
 	TmRegexp = regexp.MustCompile("^" + TmNameFmt + "$")
+	// protocols that are accepted by storage uri
+	StorageUriProtocols = strings.Join(storage.GetAllProtocol(), CommaSpaceSeparator)
 )
 
 // +kubebuilder:webhook:verbs=create;update,path=/validate-trainedmodel,mutating=false,failurePolicy=fail,groups=serving.kubeflow.org,resources=trainedmodels,versions=v1alpha1,name=trainedmodel.kfserving-webhook-server.validator
@@ -46,7 +53,6 @@ var _ webhook.Validator = &TrainedModel{}
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (tm *TrainedModel) ValidateCreate() error {
 	tmLogger.Info("validate create", "name", tm.Name)
-	// TODO: Validate storageURI
 	return utils.FirstNonNilError([]error{
 		tm.validateTrainedModel(),
 	})
@@ -83,6 +89,7 @@ func (tm *TrainedModel) validateMemorySpecNotModified(oldTm *TrainedModel) error
 func (tm *TrainedModel) validateTrainedModel() error {
 	return utils.FirstNonNilError([]error{
 		tm.validateTrainedModelName(),
+		tm.validateStorageURI(),
 	})
 }
 
@@ -96,6 +103,14 @@ func convertToTrainedModel(old runtime.Object) *TrainedModel {
 func (tm *TrainedModel) validateTrainedModelName() error {
 	if !TmRegexp.MatchString(tm.Name) {
 		return fmt.Errorf(InvalidTmNameFormatError, tm.Name, TmRegexp)
+	}
+	return nil
+}
+
+// Validates TrainModel's storageURI
+func (tm *TrainedModel) validateStorageURI() error {
+	if !v1beta1.IsPrefixStorageURISupported(tm.Spec.Model.StorageURI, storage.GetAllProtocol()) {
+		return fmt.Errorf(InvalidStorageUriFormatError, tm.Name, StorageUriProtocols, tm.Spec.Model.StorageURI)
 	}
 	return nil
 }
