c674d88dd1be58be91c2d4af228d9f128063d1ad
zhangzhengyuan <zhangzhengyuan0604@gmail.com>
2021-05-10 10:10:05 +0800

    add python/paddleserver
    Signed-off-by: zhangzhengyuan <zhangzhengyuan0604@gmail.com>
    
    add paddle to Makefile
    
    Signed-off-by: zhangzhengyuan <zhangzhengyuan0604@gmail.com>
    
    github workflow: install paddleserver before pytest
    
    Signed-off-by: zhangzhengyuan <zhangzhengyuan0604@gmail.com>

---
+++ python/paddleserver/paddleserver/__main__.py
@@ -0,0 +1,31 @@
+# Copyright 2021 kubeflow.org.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+from paddleserver import PaddleModel
+import kfserving
+
+DEFAULT_MODEL_NAME = "model"
+
+parser = argparse.ArgumentParser(parents=[kfserving.kfserver.parser])
+parser.add_argument('--model_dir', required=True,
+                    help='A URI pointer to the model directory')
+parser.add_argument('--model_name', default=DEFAULT_MODEL_NAME,
+                    help='The name that the model is served under.')
+args, _ = parser.parse_known_args()
+
+if __name__ == "__main__":
+    model = PaddleModel(args.model_name, args.model_dir)
+    model.load()
+    kfserving.KFServer().start([model])
---
+++ python/paddleserver/setup.py
@@ -0,0 +1,32 @@
+# Copyright 2021 kubeflow.org.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='paddleserver',
+    version='0.5.0',
+    author_email='zhangzhengyuan0604@gmail.com',
+    license='https://github.com/kubeflow/kfserving/LICENSE',
+    description='Model Server implementation for Paddle. \
+                 Not intended for use outside KFServing Frameworks Images',
+    long_description=open('README.md').read(),
+    python_requires='>3.4',
+    packages=find_packages("paddleserver"),
+    install_requires=[
+        "kfserving>=0.5.1",
+        "paddlepaddle>=2.0.2"
+    ],
+    extras_require={'test': ['opencv-python']}
+)
