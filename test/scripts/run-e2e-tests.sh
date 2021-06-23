eede877cc774ef0b6a9b103ee9191d0f83c87e86
DavidSpek <vanderspek.david@gmail.com>
2021-06-23 22:36:31 +0200

    Add global deny all policy and make logs easier to read

--- test/scripts/run-e2e-tests.sh
+++ test/scripts/run-e2e-tests.sh
@@ -28,15 +28,15 @@ KNATIVE_VERSION="v0.22.0"
 KUBECTL_VERSION="v1.20.2"
 CERT_MANAGER_VERSION="v1.2.0"
 
-echo "Upgrading kubectl ..."
+echo "==========Upgrading kubectl ...=========="
 wget -q -O /usr/local/bin/kubectl https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
 chmod a+x /usr/local/bin/kubectl
 
-echo "Configuring kubectl ..."
+echo "==========Configuring kubectl ...=========="
 pip3 install awscli --upgrade --user
 aws eks update-kubeconfig --region=${AWS_REGION} --name=${CLUSTER_NAME}
 
-echo "Install istio ..."
+echo "==========Install istio ...=========="
 mkdir istio_tmp
 pushd istio_tmp >/dev/null
   curl -L https://git.io/getLatestIstio | ISTIO_VERSION=${ISTIO_VERSION} sh -
@@ -72,10 +72,20 @@ EOF
 
 popd
 
-echo "Waiting for istio started ..."
+echo "==========Waiting for istio started ...=========="
 kubectl wait --for=condition=Ready pods --all --timeout=180s -n istio-system
 
-echo "Installing knative serving ..."
+echo "==========Installing Istio Global Deny-All AuthorizationPolicy ...=========="
+cat <<EOF | kubectl apply -f -
+apiVersion: security.istio.io/v1beta1
+kind: AuthorizationPolicy
+metadata:
+  name: global-deny-all
+  namespace: istio-system
+spec: {}
+EOF
+
+echo "==========Installing knative serving ...=========="
 kubectl apply -f https://github.com/knative/operator/releases/download/${KNATIVE_VERSION}/operator.yaml
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -92,7 +102,7 @@ metadata:
   namespace: knative-serving
 EOF
 
-echo "Waiting for knative started ..."
+echo "==========Waiting for knative started ...=========="
 kubectl wait --for=condition=Ready knativeservings -n knative-serving knative-serving --timeout=180s
 kubectl wait --for=condition=Ready pods --all --timeout=180s -n knative-serving -l 'app in (activator,autoscaler,autoscaler-hpa,controller,istio-webhook,networking-istio)'
 
@@ -101,15 +111,15 @@ kubectl patch cm config-deployment --patch '{"data":{"registriesSkippingTagResol
 # give longer revision timeout
 kubectl patch cm config-deployment --patch '{"data":{"progressDeadline": "600s"}}' -n knative-serving
 
-echo "Installing cert manager ..."
+echo "==========Installing cert manager ...=========="
 kubectl create namespace cert-manager
 sleep 2
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.yaml
 
-echo "Waiting for cert manager started ..."
+echo "==========Waiting for cert manager started ...=========="
 kubectl wait --for=condition=ready pod -l 'app in (cert-manager,webhook)' --timeout=180s -n cert-manager
 
-echo "Install KFServing ..."
+echo "==========Install KFServing ...=========="
 export GOPATH="$HOME/go"
 export PATH="${PATH}:${GOPATH}/bin"
 mkdir -p ${GOPATH}/src/github.com/kubeflow
@@ -122,16 +132,16 @@ sed -i -e "s/latest/${PULL_BASE_SHA}/g" config/overlays/test/configmap/inference
 sed -i -e "s/latest/${PULL_BASE_SHA}/g" config/overlays/test/manager_image_patch.yaml
 make deploy-ci
 
-echo "Waiting for KFServing started ..."
+echo "==========Waiting for KFServing started ...=========="
 kubectl wait --for=condition=Ready pods --all --timeout=180s -n kfserving-system
 
-echo "Creating a namespace kfserving-ci-test ..."
+echo "==========Creating a namespace kfserving-ci-test ...=========="
 kubectl create namespace kfserving-ci-e2e-test
 
-echo "Enabling istio injection in kfserving-ci-test namespace ..."
+echo "==========Enabling istio injection in kfserving-ci-test namespace ...=========="
 kubectl label namespace kfserving-ci-e2e-test istio-injection=enabled
 
-echo "Creating istio AuthorizationPolicy in kfserving-ci-test namespace ..."
+echo "==========Creating istio AuthorizationPolicy in kfserving-ci-test namespace ...=========="
 cat <<EOF | kubectl apply -f -
 apiVersion: security.istio.io/v1beta1
 kind: AuthorizationPolicy
@@ -158,9 +168,9 @@ spec:
         - /v1/models/*
 EOF
 
-echo "Istio, Knative and KFServing have been installed and started."
+echo "==========Istio, Knative and KFServing have been installed and started.=========="
 
-echo "Installing KFServing Python SDK ..."
+echo "==========Installing KFServing Python SDK ...=========="
 python3 -m pip install --upgrade pip
 pip3 install pytest==6.0.2 pytest-xdist pytest-rerunfailures
 pip3 install --upgrade pytest-tornasync
@@ -171,7 +181,7 @@ pushd python/kfserving >/dev/null
     python3 setup.py install --force --user
 popd
 
-echo "Starting E2E functional tests ..."
+echo "==========Starting E2E functional tests ...=========="
 pushd test/e2e >/dev/null
   pytest -n 4 --ignore=credentials/test_set_creds.py
 popd
