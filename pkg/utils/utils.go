119380d8a40b1a0a97dbb882aa23e9216c1c3e33
Hong Xu <hongx@ibm.com>
2021-07-09 16:39:05 -0700

    `ContainsString` and `Includes` utils are duplicates
    They achieve exactly the same functionality.

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -90,16 +90,7 @@ func FirstNonNilError(objects []error) error {
 	return nil
 }
 
-// Helper functions to check and remove string from a slice of strings.
-func ContainsString(slice []string, s string) bool {
-	for _, item := range slice {
-		if item == s {
-			return true
-		}
-	}
-	return false
-}
-
+// Helper functions to remove string from a slice of strings.
 func RemoveString(slice []string, s string) (result []string) {
 	for _, item := range slice {
 		if item == s {
