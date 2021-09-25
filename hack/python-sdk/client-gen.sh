15a6930fdb2d2e8ba075cabc1a51987b01476ce9
Dan Sun <dsun20@bloomberg.net>
2021-09-25 17:26:37 -0400

    Update pointer for creating first isvc

--- hack/python-sdk/client-gen.sh
+++ hack/python-sdk/client-gen.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Note: Need to merge /python/kfserving/README.md and some documents manually after the script execution.
+# Note: Need to merge /python/kserve/README.md and some documents manually after the script execution.
 
 set -o errexit
 set -o nounset
