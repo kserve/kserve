3717a2b5a0c01c92c924df9e5e4a64df35bad7f4
Adrian Gonzalez-Martin <agm@seldon.io>
2021-11-22 10:55:05 +0000

    Try to increase memory in case is OOO again

--- test/e2e/predictor/test_xgboost.py
+++ test/e2e/predictor/test_xgboost.py
@@ -66,7 +66,7 @@ def test_xgboost_v2_kserve():
             protocol_version="v2",
             resources=V1ResourceRequirements(
                 requests={"cpu": "100m", "memory": "256Mi"},
-                limits={"cpu": "100m", "memory": "512Mi"},
+                limits={"cpu": "100m", "memory": "1024Mi"},
             ),
         ),
     )
