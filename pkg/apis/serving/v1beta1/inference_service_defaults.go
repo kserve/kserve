8031f9c8b55f000259f7b8066f51ea819b554c38
Suresh Nakkeran <30799624+Suresh-Nakkeran@users.noreply.github.com>
2022-02-15 21:25:49 +0530

    add mlflow support (#2034)
    * add mlflow support
    
    Signed-off-by: Suresh Nakkeran <suresh.n@ideas2it.com>
    
    * added e2e test for lightgbm, mlflow models with mlserver
    
    Signed-off-by: Suresh Nakkeran <suresh.n@ideas2it.com>

--- pkg/apis/serving/v1beta1/inference_service_defaults.go
+++ pkg/apis/serving/v1beta1/inference_service_defaults.go
@@ -100,8 +100,9 @@ func (isvc *InferenceService) DefaultInferenceService(config *InferenceServicesC
 func (isvc *InferenceService) setPredictorModelDefaults() {
 	if isvc.Spec.Predictor.Model != nil {
 		// add mlserver specific default values
-		if isvc.Spec.Predictor.Model.Runtime != nil &&
-			*isvc.Spec.Predictor.Model.Runtime == constants.MLServer {
+		if isvc.Spec.Predictor.Model.ModelFormat.Name == constants.SupportedModelMLFlow ||
+			(isvc.Spec.Predictor.Model.Runtime != nil &&
+				*isvc.Spec.Predictor.Model.Runtime == constants.MLServer) {
 			isvc.setMlServerDefaults()
 		}
 		// add torchserve specific default values
@@ -373,6 +374,8 @@ func (isvc *InferenceService) setMlServerDefaults() {
 		modelClass = constants.MLServerModelClassXGBoost
 	} else if isvc.Spec.Predictor.Model.ModelFormat.Name == constants.SupportedModelLightGBM {
 		modelClass = constants.MLServerModelClassLightGBM
+	} else if isvc.Spec.Predictor.Model.ModelFormat.Name == constants.SupportedModelMLFlow {
+		modelClass = constants.MLServerModelClassMLFlow
 	}
 	if isvc.ObjectMeta.Labels == nil {
 		isvc.ObjectMeta.Labels = map[string]string{constants.ModelClassLabel: modelClass}
---
+++ test/e2e/predictor/test_mlflow.py
@@ -0,0 +1,66 @@
+# Copyright 2022 The KServe Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from kubernetes import client
+from kserve import (
+    constants,
+    KServeClient,
+    V1beta1InferenceService,
+    V1beta1InferenceServiceSpec,
+    V1beta1PredictorSpec,
+    V1beta1ModelSpec,
+    V1beta1ModelFormat,
+)
+from kubernetes.client import V1ResourceRequirements
+
+from ..common.utils import predict
+from ..common.utils import KSERVE_TEST_NAMESPACE
+
+
+def test_mlflow_v2_runtime_kserve():
+    service_name = "isvc-mlflow-v2-runtime"
+
+    predictor = V1beta1PredictorSpec(
+        min_replicas=1,
+        model=V1beta1ModelSpec(
+            model_format=V1beta1ModelFormat(
+                name="mlflow",
+            ),
+            storage_uri="gs://kfserving-examples/models/mlflow/wine",
+            protocol_version="v2",
+            resources=V1ResourceRequirements(
+                requests={"cpu": "100m", "memory": "256Mi"},
+                limits={"cpu": "1", "memory": "1Gi"},
+            ),
+        ),
+    )
+
+    isvc = V1beta1InferenceService(
+        api_version=constants.KSERVE_V1BETA1,
+        kind=constants.KSERVE_KIND,
+        metadata=client.V1ObjectMeta(
+            name=service_name, namespace=KSERVE_TEST_NAMESPACE
+        ),
+        spec=V1beta1InferenceServiceSpec(predictor=predictor),
+    )
+
+    kserve_client = KServeClient(config_file=os.environ.get("KUBECONFIG", "~/.kube/config"))
+    kserve_client.create(isvc)
+    kserve_client.wait_isvc_ready(service_name, namespace=KSERVE_TEST_NAMESPACE)
+
+    res = predict(service_name, "./data/mlflow_input_v2.json", protocol_version="v2")
+    assert res["outputs"][0]["data"] == [5.576883936610762]
+
+    kserve_client.delete(service_name, KSERVE_TEST_NAMESPACE)
