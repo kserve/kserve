e6b0ac00f574c3796b9cae8722b2448a806a63d6
Thang Vu <vuminhthang.cm@gmail.com>
2022-03-04 12:03:09 +0900

    fix: replace image tag Signed-off-by: Thang Vu <vuminhthang.cm@gmail.com>

--- pkg/controller/v1beta1/inferenceservice/utils/utils.go
+++ pkg/controller/v1beta1/inferenceservice/utils/utils.go
@@ -21,6 +21,7 @@ import (
 	"context"
 	"encoding/json"
 	"html/template"
+	"regexp"
 	"strings"
 
 	"github.com/kserve/kserve/pkg/apis/serving/v1alpha1"
@@ -186,7 +187,12 @@ func ReplacePlaceholders(container *v1.Container, meta metav1.ObjectMeta) error 
 func UpdateImageTag(container *v1.Container, runtimeVersion *string, isvcConfig *v1beta1.InferenceServicesConfig) {
 	image := container.Image
 	if runtimeVersion != nil && len(strings.Split(image, ":")) > 0 {
-		container.Image = strings.Split(image, ":")[0] + ":" + *runtimeVersion
+		re := regexp.MustCompile(`(:([\w.\-_]*))$`)
+		if len(re.FindString(image)) == 0 {
+			container.Image = image + ":" + *runtimeVersion
+		} else {
+			container.Image = re.ReplaceAllString(image, ":" + *runtimeVersion)
+		}
 		return
 	}
 	if utils.IsGPUEnabled(container.Resources) && len(strings.Split(image, ":")) > 0 {
