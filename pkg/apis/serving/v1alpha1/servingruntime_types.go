7f28d8e8c489195a44d6d4983587910f4c537209
Nick Hill <nickhill@us.ibm.com>
2022-04-29 17:27:46 -0700

    Add Volumes to ServingRuntimePodSpec; allow other built-in ServerTypes (#2147)
    * Add Volumes to ServingRuntimePodSpec; allow other built-in ServerTypes
    
    Update unit tests to cover merging volumes, and improve environment variable merging.
    
    Signed-off-by: Nick Hill <nickhill@us.ibm.com>
    
    * Address some review comments
    
    Signed-off-by: Nick Hill <nickhill@us.ibm.com>
    
    * Change to use strategic merge patch from k8s apimachinery
    
    Signed-off-by: Nick Hill <nickhill@us.ibm.com>
    
    * Change container args merge behaviour to concatenate
    
    Also rename a coreContainer var to mergedContainer.
    
    Signed-off-by: Nick Hill <nickhill@us.ibm.com>

--- pkg/apis/serving/v1alpha1/servingruntime_types.go
+++ pkg/apis/serving/v1alpha1/servingruntime_types.go
@@ -54,6 +54,13 @@ type ServingRuntimePodSpec struct {
 	// +patchStrategy=merge
 	Containers []corev1.Container `json:"containers" patchStrategy:"merge" patchMergeKey:"name" validate:"required"`
 
+	// List of volumes that can be mounted by containers belonging to the pod.
+	// More info: https://kubernetes.io/docs/concepts/storage/volumes
+	// +optional
+	// +patchMergeKey=name
+	// +patchStrategy=merge,retainKeys
+	Volumes []corev1.Volume `json:"volumes,omitempty" patchStrategy:"merge,retainKeys" patchMergeKey:"name" protobuf:"bytes,1,rep,name=volumes"`
+
 	// NodeSelector is a selector which must be true for the pod to fit on a node.
 	// Selector which must match a node's labels for the pod to be scheduled on that node.
 	// More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
@@ -131,20 +138,22 @@ type ServingRuntimeStatus struct {
 
 // ServerType constant for specifying the runtime name
 // +k8s:openapi-gen=true
-// +kubebuilder:validation:Enum=triton;mlserver
 type ServerType string
 
-// ServerType Enum
+// Built-in ServerTypes (others may be supported)
 const (
 	// Model server is Triton
 	Triton ServerType = "triton"
 	// Model server is MLServer
 	MLServer ServerType = "mlserver"
+	// Model server is OpenVino Model Server
+	OVMS ServerType = "ovms"
 )
 
 // +k8s:openapi-gen=true
 type BuiltInAdapter struct {
-	// ServerType can be one of triton/mlserver and the runtime's container must have the same name
+	// ServerType must be one of the supported built-in types such as "triton" or "mlserver",
+	// and the runtime's container must have the same name
 	ServerType ServerType `json:"serverType,omitempty"`
 	// Port which the runtime server listens for model management requests
 	RuntimeManagementPort int `json:"runtimeManagementPort,omitempty"`
