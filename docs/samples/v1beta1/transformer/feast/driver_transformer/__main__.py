9b720aef1f8533e78f15807ef7639907f5d737ea
Chin Huang <chhuang@us.ibm.com>
2022-06-03 15:47:30 -0700

    Update Feast transformer to support ModelMesh (#2204)
    Update Feast transformer to support ModelMesh
      - Add a new yaml file with ModelMesh annotation
      - Update the transformer code to check the predictor
        protocol and convert content types accordingly
    
    Signed-off-by: Chin Huang <chhuang@us.ibm.com>

--- docs/samples/v1beta1/transformer/feast/driver_transformer/__main__.py
+++ docs/samples/v1beta1/transformer/feast/driver_transformer/__main__.py
@@ -13,17 +13,20 @@
 
 import argparse
 import kserve
+
 from .driver_transformer import DriverTransformer
 
-import logging
-logging.basicConfig(level=kserve.constants.KSERVE_LOGLEVEL)
-
 DEFAULT_MODEL_NAME = "sklearn-driver-transformer"
+DEFAULT_PROTOCOL = "v1"
 
 parser = argparse.ArgumentParser(parents=[kserve.model_server.parser])
 parser.add_argument(
     "--predictor_host",
     help="The URL for the model predict function", required=True
+)
+parser.add_argument(
+    "--protocol", default=DEFAULT_PROTOCOL,
+    help="The protocol for the predictor"
 )
 parser.add_argument(
     "--model_name", default=DEFAULT_MODEL_NAME,
@@ -50,6 +53,7 @@ if __name__ == "__main__":
     transformer = DriverTransformer(
         name=args.model_name,
         predictor_host=args.predictor_host,
+        protocol=args.protocol,
         feast_serving_url=args.feast_serving_url,
         entity_ids=args.entity_ids,
         feature_refs=args.feature_refs)
