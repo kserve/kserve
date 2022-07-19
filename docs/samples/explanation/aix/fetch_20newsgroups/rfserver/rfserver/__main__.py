a5495ba70fa132d79d9904e9b951630f7241d8eb
408410069 <93316636+408410069@users.noreply.github.com>
2022-07-19 22:33:16 +0800

    Add files via upload

---
+++ docs/samples/explanation/aix/fetch_20newsgroups/rfserver/rfserver/__main__.py
@@ -0,0 +1,29 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import kserve
+import argparse
+
+from .model import RFModel
+
+DEFAULT_MODEL_NAME = "rfserver"
+
+parser = argparse.ArgumentParser(parents=[kserve.model_server.parser])
+parser.add_argument('--model_name', default=DEFAULT_MODEL_NAME,
+                    help='The name that the model is served under.')
+args, _ = parser.parse_known_args()
+
+if __name__ == "__main__":
+    model = RFModel(args.model_name)
+    model.load()
+    kserve.ModelServer().start([model])
---
+++ docs/samples/explanation/aix/fetch_20newsgroups/train_rf.py
@@ -0,0 +1,61 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import pickle
+import sklearn
+import numpy as np
+
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import datasets
+from sklearn.pipeline import make_pipeline
+
+
+class PipeStep(object):
+    """
+    Wrapper for turning functions into pipeline transforms (no-fitting)
+    """
+    def __init__(self, step_func):
+        self._step_func = step_func
+
+    def fit(self, *args):
+        return self
+
+    def transform(self, X):
+        return self._step_func(X)
+
+
+path_to_save = sys.argv[1]
+
+dataset = sys.argv[2]
+
+data = datasets.fetch_20newsgroups()
+
+X = data.data[1:1001]
+Y = data.target[1:1001]
+
+vectorizer = sklearn.feature_extraction.text.TfidfVectorizer(lowercase=False)
+X = vectorizer.fit_transform(X)
+
+simple_rf_pipeline = Pipeline([('RF', RandomForestClassifier())])
+
+simple_rf_pipeline.fit(X, Y)
+
+simple_rf_pipeline = Pipeline([('vectorizer', vectorizer), ('RF + fit', simple_rf_pipeline)])
+
+with open(path_to_save, 'wb') as f:
+    pickle.dump(simple_rf_pipeline, f)
+
+print("File saved")
