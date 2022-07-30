242ec1cffcdad93b8570e9ff0a7f6644b1d1ab02
Dan Sun <dsun20@bloomberg.net>
2022-07-30 08:48:33 -0400

    Update quick install to use 0.9.0
    Signed-off-by: Dan Sun <dsun20@bloomberg.net>

--- hack/quick_install.sh
+++ hack/quick_install.sh
@@ -32,7 +32,7 @@ done
 
 export ISTIO_VERSION=1.14.0
 export KNATIVE_VERSION=knative-v1.4.0
-export KSERVE_VERSION=v0.9.0-rc0
+export KSERVE_VERSION=v0.9.0
 export CERT_MANAGER_VERSION=v1.3.0
 export SCRIPT_DIR="$( dirname -- "${BASH_SOURCE[0]}" )"
 
