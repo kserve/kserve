e176787663fa54e27b77ba79936bc7739e0eb9a4
MessKon <35975404+MessKon@users.noreply.github.com>
2022-08-03 13:28:23 +0100

    Patch fixed critical CVEs in kserve/alibi-explainer (#2270)
    * update versions for alibi-explainer to resolve several critical CVEs
    
    Signed-off-by: MessKon <messiskon@gmail.com>
    
    * change required python version to >=3.7; no impact on critical CVEs
    
    Signed-off-by: MessKon <messiskon@gmail.com>
    
    * revert the kserve version change
    
    Signed-off-by: MessKon <messiskon@gmail.com>
    
    * revert alibi version change
    
    Signed-off-by: MessKon <messiskon@gmail.com>
    
    * fix flake8 lint errors
    
    Signed-off-by: MessKon <messiskon@gmail.com>
    
    * update aix360 module reference
    
    Signed-off-by: MessKon <messiskon@gmail.com>
    
    * revert aix360 module reference
    
    Signed-off-by: MessKon <messiskon@gmail.com>

--- test/e2e/transformer/test_transformer.py
+++ test/e2e/transformer/test_transformer.py
@@ -78,5 +78,5 @@ def test_transformer():
             print(pod)
         raise e
     res = predict(service_name, "./data/transformer.json", model_name="mnist")
-    assert(res.get("predictions")[0] == 2)
+    assert (res.get("predictions")[0] == 2)
     kserve_client.delete(service_name, KSERVE_TEST_NAMESPACE)
