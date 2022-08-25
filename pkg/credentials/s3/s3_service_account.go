df0d5414505f66b7cb567b60e42721ca11176920
Matt Rose <40691156+matty-rose@users.noreply.github.com>
2022-08-25 20:20:40 +1000

    AWS S3 IRSA Support (#2373)
    Adds S3 configuration options through the following
    - `inferenceservice` configmap (global options)
    - k8s service account annotations for AWS IRSA
    - k8s secret annotations attached to k8s service account for static
      credentials
    
    Signed-off-by: Matthew Rose <matthew.rose@maxkelsen.com>
    
    Signed-off-by: Matthew Rose <matthew.rose@maxkelsen.com>
    Signed-off-by: alexagriffith <agriffith96@gmail.com>

---
+++ pkg/credentials/s3/s3_service_account.go
@@ -0,0 +1,35 @@
+/*
+Copyright 2022 The KServe Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package s3
+
+import (
+	v1 "k8s.io/api/core/v1"
+)
+
+/*
+For a quick reference about AWS ENV variables:
+AWS Cli: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html
+Boto: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html#using-environment-variables
+*/
+
+func BuildServiceAccountEnvs(serviceAccount *v1.ServiceAccount, s3Config *S3Config) []v1.EnvVar {
+	envs := []v1.EnvVar{}
+
+	envs = append(envs, BuildS3EnvVars(serviceAccount.Annotations, s3Config)...)
+
+	return envs
+}
---
+++ pkg/credentials/s3/utils_test.go
@@ -0,0 +1,100 @@
+/*
+Copyright 2022 The KServe Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package s3
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	v1 "k8s.io/api/core/v1"
+)
+
+func TestBuildS3EnvVars(t *testing.T) {
+	scenarios := map[string]struct {
+		config      S3Config
+		annotations map[string]string
+		expected    []v1.EnvVar
+	}{
+		"S3Endpoint": {
+			annotations: map[string]string{
+				InferenceServiceS3SecretEndpointAnnotation: "s3.aws.com",
+			},
+			expected: []v1.EnvVar{
+				{
+					Name:  S3Endpoint,
+					Value: "s3.aws.com",
+				},
+				{
+					Name:  AWSEndpointUrl,
+					Value: "https://s3.aws.com",
+				},
+			},
+		},
+		"AllAnnotations": {
+			annotations: map[string]string{
+				InferenceServiceS3SecretEndpointAnnotation:   "s3.aws.com",
+				InferenceServiceS3SecretRegionAnnotation:     "us-east-2",
+				InferenceServiceS3SecretSSLAnnotation:        "0",
+				InferenceServiceS3SecretHttpsAnnotation:      "0",
+				InferenceServiceS3UseVirtualBucketAnnotation: "true",
+				InferenceServiceS3UseAnonymousCredential:     "true",
+				InferenceServiceS3CABundleAnnotation:         "value",
+			},
+			expected: []v1.EnvVar{
+				{
+					Name:  S3UseHttps,
+					Value: "0",
+				},
+				{
+					Name:  S3Endpoint,
+					Value: "s3.aws.com",
+				},
+				{
+					Name:  AWSEndpointUrl,
+					Value: "http://s3.aws.com",
+				},
+				{
+					Name:  S3VerifySSL,
+					Value: "0",
+				},
+				{
+					Name:  AWSAnonymousCredential,
+					Value: "true",
+				},
+				{
+					Name:  AWSRegion,
+					Value: "us-east-2",
+				},
+				{
+					Name:  S3UseVirtualBucket,
+					Value: "true",
+				},
+				{
+					Name:  AWSCABundle,
+					Value: "value",
+				},
+			},
+		},
+	}
+	for name, scenario := range scenarios {
+		envs := BuildS3EnvVars(scenario.annotations, &scenario.config)
+
+		if diff := cmp.Diff(scenario.expected, envs); diff != "" {
+			t.Errorf("Test %q unexpected result (-want +got): %v", name, diff)
+		}
+	}
+}
