0d2bb70b86160923a51c2ab193754f5c5ff27ce0
Lize Cai <lize.cai@sap.com>
2022-10-13 09:23:11 +0800

    Add ImagePullSecrets to ServingRuntimePodSpec (#2443)
    Signed-off-by: Lize Cai <lize.cai@sap.com>
    
    Signed-off-by: Lize Cai <lize.cai@sap.com>
    Signed-off-by: dilverse <109769432+dilverse@users.noreply.github.com>

--- pkg/apis/serving/v1alpha1/servingruntime_types.go
+++ pkg/apis/serving/v1alpha1/servingruntime_types.go
@@ -84,6 +84,14 @@ type ServingRuntimePodSpec struct {
 	// More info: http://kubernetes.io/docs/user-guide/annotations
 	// +optional
 	Annotations map[string]string `json:"annotations,omitempty"`
+	// ImagePullSecrets is an optional list of references to secrets in the same namespace to use for pulling any of the images used by this PodSpec.
+	// If specified, these secrets will be passed to individual puller implementations for them to use. For example,
+	// in the case of docker, only DockerConfig type secrets are honored.
+	// More info: https://kubernetes.io/docs/concepts/containers/images#specifying-imagepullsecrets-on-a-pod
+	// +optional
+	// +patchMergeKey=name
+	// +patchStrategy=merge
+	ImagePullSecrets []corev1.LocalObjectReference `json:"imagePullSecrets,omitempty" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,15,rep,name=imagePullSecrets"`
 
 	// Possibly other things here
 }
