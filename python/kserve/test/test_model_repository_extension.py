68dc55ddb90bfebce0eaf77baf4ad96cbbea975a
Xin Fu <xfu83@bloomberg.net>
2022-10-20 13:06:33 -0400

    Fix test_model_repository_extension.py with async
    Signed-off-by: Xin Fu <xfu83@bloomberg.net>

--- python/kserve/test/test_model_repository_extension.py
+++ python/kserve/test/test_model_repository_extension.py
@@ -5,6 +5,7 @@ from kserve.model_repository import ModelRepository
 from test.test_server import DummyModel, DummyModelRepository
 
 
+@pytest.mark.asyncio
 class TestModelRepositoryExtension:
     MODEL_NAME = "TestModel"
 
@@ -16,7 +17,7 @@ class TestModelRepositoryExtension:
         model_repo_ext._model_registry.update(model)
         return model_repo_ext
 
-    def test_index(self, model_repo_ext):
+    async def test_index(self, model_repo_ext):
         assert model_repo_ext.index() == [
             {
                 "name": self.MODEL_NAME,
@@ -52,39 +53,39 @@ class TestModelRepositoryExtension:
             }
         ]
 
-    def test_load(self):
+    async def test_load(self):
         model_repo_ext = ModelRepositoryExtension(
             model_registry=DummyModelRepository(test_load_success=True)
         )
-        model_repo_ext.load(self.MODEL_NAME)
+        await model_repo_ext.load(self.MODEL_NAME)
         model = model_repo_ext._model_registry.get_model(self.MODEL_NAME)
         assert model.name == self.MODEL_NAME
 
-    def test_load_fail(self):
+    async def test_load_fail(self):
         model_repo_ext = ModelRepositoryExtension(
             model_registry=DummyModelRepository(test_load_success=False)
         )
         with pytest.raises(ModelNotReady) as e:
-            model_repo_ext.load(self.MODEL_NAME)
+            await model_repo_ext.load(self.MODEL_NAME)
         assert e.value.model_name == self.MODEL_NAME
         assert e.value.error_msg == f"Model with name {self.MODEL_NAME} is not ready."
 
-    def test_load_fail_with_exception(self):
+    async def test_load_fail_with_exception(self):
         model_repo_ext = ModelRepositoryExtension(
             model_registry=DummyModelRepository(test_load_success=False, fail_with_exception=True)
         )
         with pytest.raises(ModelNotReady) as e:
-            model_repo_ext.load(self.MODEL_NAME)
+            await model_repo_ext.load(self.MODEL_NAME)
         assert e.value.model_name == self.MODEL_NAME
         assert e.value.error_msg == f"Model with name {self.MODEL_NAME} is not ready. " \
                                     f"Error type: <class 'Exception'> error " \
                                     f"msg: Could not load model {self.MODEL_NAME}."
 
-    def test_unload(self, model_repo_ext):
-        model_repo_ext.unload(self.MODEL_NAME)
+    async def test_unload(self, model_repo_ext):
+        await model_repo_ext.unload(self.MODEL_NAME)
         assert model_repo_ext._model_registry.get_models() == {}
 
-    def test_unload_fail(self, model_repo_ext):
+    async def test_unload_fail(self, model_repo_ext):
         with pytest.raises(ModelNotFound) as e:
-            model_repo_ext.unload("FAKE_NAME")
+            await model_repo_ext.unload("FAKE_NAME")
         assert e.value.reason == "Model with name FAKE_NAME does not exist."
