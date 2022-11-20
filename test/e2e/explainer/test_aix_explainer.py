6bfb68dce4d6036ff2a6b9822bc0bb44ed9357ab
lionl1106 <66839621+lionl1106@users.noreply.github.com>
2022-11-20 16:39:06 +0800

    Set up the configuration
    Signed-off-by: lionl1106 <66839621+lionl1106@users.noreply.github.com>

--- test/e2e/explainer/test_aix_explainer.py
+++ test/e2e/explainer/test_aix_explainer.py
@@ -57,7 +57,8 @@ def test_tabular_explainer():
             type='LimeImages',
             resources=V1ResourceRequirements(
                 requests={'cpu': '10m', 'memory': '128Mi'},
-                limits={'cpu': '100m', 'memory': '256Mi'}
+                limits={'cpu': '100m', 'memory': '256Mi'},
+                config={'top_labels': '10', 'min_weight': '0.01', 'num_samples': '100'}
             )
         )
     )
