a1c6a77ebd703f35363b573841b88ee8e165c149
rachitchauhan43 <rachitchauhan43@gmail.com>
2022-12-21 11:42:31 -0800

    Bumping up version to 0.10.0 for all the manifests. Signed-off-by: rachitchauhan43 <rachitchauhan43@gmail.com>
    Signed-off-by: rachitchauhan43 <rachitchauhan43@gmail.com>

--- hack/generate-install.sh
+++ hack/generate-install.sh
@@ -41,6 +41,7 @@ RELEASES=(
     "v0.9.0-rc0"
     "v0.9.0"
     "v0.10.0-rc0"
+    "v0.10.0"
 )
 
 TAG=$1
@@ -64,8 +65,8 @@ kustomize build config/runtimes | sed s/:latest/:$TAG/ >> $RUNTIMES_INSTALL_PATH
 yq -i 'select(.metadata.name == "inferenceservice-config").data.ingress |= (fromjson | .ingressGateway = "kubeflow/kubeflow-gateway" | tojson)' $KUBEFLOW_INSTALL_PATH
 
 # Copy CRD files to charts crds directory
-cp config/crd/serving.kserve.io_clusterservingruntimes.yaml charts/kserve/crds/serving.kserve.io_clusterservingruntimes.yaml
-cp config/crd/serving.kserve.io_inferenceservices.yaml charts/kserve/crds/serving.kserve.io_inferenceservices.yaml
-cp config/crd/serving.kserve.io_trainedmodels.yaml charts/kserve/crds/serving.kserve.io_trainedmodels.yaml
-cp config/crd/serving.kserve.io_inferencegraphs.yaml charts/kserve/crds/serving.kserve.io_inferencegraphs.yaml
-cp config/crd/serving.kserve.io_servingruntimes.yaml charts/kserve/crds/serving.kserve.io_servingruntimes.yaml
+cp config/crd/serving.kserve.io_clusterservingruntimes.yaml charts/kserve-crd/serving.kserve.io_clusterservingruntimes.yaml
+cp config/crd/serving.kserve.io_inferenceservices.yaml charts/kserve-crd/serving.kserve.io_inferenceservices.yaml
+cp config/crd/serving.kserve.io_trainedmodels.yaml charts/kserve-crd/serving.kserve.io_trainedmodels.yaml
+cp config/crd/serving.kserve.io_inferencegraphs.yaml charts/kserve-crd/serving.kserve.io_inferencegraphs.yaml
+cp config/crd/serving.kserve.io_servingruntimes.yaml charts/kserve-crd/serving.kserve.io_servingruntimes.yaml
