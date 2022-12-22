d35a1c14e33393455b5e04f03eae00951d3e31b8
Dan Sun <dsun20@bloomberg.net>
2022-12-21 22:14:31 -0500

    Add istio logs
    Signed-off-by: Dan Sun <dsun20@bloomberg.net>

--- test/scripts/gh-actions/status-check.sh
+++ test/scripts/gh-actions/status-check.sh
@@ -10,3 +10,5 @@ kubectl describe pods -n kserve-ci-e2e-test
 kubectl get events -n kserve-ci-e2e-test
 kubectl logs -l control-plane=kserve-controller-manager -n kserve -c manager
 kubectl logs -l 'component in (predictor, explainer, transformer)' -c kserve-container -n kserve-ci-e2e-test
+kubectl logs -l istio=ingressgateway -n istio-system
+
