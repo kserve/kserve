0c2517f9d1a873d2386c67717ac01d696eea610d
Dimitris Aragiorgis <dimara@arrikto.com>
2023-01-17 12:35:05 -0500

    Introduce pathTemplate in ingressConfig
    Extend the IngressConfig struct with a pathTemplate field to enable
    path-based routing. If this is set the user must also provide
    ingressDomain and urlScheme.
    
    Validate the given template just by trying to parse it. The user is
    responsible for providing a valid template that is unique for a given
    inference service, for example, by using both namespace and name in the
    template.
    
    Introduce a path module (similar to the existing domain) that has
    GenerateUrlPath() helper. This will will try to render the pathTemplate
    based on the name and namespace of an isvc.
    
    Add unittests for this new helper.
    
    Refs kserve/kserve#2257
    
    Signed-off-by: Dimitris Aragiorgis <dimara@arrikto.com>
    Signed-off-by: Kostis Andriopoulos <kandrio@arrikto.com>

---
+++ pkg/controller/v1beta1/inferenceservice/reconcilers/ingress/path.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2023 The KServe Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ingress
+
+import (
+	"bytes"
+	"fmt"
+	"text/template"
+
+	"net/url"
+
+	"github.com/kserve/kserve/pkg/apis/serving/v1beta1"
+)
+
+type PathTemplateValues struct {
+	Name          string
+	Namespace     string
+}
+
+// GenerateUrlPath generates the path using the pathTemplate configured in IngressConfig
+func GenerateUrlPath(name string, namespace string, ingressConfig *v1beta1.IngressConfig) (string, error) {
+	if ingressConfig.PathTemplate == "" {
+		return "", nil
+	}
+
+	values := PathTemplateValues{
+		Name:          name,
+		Namespace:     namespace,
+	}
+	tpl, err := template.New("url-template").Parse(ingressConfig.PathTemplate)
+	if err != nil {
+		return "", err
+	}
+
+	buf := bytes.Buffer{}
+	if err := tpl.Execute(&buf, values); err != nil {
+		return "", fmt.Errorf("error rendering the url template: %w", err)
+	}
+
+	// Validate generated URL. Use url.ParseRequestURI() instead of
+	// apis.ParseURL(). The latter calls url.Parse() which allows pretty much anything.
+	url, err := url.ParseRequestURI(buf.String())
+	if err != nil {
+		return "", fmt.Errorf("invalid url %q: %w", buf.String(), err)
+	}
+
+	if url.Scheme != "" || url.Host != "" {
+		return "", fmt.Errorf("invalid url path %q: contains either a scheme or a host", buf.String())
+	}
+
+	return url.Path, nil
+}
