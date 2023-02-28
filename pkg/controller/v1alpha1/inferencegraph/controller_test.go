866344c4cd30e2ab8d2ee070bdbbabb03be75392
rachitchauhan43 <rachitchauhan43@gmail.com>
2023-02-28 12:45:00 -0800

    Adding first test for ig controller.
    Signed-off-by: rachitchauhan43 <rachitchauhan43@gmail.com>

---
+++ pkg/controller/v1alpha1/inferencegraph/controller_test.go
@@ -0,0 +1,191 @@
+package inferencegraph
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/kserve/kserve/pkg/apis/serving/v1alpha1"
+	"github.com/kserve/kserve/pkg/constants"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"knative.dev/pkg/kmp"
+	"knative.dev/pkg/ptr"
+	knservingdefaults "knative.dev/serving/pkg/apis/config"
+	knservingv1 "knative.dev/serving/pkg/apis/serving/v1"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"time"
+)
+
+var _ = Describe("Inference Graph controller test", func() {
+
+	// Define utility constants for object names and testing timeouts/durations and intervals.
+	const (
+		timeout  = time.Second * 10
+		interval = time.Millisecond * 250
+		domain   = "example.com"
+	)
+
+	var (
+		configs = map[string]string{
+			"router": `{
+					  "image": "kserve/router:v0.10.0",
+					  "memoryRequest": "100Mi",
+					  "memoryLimit": "500Mi",
+					  "cpuRequest": "100m",
+					  "cpuLimit": "100m",
+					  "headers": {
+						"propagate": [
+						  "Authorization",
+						  "Intuit_tid"
+						]
+					  }
+				}`,
+		}
+	)
+
+	Context("When creating an inferencegraph with headers in global config", func() {
+		It("Should create a knative service with headers as env var of podspec", func() {
+			By("By creating a new InferenceGraph")
+			var configMap = &v1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      constants.InferenceServiceConfigMapName,
+					Namespace: constants.KServeNamespace,
+				},
+				Data: configs,
+			}
+			Expect(k8sClient.Create(context.TODO(), configMap)).NotTo(HaveOccurred())
+			defer k8sClient.Delete(context.TODO(), configMap)
+			graphName := "singlenode"
+			var expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: graphName, Namespace: "default"}}
+			var serviceKey = expectedRequest.NamespacedName
+			ctx := context.Background()
+			ig := &v1alpha1.InferenceGraph{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      serviceKey.Name,
+					Namespace: serviceKey.Namespace,
+					Annotations: map[string]string{
+						"serving.kserve.io/deploymentMode": string(constants.Serverless),
+					},
+				},
+				Spec: v1alpha1.InferenceGraphSpec{
+					Nodes: map[string]v1alpha1.InferenceRouter{
+						v1alpha1.GraphRootNodeName: {
+							RouterType: v1alpha1.Sequence,
+							Steps: []v1alpha1.InferenceStep{
+								{
+									InferenceTarget: v1alpha1.InferenceTarget{
+										ServiceURL: "http://someservice.exmaple.com",
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, ig)).Should(Succeed())
+			defer k8sClient.Delete(ctx, ig)
+			inferenceGraphSubmitted := &v1alpha1.InferenceGraph{}
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, serviceKey, inferenceGraphSubmitted)
+				if err != nil {
+					return false
+				}
+				return true
+			}, timeout, interval).Should(BeTrue())
+			fmt.Println(inferenceGraphSubmitted)
+
+			actualKnServiceCreated := &knservingv1.Service{}
+			Eventually(func() error {
+				return k8sClient.Get(context.TODO(), serviceKey, actualKnServiceCreated)
+			}, timeout).
+				Should(Succeed())
+			fmt.Println(inferenceGraphSubmitted)
+
+			printJsonAsString(actualKnServiceCreated)
+
+			expectedService := &knservingv1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      serviceKey.Name,
+					Namespace: serviceKey.Namespace,
+				},
+				Spec: knservingv1.ServiceSpec{
+					ConfigurationSpec: knservingv1.ConfigurationSpec{
+						Template: knservingv1.RevisionTemplateSpec{
+							ObjectMeta: metav1.ObjectMeta{
+								Labels: map[string]string{
+									"serving.kserve.io/inferencegraph": "singlenode",
+								},
+								Annotations: map[string]string{
+									"autoscaling.knative.dev/min-scale": "1",
+									"autoscaling.knative.dev/class":     "kpa.autoscaling.knative.dev",
+									"serving.kserve.io/deploymentMode":  "Serverless",
+								},
+							},
+							Spec: knservingv1.RevisionSpec{
+								ContainerConcurrency: ptr.Int64(knservingdefaults.DefaultContainerConcurrency),
+								TimeoutSeconds:       ptr.Int64(knservingdefaults.DefaultRevisionTimeoutSeconds),
+								PodSpec: v1.PodSpec{
+									Containers: []v1.Container{
+										{
+											Image: "kserve/router:v0.10.0",
+											Name:  knservingdefaults.DefaultUserContainerName,
+											Env: []v1.EnvVar{
+												{
+													Name:  "PROPAGATE_HEADERS",
+													Value: "Authorization,Intuit_tid",
+												},
+											},
+											Args: []string{
+												"--graph-json",
+												"{\"nodes\":{\"root\":{\"routerType\":\"Sequence\",\"steps\":[{\"serviceUrl\":\"http://someservice.exmaple.com\"}]}},\"resources\":{}}",
+											},
+											Resources: v1.ResourceRequirements{
+												Limits: v1.ResourceList{
+													v1.ResourceCPU:    resource.MustParse("100m"),
+													v1.ResourceMemory: resource.MustParse("500Mi"),
+												},
+												Requests: v1.ResourceList{
+													v1.ResourceCPU:    resource.MustParse("100m"),
+													v1.ResourceMemory: resource.MustParse("100Mi"),
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+			expectedService.SetDefaults(context.TODO())
+			printJsonAsString(expectedService)
+			fmt.Println(inferenceGraphSubmitted)
+			Expect(kmp.SafeDiff(actualKnServiceCreated.Spec, expectedService.Spec)).To(Equal(""))
+
+		})
+	})
+
+	//Context("When creating an IG with Serverless annotation", func() {
+	//	It("Should succeed")
+	//})
+	//
+	//Context("When creating an IG with RawDeployment annotation", func() {
+	//	It("Should not succeed")
+	//
+	//})
+
+})
+
+func printJsonAsString(x interface{}) {
+	marshelledKNSvc, err := json.Marshal(x)
+	if err == nil {
+		fmt.Println("json output is: ", string(marshelledKNSvc))
+	} else {
+		fmt.Println("Error marshalling")
+		fmt.Println(err)
+	}
+}
---
+++ pkg/controller/v1alpha1/inferencegraph/suite_test.go
@@ -0,0 +1,112 @@
+package inferencegraph
+
+import (
+	"context"
+	kfservingv1alpha1 "github.com/kserve/kserve/pkg/apis/serving/v1alpha1"
+	"github.com/kserve/kserve/pkg/apis/serving/v1beta1"
+	"github.com/kserve/kserve/pkg/constants"
+	//"github.com/kserve/kserve/pkg/controller/v1alpha1/inferencegraph"
+	pkgtest "github.com/kserve/kserve/pkg/testing"
+	v1 "k8s.io/api/core/v1"
+	netv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	knservingv1 "knative.dev/serving/pkg/apis/serving/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// These tests use Ginkgo (BDD-style Go testing framework). Refer to
+// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+
+var (
+	cfg       *rest.Config
+	k8sClient client.Client
+	testEnv   *envtest.Environment
+	cancel    context.CancelFunc
+	ctx       context.Context
+)
+
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "inferencegraph Controller Suite")
+}
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+	ctx, cancel = context.WithCancel(context.TODO())
+	By("bootstrapping test environment")
+	testEnv = pkgtest.SetupEnvTest()
+	cfg, err := testEnv.Start()
+	Expect(err).ToNot(HaveOccurred())
+	Expect(cfg).ToNot(BeNil())
+
+	err = kfservingv1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+	err = v1beta1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+	err = knservingv1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+	err = netv1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	Expect(err).ToNot(HaveOccurred())
+	Expect(k8sClient).ToNot(BeNil())
+
+	//Create namespace
+	kfservingNamespaceObj := &v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: constants.KServeNamespace,
+		},
+	}
+	Expect(k8sClient.Create(context.Background(), kfservingNamespaceObj)).Should(Succeed())
+
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme:             scheme.Scheme,
+		MetricsBindAddress: "0",
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	deployConfig := &v1beta1.DeployConfig{DefaultDeploymentMode: "Serverless"}
+	//ingressConfig := &v1beta1.IngressConfig{
+	//	IngressGateway:          constants.KnativeIngressGateway,
+	//	IngressServiceName:      "someIngressServiceName",
+	//	LocalGateway:            constants.KnativeLocalGateway,
+	//	LocalGatewayServiceName: "knative-local-gateway.istio-system.svc.cluster.local",
+	//	DisableIstioVirtualHost: false,
+	//}
+
+	err = (&InferenceGraphReconciler{
+		Client:   k8sClient,
+		Scheme:   k8sClient.Scheme(),
+		Log:      ctrl.Log.WithName("V1alpha1InferenceGraphController"),
+		Recorder: k8sManager.GetEventRecorderFor("V1alpha1InferenceGraphController"),
+	}).SetupWithManager(k8sManager, deployConfig)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		defer GinkgoRecover()
+		err = k8sManager.Start(ctx)
+		Expect(err).ToNot(HaveOccurred())
+	}()
+
+	k8sClient = k8sManager.GetClient()
+	Expect(k8sClient).ToNot(BeNil())
+
+})
+
+var _ = AfterSuite(func() {
+	cancel()
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).ToNot(HaveOccurred())
+})
