f777292c1cee7d074b21ed32617008c8dddc201c
rachitchauhan43 <rachitchauhan43@gmail.com>
2023-03-04 09:10:50 -0800

    addressing review comments
    Signed-off-by: rachitchauhan43 <rachitchauhan43@gmail.com>

--- pkg/controller/v1alpha1/inferencegraph/controller_test.go
+++ pkg/controller/v1alpha1/inferencegraph/controller_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2021 The KServe Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package inferencegraph
 
 import (
@@ -307,20 +323,7 @@ var _ = Describe("Inference Graph controller test", func() {
 						"serving.kserve.io/deploymentMode": string(constants.Serverless),
 					},
 				},
-				/*
-					affinity:
-					    podAntiAffinity:
-					      preferredDuringSchedulingIgnoredDuringExecution:
-					        - weight: 100
-					          podAffinityTerm:
-					            labelSelector:
-					              matchExpressions:
-					                - key: serving.kserve.io/inferencegraph
-					                  operator: In
-					                  values:
-					                    - usual-flow
-					            topologyKey: topology.kubernetes.io/zone
-				*/
+
 				Spec: v1alpha1.InferenceGraphSpec{
 					Affinity: &v1.Affinity{
 						PodAffinity: &v1.PodAffinity{
--- pkg/controller/v1alpha1/inferencegraph/suite_test.go
+++ pkg/controller/v1alpha1/inferencegraph/suite_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2021 The KServe Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package inferencegraph
 
 import (
@@ -5,7 +21,6 @@ import (
 	kfservingv1alpha1 "github.com/kserve/kserve/pkg/apis/serving/v1alpha1"
 	"github.com/kserve/kserve/pkg/apis/serving/v1beta1"
 	"github.com/kserve/kserve/pkg/constants"
-	//"github.com/kserve/kserve/pkg/controller/v1alpha1/inferencegraph"
 	pkgtest "github.com/kserve/kserve/pkg/testing"
 	v1 "k8s.io/api/core/v1"
 	netv1 "k8s.io/api/networking/v1"
