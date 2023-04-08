94052fd84afae9901177f7ce3d219f0de0696c3c
Lukas <41356653+Naegionn@users.noreply.github.com>
2023-04-08 03:50:49 +0200

    pass parameter  in rest v2 infer (#2797)
    * pass parameter  in rest v2 infer
    
    Signed-off-by: Naegionn <41356653+Naegionn@users.noreply.github.com>
    
    * Set parameter to empty dict if it is none
    
    Signed-off-by: Lukas <41356653+Naegionn@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Naegionn <41356653+Naegionn@users.noreply.github.com>
    Signed-off-by: Lukas <41356653+Naegionn@users.noreply.github.com>

--- python/kserve/kserve/protocol/rest/v2_endpoints.py
+++ python/kserve/kserve/protocol/rest/v2_endpoints.py
@@ -125,7 +125,9 @@ class V2Endpoints:
 
         request_headers = dict(raw_request.headers)
         infer_inputs = [InferInput(name=input.name, shape=input.shape, datatype=input.datatype,
-                                   data=input.data) for input in request_body.inputs]
+                                   data=input.data,
+                                   parameters={} if input.parameters is None else input.parameters
+                                   ) for input in request_body.inputs]
         infer_request = InferRequest(model_name=model_name, infer_inputs=infer_inputs)
         response, response_headers = await self.dataplane.infer(
             model_name=model_name, body=infer_request, headers=request_headers)
