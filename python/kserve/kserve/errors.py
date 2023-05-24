6e86376c1c0d29e78aa573a4587f8f6aec909d39
Dan Sun <dsun20@bloomberg.net>
2023-05-24 07:21:03 -0400

    Log exception stack
    Signed-off-by: Dan Sun <dsun20@bloomberg.net>

--- python/kserve/kserve/errors.py
+++ python/kserve/kserve/errors.py
@@ -11,8 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from http import HTTPStatus
 
+from http import HTTPStatus
+from kserve.logging import logger
 from fastapi.responses import JSONResponse
 
 
@@ -75,29 +76,36 @@ class ModelNotReady(RuntimeError):
 
 
 async def exception_handler(_, exc):
+    logger.error("Exception:", exc_info=exc)
     return JSONResponse(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, content={"error": str(exc)})
 
 
 async def invalid_input_handler(_, exc):
+    logger.error("Exception:", exc_info=exc)
     return JSONResponse(status_code=HTTPStatus.BAD_REQUEST, content={"error": str(exc)})
 
 
 async def inference_error_handler(_, exc):
+    logger.error("Exception:", exc_info=exc)
     return JSONResponse(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, content={"error": str(exc)})
 
 
 async def generic_exception_handler(_, exc):
+    logger.error("Exception:", exc_info=exc)
     return JSONResponse(status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
                         content={"error": f"{type(exc).__name__} : {str(exc)}"})
 
 
 async def model_not_found_handler(_, exc):
+    logger.error("Exception:", exc_info=exc)
     return JSONResponse(status_code=HTTPStatus.NOT_FOUND, content={"error": str(exc)})
 
 
 async def model_not_ready_handler(_, exc):
+    logger.error("Exception:", exc_info=exc)
     return JSONResponse(status_code=HTTPStatus.SERVICE_UNAVAILABLE, content={"error": str(exc)})
 
 
 async def not_implemented_error_handler(_, exc):
+    logger.error("Exception:", exc_info=exc)
     return JSONResponse(status_code=HTTPStatus.NOT_IMPLEMENTED, content={"error": str(exc)})
