632d6ee40a2901b15eda6c0f80e874aa2702179b
rachitchauhan43 <rachitchauhan43@gmail.com>
2023-07-19 11:06:59 -0700

    Removing unused code
    Signed-off-by: rachitchauhan43 <rachitchauhan43@gmail.com>

--- cmd/router/utils.go
+++ cmd/router/utils.go
@@ -10,8 +10,3 @@ type InferenceGraphRoutingError struct {
 func (e *InferenceGraphRoutingError) Error() string {
 	return fmt.Sprintf("%s. %s", e.ErrorMessage, e.Cause)
 }
-
-var (
-	RouteNotFoundInSwitchErrMsg = `{"err": "None of the routes matched with the switch condition", "cause":"None of the routes matched with the switch condition with the switch condition under node %v"}`
-	ErrorWhenCallingServiceUrl  = `{"err":"Could not call service url %v", "cause":"%v"}`
-)
