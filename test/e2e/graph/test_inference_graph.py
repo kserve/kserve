efa7d3b3bcd5c4251bf9219940287e0acaf8c005
rachitchauhan43 <rachitchauhan43@gmail.com>
2023-08-07 11:20:16 -0700

    Adding more e2e tests
    Signed-off-by: rachitchauhan43 <rachitchauhan43@gmail.com>

--- test/e2e/graph/test_inference_graph.py
+++ test/e2e/graph/test_inference_graph.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import yaml
+
 
 import pytest
 from requests.exceptions import HTTPError
@@ -16,7 +18,7 @@ from kserve import (
     V1alpha1InferenceStep,
     V1beta1XGBoostSpec,
 )
-from kubernetes import client
+from kubernetes import client, config
 from kubernetes.client import V1Container
 from kubernetes.client import V1ResourceRequirements
 
@@ -146,8 +148,12 @@ def construct_isvc_to_submit(service_name, image, model_name):
 @pytest.mark.kourier
 def test_ig_scenario1():
     """
-    Scenario: Sequence graph with 2 nodes: error_isvc (soft) -> success_isvc(soft)
-    Expectation: IG will return response of success_isvc
+    Scenario: Sequence graph with 2 steps that rae both soft dependencies.
+     success_isvc(soft) -> error_isvc (soft)
+
+    We are not marking steps as soft or hard explicitly so this will test that default behavior of steps being soft
+    is as expected.
+    Expectation: IG will return response of error_isvc and predict_ig will raise exception
     :return:
     """
 
@@ -214,3 +220,150 @@ def test_ig_scenario1():
     kserve_client.delete_inference_graph(graph_name, KSERVE_TEST_NAMESPACE)
     kserve_client.delete(success_isvc_name, KSERVE_TEST_NAMESPACE)
     kserve_client.delete(error_isvc_name, KSERVE_TEST_NAMESPACE)
+
+
+# @pytest.mark.rc4test
+@pytest.mark.graph
+@pytest.mark.kourier
+def test_ig_scenario2():
+    """
+    Scenario: Sequence graph with 2 steps that rae both soft dependencies.
+       error_isvc (soft) -> success_isvc(soft)
+
+    Expectation: IG will return response of success_isvc and predict_ig will not raise any exception
+    :return:
+    """
+
+    logging.info("starting test")
+    logging.info(f"SUCCESS_ISVC_IMAGE is {SUCCESS_ISVC_IMAGE}")
+    logging.info(f"ERROR_ISVC_IMAGE is {ERROR_ISVC_IMAGE}")
+
+    # Create success isvc
+    model_name = success_isvc_name = "success-200-isvc"
+    success_isvc = construct_isvc_to_submit(
+        success_isvc_name, image=SUCCESS_ISVC_IMAGE, model_name=model_name
+    )
+
+    # Create error isvc
+    model_name = error_isvc_name = "error-404-isvc"
+    error_isvc = construct_isvc_to_submit(
+        error_isvc_name, image=ERROR_ISVC_IMAGE, model_name=model_name
+    )
+
+    # Create graph
+    graph_name = "sequence-graph"
+
+    nodes = {
+        "root": V1alpha1InferenceRouter(
+            router_type="Sequence",
+            steps=[
+                V1alpha1InferenceStep(
+                    service_name=error_isvc_name,
+                ),
+                V1alpha1InferenceStep(
+                    service_name=success_isvc_name,
+                    data="$request",
+                ),
+            ],
+        )
+    }
+    graph_spec = V1alpha1InferenceGraphSpec(
+        nodes=nodes,
+    )
+    ig = V1alpha1InferenceGraph(
+        api_version=constants.KSERVE_V1ALPHA1,
+        kind=constants.KSERVE_KIND_INFERENCEGRAPH,
+        metadata=client.V1ObjectMeta(name=graph_name, namespace=KSERVE_TEST_NAMESPACE),
+        spec=graph_spec,
+    )
+
+    kserve_client = KServeClient(
+        config_file=os.environ.get("KUBECONFIG", "~/.kube/config")
+    )
+    kserve_client.create(success_isvc)
+    kserve_client.create(error_isvc)
+    kserve_client.create_inference_graph(ig)
+
+    kserve_client.wait_isvc_ready(success_isvc_name, namespace=KSERVE_TEST_NAMESPACE)
+    kserve_client.wait_isvc_ready(error_isvc_name, namespace=KSERVE_TEST_NAMESPACE)
+    kserve_client.wait_ig_ready(graph_name, namespace=KSERVE_TEST_NAMESPACE)
+
+
+    response = predict_ig(graph_name, "/Users/rchauhan4/Desktop/git-personal/rachitchauhan43-forks/kserve/test/e2e/data/iris_input.json")
+
+    assert response == {"message": "SUCCESS"}
+
+
+    kserve_client.delete_inference_graph(graph_name, KSERVE_TEST_NAMESPACE)
+    kserve_client.delete(success_isvc_name, KSERVE_TEST_NAMESPACE)
+    kserve_client.delete(error_isvc_name, KSERVE_TEST_NAMESPACE)
+
+
+def create_ig_using_custom_object_api(resource_body):
+    config.load_kube_config()
+    k8s_client_custom_object = client.CustomObjectsApi()
+    try:
+        resource = k8s_client_custom_object.create_namespaced_custom_object(
+            group="serving.kserve.io",
+            version="v1alpha1",
+            plural="inferencegraphs",
+            namespace="kserve-ci-e2e-test",
+            body=resource_body,
+        )
+    except Exception as e:
+        raise e
+
+@pytest.mark.rc4test
+@pytest.mark.graph
+@pytest.mark.kourier
+def test_ig_scenario3():
+    """
+     Scenario: Sequence graph with 2 steps - first is hard (and returns non-200) and second is soft dependency.
+     error_isvc(hard) -> success_isvc (soft)
+
+    Expectation: IG will return response of error_isvc and predict_ig will raise exception
+    """
+    logging.info("starting test")
+    logging.info(f"SUCCESS_ISVC_IMAGE is {SUCCESS_ISVC_IMAGE}")
+    logging.info(f"ERROR_ISVC_IMAGE is {ERROR_ISVC_IMAGE}")
+
+    # Create success isvc
+    model_name = success_isvc_name = "success-200-isvc"
+    success_isvc = construct_isvc_to_submit(
+        success_isvc_name, image=SUCCESS_ISVC_IMAGE, model_name=model_name
+    )
+
+    # Create error isvc
+    model_name = error_isvc_name = "error-404-isvc"
+    error_isvc = construct_isvc_to_submit(
+        error_isvc_name, image=ERROR_ISVC_IMAGE, model_name=model_name
+    )
+
+    kserve_client = KServeClient(
+        config_file=os.environ.get("KUBECONFIG", "~/.kube/config")
+    )
+    kserve_client.create(success_isvc)
+    kserve_client.create(error_isvc)
+
+    # Create graph
+    graph_name = "sequence-graph"
+    deployment_yaml_path = "graph/test-resources/ig_test_scenario_3.yaml"
+    # Read YAML file
+    with open(deployment_yaml_path, 'r') as stream:
+        resource_body = yaml.safe_load(stream)
+        create_ig_using_custom_object_api(resource_body)
+
+    kserve_client.wait_isvc_ready(success_isvc_name, namespace=KSERVE_TEST_NAMESPACE)
+    kserve_client.wait_isvc_ready(error_isvc_name, namespace=KSERVE_TEST_NAMESPACE)
+    kserve_client.wait_ig_ready(graph_name, namespace=KSERVE_TEST_NAMESPACE)
+
+    with pytest.raises(HTTPError) as exc_info:
+        predict_ig(graph_name, "./data/iris_input.json")
+
+    assert exc_info.value.response.json() == {"detail": "Intentional 404 code"}
+    assert exc_info.value.response.status_code == 404
+
+    kserve_client.delete_inference_graph(graph_name, KSERVE_TEST_NAMESPACE)
+    kserve_client.delete(success_isvc_name, KSERVE_TEST_NAMESPACE)
+    kserve_client.delete(error_isvc_name, KSERVE_TEST_NAMESPACE)
+
