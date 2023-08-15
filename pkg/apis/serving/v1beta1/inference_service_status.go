d7bfd224228f8c6888a410b6d1456a8de7d50817
Tessa Pham <hpham111@bloomberg.net>
2023-08-14 22:17:51 -0400

    document status conditions for RoutesReady and LatestDeploymentReady (#3069)
    * document new status conditions
    
    Signed-off-by: Tessa Pham <hpham111@bloomberg.net>
    
    * make generate
    
    Signed-off-by: Tessa Pham <hpham111@bloomberg.net>
    
    * fix linting
    
    Signed-off-by: Tessa Pham <hpham111@bloomberg.net>
    
    * remove requirements.txt in python
    
    Signed-off-by: Tessa Pham <hpham111@bloomberg.net>
    
    * comment out unnecessary git checkout lines
    
    Signed-off-by: Tessa Pham <hpham111@bloomberg.net>
    
    * indicate conditions for serverless mode only
    
    Signed-off-by: Tessa Pham <hpham111@bloomberg.net>
    
    * go mod tidy
    
    Signed-off-by: Tessa Pham <hpham111@bloomberg.net>
    
    ---------
    
    Signed-off-by: Tessa Pham <hpham111@bloomberg.net>

--- pkg/apis/serving/v1beta1/inference_service_status.go
+++ pkg/apis/serving/v1beta1/inference_service_status.go
@@ -34,7 +34,8 @@ type InferenceServiceStatus struct {
 	// - PredictorReady: predictor readiness condition; <br/>
 	// - TransformerReady: transformer readiness condition; <br/>
 	// - ExplainerReady: explainer readiness condition; <br/>
-	// - RoutesReady: aggregated routing condition; <br/>
+	// - RoutesReady (serverless mode only): aggregated routing condition, i.e. endpoint readiness condition; <br/>
+	// - LatestDeploymentReady (serverless mode only): aggregated configuration condition, i.e. latest deployment readiness condition; <br/>
 	// - Ready: aggregated condition; <br/>
 	duckv1.Status `json:",inline"`
 	// Addressable endpoint for the InferenceService
