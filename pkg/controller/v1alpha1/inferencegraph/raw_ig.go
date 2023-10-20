29fe21a88c69011756838f6365dbf63748cfee8c
Tanvi Thakur <tanvi_thakur@intuit.com>
2023-10-19 22:41:10 -0700

    Fix logging message
    Signed-off-by: Mopuri, Bharath <bharath_mopuri@intuit.com>
    Signed-off-by: Tanvi Thakur <tanvi_thakur@intuit.com>

--- pkg/controller/v1alpha1/inferencegraph/raw_ig.go
+++ pkg/controller/v1alpha1/inferencegraph/raw_ig.go
@@ -19,7 +19,7 @@ import (
 	"strings"
 )
 
-var logger = logf.Log.WithName("RawIG")
+var logger = logf.Log.WithName("Inference Graph RAWDeploymentMode")
 
 func createInferenceGraphPodSpec(graph *v1alpha1api.InferenceGraph, config *RouterConfig) *v1.PodSpec {
 	bytes, err := json.Marshal(graph.Spec)
