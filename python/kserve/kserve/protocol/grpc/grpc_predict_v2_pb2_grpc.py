347b152b58bd19377ae5cda45dcc6cc85013c4ef
Lasse <lasse.reedtz@visma.com>
2023-11-10 09:49:38 +0100

    import explicit from kserve
    Signed-off-by: Lasse <lasse.reedtz@visma.com>

--- python/kserve/kserve/protocol/grpc/grpc_predict_v2_pb2_grpc.py
+++ python/kserve/kserve/protocol/grpc/grpc_predict_v2_pb2_grpc.py
@@ -16,8 +16,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import grpc_predict_v2_pb2 as grpc__predict__v2__pb2
-
+import kserve.protocol.grpc.grpc_predict_v2_pb2 as grpc__predict__v2__pb2
 
 class GRPCInferenceServiceStub(object):
     """Inference Server GRPC endpoints.
