03ebc47f9751945f87c671a49634b144357a3e08
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2023-11-21 16:10:22 +0530

    -default removed from batcher e2e test.
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- test/e2e/batcher/test_batcher.py
+++ test/e2e/batcher/test_batcher.py
@@ -118,7 +118,7 @@ def test_batcher_v2():
     except RuntimeError as e:
         print(kserve_client.api_instance.get_namespaced_custom_object("serving.knative.dev", "v1",
                                                                       KSERVE_TEST_NAMESPACE,
-                                                                      "services", service_name + "-predictor-default"))
+                                                                      "services", service_name + "-predictor"))
         pods = kserve_client.core_api.list_namespaced_pod(KSERVE_TEST_NAMESPACE,
                                                           label_selector='serving.kserve.io/inferenceservice={}'.
                                                           format(service_name))
@@ -182,7 +182,7 @@ def test_torchserve_v2_kserve():
     except RuntimeError as e:
         print(kserve_client.api_instance.get_namespaced_custom_object("serving.knative.dev", "v1",
                                                                       KSERVE_TEST_NAMESPACE,
-                                                                      "services", service_name + "-predictor-default"))
+                                                                      "services", service_name + "-predictor"))
         pods = kserve_client.core_api.list_namespaced_pod(KSERVE_TEST_NAMESPACE,
                                                           label_selector='serving.kserve.io/inferenceservice={}'.
                                                           format(service_name))
