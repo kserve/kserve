03b2f4a35ad622ce55a11dab39194c125af02a29
Tanvi Thakur <tanvi_thakur@intuit.com>
2023-12-04 09:41:46 -0800

    e2e test for ISVC in raw mode
    Signed-off-by: Mopuri, Bharath <bharath_mopuri@intuit.com>

--- test/e2e/graph/test_inference_graph.py
+++ test/e2e/graph/test_inference_graph.py
@@ -862,3 +862,96 @@ def test_ig_scenario10():
     kserve_client.delete_inference_graph(graph_name, KSERVE_TEST_NAMESPACE)
     kserve_client.delete(success_isvc_name, KSERVE_TEST_NAMESPACE)
     kserve_client.delete(error_isvc_name, KSERVE_TEST_NAMESPACE)
+
+
+@pytest.mark.graph
+@pytest.mark.kourier
+def test_inference_graph_raw_mode():
+    logging.info("Starting test test_inference_graph_raw_mode")
+    sklearn_name = "isvc-sklearn-graph"
+    xgb_name = "isvc-xgboost-graph"
+    graph_name = "model-chainer"
+
+    annotations = dict()
+    annotations['serving.kserve.io/deploymentMode'] = 'RawDeployment'
+
+    sklearn_predictor = V1beta1PredictorSpec(
+        min_replicas=1,
+        sklearn=V1beta1SKLearnSpec(
+            storage_uri="gs://kfserving-examples/models/sklearn/1.0/model",
+            resources=V1ResourceRequirements(
+                requests={"cpu": "50m", "memory": "128Mi"},
+                limits={"cpu": "100m", "memory": "256Mi"},
+            ),
+        ),
+    )
+    sklearn_isvc = V1beta1InferenceService(
+        api_version=constants.KSERVE_V1BETA1,
+        kind=constants.KSERVE_KIND,
+        metadata=client.V1ObjectMeta(
+            name=sklearn_name, namespace=KSERVE_TEST_NAMESPACE,annotations=annotations,
+        ),
+        spec=V1beta1InferenceServiceSpec(predictor=sklearn_predictor),
+    )
+
+    xgb_predictor = V1beta1PredictorSpec(
+        min_replicas=1,
+        xgboost=V1beta1XGBoostSpec(
+            storage_uri="gs://kfserving-examples/models/xgboost/1.5/model",
+            resources=V1ResourceRequirements(
+                requests={"cpu": "50m", "memory": "128Mi"},
+                limits={"cpu": "100m", "memory": "256Mi"},
+            ),
+        ),
+    )
+    xgb_isvc = V1beta1InferenceService(
+        api_version=constants.KSERVE_V1BETA1,
+        kind=constants.KSERVE_KIND,
+        metadata=client.V1ObjectMeta(name=xgb_name, namespace=KSERVE_TEST_NAMESPACE,annotations=annotations,),
+        spec=V1beta1InferenceServiceSpec(predictor=xgb_predictor),
+    )
+
+    nodes = {
+        "root": V1alpha1InferenceRouter(
+            router_type="Sequence",
+            steps=[
+                V1alpha1InferenceStep(
+                    service_name=sklearn_name,
+                ),
+                V1alpha1InferenceStep(
+                    service_name=xgb_name,
+                    data="$request",
+                ),
+            ],
+        )
+    }
+    graph_spec = V1alpha1InferenceGraphSpec(
+        nodes=nodes,
+    )
+    ig = V1alpha1InferenceGraph(
+        api_version=constants.KSERVE_V1ALPHA1,
+        kind=constants.KSERVE_KIND_INFERENCEGRAPH,
+        metadata=client.V1ObjectMeta(name=graph_name, namespace=KSERVE_TEST_NAMESPACE),
+        spec=graph_spec,
+    )
+
+    kserve_client = KServeClient(
+        config_file=os.environ.get("KUBECONFIG", "~/.kube/config")
+    )
+    kserve_client.create(sklearn_isvc)
+    kserve_client.create(xgb_isvc)
+    kserve_client.create_inference_graph(ig)
+
+    kserve_client.wait_isvc_ready(sklearn_name, namespace=KSERVE_TEST_NAMESPACE)
+    kserve_client.wait_isvc_ready(xgb_name, namespace=KSERVE_TEST_NAMESPACE)
+    kserve_client.wait_ig_ready(graph_name, namespace=KSERVE_TEST_NAMESPACE)
+
+    res = predict_ig(
+        graph_name,
+        os.path.join(IG_TEST_RESOURCES_BASE_LOCATION, "iris_input.json"),
+    )
+    assert res["predictions"] == [1, 1]
+
+    kserve_client.delete_inference_graph(graph_name, KSERVE_TEST_NAMESPACE)
+    kserve_client.delete(sklearn_name, KSERVE_TEST_NAMESPACE)
+    kserve_client.delete(xgb_name, KSERVE_TEST_NAMESPACE)
