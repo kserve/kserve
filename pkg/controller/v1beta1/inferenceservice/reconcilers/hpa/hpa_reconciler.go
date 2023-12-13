9b0e19a6e8e2f25eca0f8d29dd5ceec599c391b1
Yuan Tang <terrytangyuan@gmail.com>
2023-12-13 15:51:59 -0500

    Update pkg/controller/v1beta1/inferenceservice/reconcilers/hpa/hpa_reconciler.go
    Co-authored-by: Jooho Lee <ljhiyh@gmail.com>
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- pkg/controller/v1beta1/inferenceservice/reconcilers/hpa/hpa_reconciler.go
+++ pkg/controller/v1beta1/inferenceservice/reconcilers/hpa/hpa_reconciler.go
@@ -145,6 +145,7 @@ func (r *HPAReconciler) checkHPAExist(client client.Client) (constants.CheckResu
 
 func semanticHPAEquals(desired, existing *autoscalingv2.HorizontalPodAutoscaler) bool {
 	return equality.Semantic.DeepEqual(desired.Spec.ScaleTargetRef, existing.Spec.ScaleTargetRef) &&
+	equality.Semantic.DeepEqual(desired.Spec.Behavior, existing.Spec.Behavior) &&
 		equality.Semantic.DeepEqual(desired.Spec.Metrics, existing.Spec.Metrics) &&
 		equality.Semantic.DeepEqual(desired.Spec.MaxReplicas, existing.Spec.MaxReplicas) &&
 		equality.Semantic.DeepEqual(*desired.Spec.MinReplicas, *existing.Spec.MinReplicas)
