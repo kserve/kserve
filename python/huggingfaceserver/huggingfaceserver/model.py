93e2de3fe23adce40e857865067cc3ddae3770d0
Dan Sun <dsun20@bloomberg.net>
2023-12-28 18:26:10 -0500

    Add postprocess
    Signed-off-by: Dan Sun <dsun20@bloomberg.net>

--- python/huggingfaceserver/huggingfaceserver/model.py
+++ python/huggingfaceserver/huggingfaceserver/model.py
@@ -11,6 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import json
+import os
+
 from torch import Tensor
 
 from kserve.logging import logger
@@ -46,26 +50,14 @@ ARCHITECTURES_2_TASK = {
     "BloomModel": "text-generation",
 }
 
-PEFT_MODEL_TASK_TO_CLS = {
-    "SEQ_CLS": AutoModelForSequenceClassification,
-    "SEQ_2_SEQ_LM": AutoModelForSeq2SeqLM,
-    "CAUSAL_LM": AutoModelForCausalLM,
-    "TOKEN_CLS": AutoModelForTokenClassification,
-    "QUESTION_ANS": AutoModelForQuestionAnswering,
-}
-
 
 class HuggingfaceModel(Model):  # pylint:disable=c-extension-no-member
     def __init__(self, model_name, **kwargs):
-        self.hf_pipeline = None
         super().__init__(model_name)
         self.kwargs = {}
         tp_degree = kwargs.get('tensor_parallel_degree', -1)
-        self.device_id = kwargs.get('device_id')
         self.device = torch.device(
-            "cuda:" + str(self.device_id)
-            if torch.cuda.is_available() and self.device_id is not None
-            else "cpu"
+            "cuda" if torch.cuda.is_available() else "cpu"
         )
         if "device_map" in kwargs:
             self.kwargs["device_map"] = kwargs['device_map']
@@ -75,7 +67,6 @@ class HuggingfaceModel(Model):  # pylint:disable=c-extension-no-member
             assert world_size == tp_degree, f"TP degree ({tp_degree}) doesn't match available GPUs ({world_size})"
         if "low_cpu_mem_usage" in kwargs:
             kwargs["low_cpu_mem_usage"] = kwargs.get("low_cpu_mem_usage")
-        self.model_id = kwargs['model_id']
         self.model_dir = kwargs['model_dir']
         self.do_lower_case = kwargs['do_lower_case']
         self.max_length = kwargs['max_length']
@@ -83,6 +74,7 @@ class HuggingfaceModel(Model):  # pylint:disable=c-extension-no-member
         self.task = kwargs['task']
         self.tokenizer = None
         self.model = None
+        self.mapping = None
         self.ready = False
 
     @staticmethod
@@ -102,12 +94,18 @@ class HuggingfaceModel(Model):  # pylint:disable=c-extension-no-member
         return task
 
     def load(self) -> bool:
-        model_id_or_path = None
-        if self.model_id:
-            model_id_or_path = self.model_id
-
+        model_id_or_path = self.name
         if self.model_dir:
             model_id_or_path = pathlib.Path(Storage.download(self.model_dir))
+            # Read the mapping file, index to object name
+            mapping_file_path = os.path.join(self.model_dir, "index_to_name.json")
+            # Question answering does not need the index_to_name.json file.
+            if self.task == "question_answering" or self.task == "text_generation":
+                if os.path.isfile(mapping_file_path):
+                    with open(mapping_file_path) as f:
+                        self.mapping = json.load(f)
+                else:
+                    logger.warning("Missing the index_to_name.json file.")
         task = self.task
         if not task:
             task = self.infer_task_from_model_architecture(model_id_or_path)
@@ -131,37 +129,12 @@ class HuggingfaceModel(Model):  # pylint:disable=c-extension-no-member
                     f"Unsupported task {task}. Please check the supported`task` option."
                 )
             self.model.eval()
-        logger.info("Transformer model from path %s loaded successfully", model_id_or_path)
+            logger.info("Transformer model from path %s loaded successfully", model_id_or_path)
         self.ready = True
         return self.ready
 
-    @staticmethod
-    def wrap_text_generation_pipeline(hf_pipeline):
-
-        def wrapped_pipeline(inputs, *args, **kwargs):
-            model = hf_pipeline.model
-            tokenizer = hf_pipeline.tokenizer
-            if torch.cuda.is_available():
-                input_tokens = tokenizer(inputs, padding=True,
-                                         return_tensors="pt").to(torch.cuda.current_device())
-            else:
-                input_tokens = tokenizer(inputs, padding=True,
-                                         return_tensors="pt").to("cpu")
-            with torch.no_grad():
-                output_tokens = model.generate(
-                    *args,
-                    input_ids=input_tokens.input_ids,
-                    attention_mask=input_tokens.attention_mask,
-                    **kwargs)
-            generated_text = tokenizer.batch_decode(output_tokens,
-                                                    skip_special_tokens=True)
-
-            return [{"generated_text": s} for s in generated_text]
-
-        return wrapped_pipeline
-
-    def preprocess(self, payload: InferRequest, headers: Dict[str, str] = None) -> Union[
-        (Tensor, Tensor), InferRequest]:
+    def preprocess(self, payload: InferRequest, headers: Dict[str, str] = None) -> \
+            Union[(Tensor, Tensor), InferRequest]:
         text_inputs = get_predict_input(payload)
         input_ids_batch = None
         attention_mask_batch = None
@@ -192,58 +165,46 @@ class HuggingfaceModel(Model):  # pylint:disable=c-extension-no-member
         else:
             return input_ids_batch, attention_mask_batch
 
-    def predict(self, input_batch: Union[(Tensor, Tensor), InferRequest], headers: Dict[str, str] = None) -> \
+    async def predict(self, input_batch: Union[(Tensor, Tensor), InferRequest], headers: Dict[str, str] = None) -> \
             Union[Dict, InferResponse]:
-        inferences = []
         if self.predictor_host:
-            inferences = super().predict(input_batch, headers)
+            return await super().predict(input_batch, headers)
         else:
             input_ids_batch, attention_mask_batch = input_batch
+            outputs = None
             try:
-                if self.task == "sequence_classification":
-                    predictions = self.model(input_ids_batch, attention_mask_batch)
-                    num_rows, num_cols = predictions[0].shape
-                    for i in range(num_rows):
-                        out = predictions[0][i].unsqueeze(0)
-                        y_hat = out.argmax(1).item()
-                        predicted_idx = str(y_hat)
-                        inferences.append(self.mapping[predicted_idx])
-                elif self.task == "token_classification":
+                if self.task == "sequence-classification":
+                    outputs = self.model(input_ids_batch, attention_mask_batch)
+                elif self.task == "token-classification":
                     outputs = self.model(input_ids_batch, attention_mask_batch)[0]
                     print(
-                        "This the output size from the token classification model",
-                        outputs.size(),
+                        "This is the output size from the token classification model", outputs.size(),
                     )
-                    print("This the output from the token classification model", outputs)
-                    num_rows = outputs.shape[0]
-                    for i in range(num_rows):
-                        output = outputs[i].unsqueeze(0)
-                        predictions = torch.argmax(output, dim=2)
-                        tokens = self.tokenizer.tokenize(
-                            self.tokenizer.decode(input_ids_batch[i])
-                        )
-                        if self.mapping:
-                            label_list = self.mapping["label_list"]
-                        label_list = label_list.strip("][").split(", ")
-                        prediction = [
-                            (token, label_list[prediction])
-                            for token, prediction in zip(tokens, predictions[0].tolist())
-                        ]
-                        inferences.append(prediction)
-                elif self.task == "text_generation":
-                    if self.setup_config["model_parallel"]:
-                        # Need to move the first device, as the transformer model has been placed there
-                        # https://github.com/huggingface/transformers/blob/v4.17.0/src/transformers/models/gpt2/modeling_gpt2.py#L970
-                        input_ids_batch = input_ids_batch.to("cuda:0")
+                    print("This is the output from the token classification model", outputs)
+                elif self.task == "text-generation":
                     outputs = self.model.generate(
-                        input_ids_batch, max_length=50, do_sample=True, top_p=0.95, top_k=60
-                    )
-                    for i, x in enumerate(outputs):
-                        inferences.append(
-                            self.tokenizer.decode(outputs[i], skip_special_tokens=True)
-                        )
+                        input_ids_batch, attention_mask_batch)
 
-                    logger.info("Generated text: '%s'", inferences)
+                    logger.info("Generated outputs: '%s'", outputs)
             except Exception as e:
                 raise InferenceError(str(e))
+            return outputs
+
+    def postprocess(self, outputs, headers: Dict[str, str] = None) -> Union[Dict, InferResponse]:
+        inferences = []
+        if self.task == "sequence-classification":
+            num_rows, num_cols = outputs[0].shape
+            for i in range(num_rows):
+                out = outputs[0][i].unsqueeze(0)
+                y_hat = out.argmax(1).item()
+                predicted_idx = str(y_hat)
+                inferences.append(self.mapping[predicted_idx])
+        elif self.task == "token-classification":
+            num_rows = outputs.shape[0]
+            for i in range(num_rows):
+                output = outputs[i].unsqueeze(0)
+                predictions = torch.argmax(output, dim=2)
+                inferences.append(predictions)
+        elif self.task == "text-generation":
+            inferences = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
         return inferences
