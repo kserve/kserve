d971ebd9181754e06bfbf949e40fb141e7da6c07
HAO <howard31124@gmail.com>
2023-12-28 17:03:27 +0800

    check empty model test.
    Co-authored-by: koshino17 <by900813@gmail.com>
    Signed-off-by: HAO <howard31124@gmail.com>

--- python/kserve/kserve/model_server.py
+++ python/kserve/kserve/model_server.py
@@ -127,10 +127,10 @@ class ModelServer:
         if isinstance(models, list):
             for model in models:
                 if isinstance(model, Model):
-                    if model.ready:
-                        self.register_model(model)
-                        # pass whether to log request latency into the model
-                        model.enable_latency_logging = self.enable_latency_logging
+                    print(model.name,"ready status is",model.ready)
+                    self.register_model(model)
+                    # pass whether to log request latency into the model
+                    model.enable_latency_logging = self.enable_latency_logging
                 else:
                     raise RuntimeError("Model type should be 'Model'")
         elif isinstance(models, dict):
