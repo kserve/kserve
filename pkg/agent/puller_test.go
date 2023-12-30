39030fd17e0092a50ad5d56dacbb8c5d50050603
zhaque44 <haque.zubair@gmail.com>
2023-12-30 15:25:31 -0600

    updated to table driven tests
    Signed-off-by: zhaque44 <haque.zubair@gmail.com>

--- pkg/agent/puller_test.go
+++ pkg/agent/puller_test.go
@@ -8,87 +8,91 @@ import (
 	"go.uber.org/zap"
 )
 
-func TestEmptyCommands(t *testing.T) {
-	logger := zap.NewNop().Sugar()
-	downloader := &Downloader{}
-	puller := Puller{
-		channelMap:  make(map[string]*ModelChannel),
-		completions: make(chan *ModelOp, 4),
-		opStats:     make(map[string]map[OpType]int),
-		waitGroup:   WaitGroupWrapper{sync.WaitGroup{}},
-		Downloader:  downloader,
-		logger:      logger,
+func TestProcessCommands(t *testing.T) {
+	testCases := []struct {
+		name     string
+		commands []ModelOp
+	}{
+		{
+			name:     "EmptyCommands",
+			commands: []ModelOp{},
+		},
+		{
+			name: "InvalidModelSpecification",
+			commands: []ModelOp{
+				{ModelName: "invalidModel", Op: Add, Spec: nil},
+			},
+		},
 	}
 
-	// Create a test command channel with no commands
-	commands := make(chan ModelOp)
-	close(commands)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			logger := zap.NewNop().Sugar()
+			downloader := &Downloader{}
+			puller := Puller{
+				channelMap:  make(map[string]*ModelChannel),
+				completions: make(chan *ModelOp, 4),
+				opStats:     make(map[string]map[OpType]int),
+				waitGroup:   WaitGroupWrapper{sync.WaitGroup{}},
+				Downloader:  downloader,
+				logger:      logger,
+			}
 
-	go puller.processCommands(commands)
-	puller.waitGroup.wg.Wait()
+			commands := make(chan ModelOp, len(tc.commands))
+			for _, cmd := range tc.commands {
+				commands <- cmd
+			}
+			close(commands)
 
-	assert.Empty(t, puller.opStats)
+			go puller.processCommands(commands)
+			puller.waitGroup.wg.Wait()
+
+			assert.Empty(t, puller.opStats)
+		})
+	}
 }
 
-func TestInvalidModelSpecification(t *testing.T) {
-	logger := zap.NewNop().Sugar()
-	downloader := &Downloader{}
-	puller := Puller{
-		channelMap:  make(map[string]*ModelChannel),
-		completions: make(chan *ModelOp, 4),
-		opStats:     make(map[string]map[OpType]int),
-		waitGroup:   WaitGroupWrapper{sync.WaitGroup{}},
-		Downloader:  downloader,
-		logger:      logger,
+func TestStartPullerAndProcessModels(t *testing.T) {
+	testCases := []struct {
+		name       string
+		downloader *Downloader
+		commands   chan ModelOp
+		logger     *zap.SugaredLogger
+	}{
+		{
+			name:       "ValidInputs",
+			downloader: &Downloader{},
+			commands:   make(chan ModelOp),
+			logger:     zap.NewNop().Sugar(),
+		},
+		{
+			name:       "NilDownloader",
+			downloader: nil,
+			commands:   make(chan ModelOp),
+			logger:     zap.NewNop().Sugar(),
+		},
+		{
+			name:       "NilCommands",
+			downloader: &Downloader{},
+			commands:   nil,
+			logger:     zap.NewNop().Sugar(),
+		},
+		{
+			name:       "NilLogger",
+			downloader: &Downloader{},
+			commands:   make(chan ModelOp),
+			logger:     nil,
+		},
 	}
 
-	// Create a test command channel with an invalid model specification
-	commands := make(chan ModelOp, 1)
-	commands <- ModelOp{ModelName: "invalidModel", Op: Add, Spec: nil}
-	close(commands)
-
-	go puller.processCommands(commands)
-	puller.waitGroup.wg.Wait()
-
-	assert.Empty(t, puller.opStats)
-}
-
-func TestStartPullerAndProcessModels_ValidInputs(t *testing.T) {
-	logger := zap.NewNop().Sugar()
-	downloader := &Downloader{}
-	commands := make(chan ModelOp)
-	defer close(commands)
-
-	assert.NotPanics(t, func() {
-		StartPullerAndProcessModels(downloader, commands, logger)
-	})
-}
-
-func TestStartPullerAndProcessModels_NilDownloader(t *testing.T) {
-	logger := zap.NewNop().Sugar()
-	commands := make(chan ModelOp)
-	defer close(commands)
-
-	assert.NotPanics(t, func() {
-		StartPullerAndProcessModels(nil, commands, logger)
-	})
-}
-
-func TestStartPullerAndProcessModels_NilCommands(t *testing.T) {
-	logger := zap.NewNop().Sugar()
-	downloader := &Downloader{}
-
-	assert.NotPanics(t, func() {
-		StartPullerAndProcessModels(downloader, nil, logger)
-	})
-}
-
-func TestStartPullerAndProcessModels_NilLogger(t *testing.T) {
-	downloader := &Downloader{}
-	commands := make(chan ModelOp)
-	defer close(commands)
-
-	assert.NotPanics(t, func() {
-		StartPullerAndProcessModels(downloader, commands, nil)
-	})
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.commands != nil {
+				defer close(tc.commands)
+			}
+			assert.NotPanics(t, func() {
+				StartPullerAndProcessModels(tc.downloader, tc.commands, tc.logger)
+			})
+		})
+	}
 }
