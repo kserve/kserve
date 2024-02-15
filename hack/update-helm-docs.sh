abe23045c7b27bf9e4800deb72d0972b8584b7b3
Yuan Tang <terrytangyuan@gmail.com>
2024-02-14 21:27:46 -0500

    fix syntax
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- hack/update-helm-docs.sh
+++ hack/update-helm-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing pre-commit"
-pip install pre-commit@3.6.1
+pip install pre-commit==3.6.1
 
 echo "Installing helm-docs"
 go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.12.0
