7b14a41d29fb4a7fd05a5defe3bc1a95a4755a88
Yuan Tang <terrytangyuan@gmail.com>
2024-02-14 21:19:22 -0500

    pin versions
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- hack/update-helm-docs.sh
+++ hack/update-helm-docs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -e
 
 echo "Installing pre-commit"
-pip install pre-commit
+pip install pre-commit@3.6.1
 
 echo "Installing helm-docs"
-go install github.com/norwoodj/helm-docs/cmd/helm-docs@latest
+go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.12.0
 
 pre-commit install; pre-commit run --all
