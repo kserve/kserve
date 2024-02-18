47ea215a23289448a29d808ab861bb15e8cd8c4e
Yuan (Terry) Tang <terrytangyuan@gmail.com>
2024-02-18 00:14:14 -0500

    Update update-helm-docs.sh
    Signed-off-by: Yuan (Terry) Tang <terrytangyuan@gmail.com>

--- hack/update-helm-docs.sh
+++ hack/update-helm-docs.sh
@@ -2,10 +2,7 @@
 
 set +e
 
-echo "Installing pre-commit"
-pip install pre-commit==3.6.1
-
 echo "Installing helm-docs"
 go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.12.0
 
-pre-commit install; pre-commit run --all
+helm-docs --chart-search-root=charts --output-file=README.md
