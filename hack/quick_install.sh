e2a9109517039ac4b21d3f5f8ef8d76167369686
Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>
2024-02-20 18:34:06 +0530

    Publish v0.12.0 release
    Signed-off-by: Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>

--- hack/quick_install.sh
+++ hack/quick_install.sh
@@ -34,7 +34,7 @@ export ISTIO_VERSION=1.19.4
 export ISTIO_DIR=istio-${ISTIO_VERSION}
 export KNATIVE_SERVING_VERSION=knative-v1.10.1
 export KNATIVE_ISTIO_VERSION=knative-v1.10.0
-export KSERVE_VERSION=v0.12.0-rc1
+export KSERVE_VERSION=v0.12.0
 export CERT_MANAGER_VERSION=v1.3.0
 export SCRIPT_DIR="$( dirname -- "${BASH_SOURCE[0]}" )"
 
