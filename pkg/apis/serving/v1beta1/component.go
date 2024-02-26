e3ef0be8e460fa7796a53990ab74e02ee89a5b1d
Yuan Tang <terrytangyuan@gmail.com>
2024-02-26 10:34:10 -0500

    goimports
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- pkg/apis/serving/v1beta1/component.go
+++ pkg/apis/serving/v1beta1/component.go
@@ -18,12 +18,12 @@ package v1beta1
 
 import (
 	"fmt"
-	appsv1 "k8s.io/api/apps/v1"
 	"reflect"
 	"strings"
 
 	"github.com/kserve/kserve/pkg/constants"
 	"github.com/kserve/kserve/pkg/utils"
+	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
