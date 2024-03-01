e20389d7760ab66a52d3238cea63a49a8231afce
Yuan Tang <terrytangyuan@gmail.com>
2024-02-29 20:59:16 -0500

    test
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

---
+++ hack/crds.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+
+	"sigs.k8s.io/yaml"
+)
+
+func removeCRDValidation(filename string) {
+	data, err := os.ReadFile(filepath.Clean(filename))
+	if err != nil {
+		panic(err)
+	}
+	crd := make(obj)
+	err = yaml.Unmarshal(data, &crd)
+	if err != nil {
+		panic(err)
+	}
+	spec := crd["spec"].(obj)
+	versions := spec["versions"].([]interface{})
+	version := versions[0].(obj)
+	properties := version["schema"].(obj)["openAPIV3Schema"].(obj)["properties"].(obj)
+	for k := range properties {
+		if k == "spec" || k == "status" {
+			properties[k] = obj{"type": "object", "x-kubernetes-preserve-unknown-fields": true, "x-kubernetes-map-type": "atomic"}
+		}
+	}
+	data, err = yaml.Marshal(crd)
+	if err != nil {
+		panic(err)
+	}
+	err = os.WriteFile(filename, data, 0o600)
+	if err != nil {
+		panic(err)
+	}
+}
