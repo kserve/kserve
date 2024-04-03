4b022d1932edfc73142e6db72198f13ee27b2a75
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2024-04-03 18:13:32 +0530

    black formatter
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- python/kserve/kserve/storage/test/test_storage.py
+++ python/kserve/kserve/storage/test/test_storage.py
@@ -302,21 +302,26 @@ def test_unpack_zip_file():
     Path(tar_file).write_bytes(FILE_ZIP_RAW)
     mimetype, _ = mimetypes.guess_type(tar_file)
     Storage._unpack_archive_file(tar_file, mimetype, out_dir)
-    assert os.path.exists(os.path.join(out_dir, 'model.pth'))
-    os.remove(os.path.join(out_dir, 'model.pth'))
+    assert os.path.exists(os.path.join(out_dir, "model.pth"))
+    os.remove(os.path.join(out_dir, "model.pth"))
 
 
 def test_download_hf(mocker):
     uri = "hf://example.com/model:hash_value"
-    
+
     mock_tokenizer_instance = mock.MagicMock()
-    mocker.patch('transformers.AutoTokenizer.from_pretrained', return_value=mock_tokenizer_instance)
+    mocker.patch(
+        "transformers.AutoTokenizer.from_pretrained",
+        return_value=mock_tokenizer_instance,
+    )
 
     mock_config_instance = mock.MagicMock()
-    mocker.patch('transformers.AutoConfig.from_pretrained', return_value=mock_config_instance)
+    mocker.patch(
+        "transformers.AutoConfig.from_pretrained", return_value=mock_config_instance
+    )
 
     mock_model_instance = mock.MagicMock()
-    mocker.patch('transformers.AutoModel.from_config', return_value=mock_model_instance)
+    mocker.patch("transformers.AutoModel.from_config", return_value=mock_model_instance)
 
     Storage.download(uri)
 
---
+++ python/sklearnserver/test_sk.py
@@ -0,0 +1,21 @@
+from sklearn import svm
+from sklearn import datasets
+from joblib import dump
+
+clf = svm.SVC(gamma='scale')
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+clf.fit(X, y)
+dump(clf, 'model.joblib')
+
+# from sklearn import svm
+# from sklearn import datasets
+# from joblib import dump
+
+# iris = datasets.load_iris()
+# X, y = iris.data, iris.target
+
+# clf = svm.SVC(gamma='scale')
+# clf.fit(X, y)
+
+# dump(clf, 'model.joblib')
