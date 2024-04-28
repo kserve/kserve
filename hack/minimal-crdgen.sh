07ea6be9d2e128543bda7c1cb8c81c55f9908d21
Yuan Tang <terrytangyuan@gmail.com>
2024-04-27 22:30:52 -0400

    build: Remove misleading logs from minimal-crdgen.sh
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- hack/minimal-crdgen.sh
+++ hack/minimal-crdgen.sh
@@ -4,10 +4,9 @@ set -eu -o pipefail
 cd "$(dirname "$0")/.."
 
 find config/crd/full -name 'serving.kserve.io*.yaml' | while read -r file; do
-  echo "Patching ${file}"
   # create minimal
   minimal="config/crd/minimal/$(basename "$file")"
-  echo "Creating ${minimal}"
+  echo "Creating minimal CRD: ${minimal}"
   cp "$file" "$minimal"
   go run ./cmd/crd-gen removecrdvalidation "$minimal"
 done
