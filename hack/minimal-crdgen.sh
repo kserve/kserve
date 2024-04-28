dc9b0edf7c062b1206e8dc235e55bab35f7ed59b
Yuan Tang <terrytangyuan@gmail.com>
2024-04-27 22:32:17 -0400

    Add file
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- hack/minimal-crdgen.sh
+++ hack/minimal-crdgen.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")/.."
 find config/crd/full -name 'serving.kserve.io*.yaml' | while read -r file; do
   # create minimal
   minimal="config/crd/minimal/$(basename "$file")"
-  echo "Creating minimal CRD: ${minimal}"
+  echo "Creating minimal CRD file: ${minimal}"
   cp "$file" "$minimal"
   go run ./cmd/crd-gen removecrdvalidation "$minimal"
 done
