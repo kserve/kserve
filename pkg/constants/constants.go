bf260e587a997f9d2c7550a7add8b9da166ce4e8
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2024-05-10 15:39:22 +0530

    integration with predictor
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -175,6 +175,7 @@ var (
 var (
 	AutoscalerClassHPA      AutoscalerClassType = "hpa"
 	AutoscalerClassExternal AutoscalerClassType = "external"
+	AutoscalerClassKeda     AutoscalerClassType = "keda"
 )
 
 // Autoscaler Metrics
