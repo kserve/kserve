929691b6c0fc6e5356ca77f66905f0e3524fcab9
Gavrish Prabhu <gavrish.prabhu@nutanix.com>
2024-05-14 17:16:39 +0000

    fix to check all arch in model config forr vllm support
    Signed-off-by: Gavrish Prabhu <gavrish.prabhu@nutanix.com>

--- python/huggingfaceserver/huggingfaceserver/vllm/utils.py
+++ python/huggingfaceserver/huggingfaceserver/vllm/utils.py
@@ -39,12 +39,12 @@ def infer_vllm_supported_from_model_architecture(
 ) -> bool:
     if not _vllm:
         return False
+    
     model_config = AutoConfig.from_pretrained(model_config_path)
-    architecture = model_config.architectures[0]
-
-    if architecture not in ModelRegistry.get_supported_archs():
-        logger.info("not a supported model by vLLM")
-        return False
+    for architecture in model_config.architectures:
+        if architecture not in ModelRegistry.get_supported_archs():
+            logger.info("not a supported model by vLLM")
+            return False
     return True
 
 
