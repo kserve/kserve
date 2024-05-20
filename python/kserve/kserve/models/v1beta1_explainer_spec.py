097432a025bac1aad93e1e548a7b7a29878069d4
Tim Kleinloog <tkleinloog@deeploy.ml>
2024-05-20 11:25:14 +0200

    Rerun make generate
    Signed-off-by: Tim Kleinloog <tkleinloog@deeploy.ml>

--- python/kserve/kserve/models/v1beta1_explainer_spec.py
+++ python/kserve/kserve/models/v1beta1_explainer_spec.py
@@ -158,7 +158,6 @@ class V1beta1ExplainerSpec(object):
         'volumes': 'volumes'
     }
 
-
     def __init__(self, active_deadline_seconds=None, affinity=None, annotations=None, art=None, automount_service_account_token=None, batcher=None, canary_traffic_percent=None, container_concurrency=None, containers=None, deployment_strategy=None, dns_config=None, dns_policy=None, enable_service_links=None, ephemeral_containers=None, host_aliases=None, host_ipc=None, host_network=None, host_pid=None, host_users=None, hostname=None, image_pull_secrets=None, init_containers=None, labels=None, logger=None, max_replicas=None, min_replicas=None, model=None, node_name=None, node_selector=None, os=None, overhead=None, preemption_policy=None, priority=None, priority_class_name=None, readiness_gates=None, resource_claims=None, restart_policy=None, runtime_class_name=None, scale_metric=None, scale_target=None, scheduler_name=None, scheduling_gates=None, security_context=None, service_account=None, service_account_name=None, set_hostname_as_fqdn=None, share_process_namespace=None, subdomain=None, termination_grace_period_seconds=None, timeout=None, tolerations=None, topology_spread_constraints=None, volumes=None, local_vars_configuration=None):  # noqa: E501
         """V1beta1ExplainerSpec - a model defined in OpenAPI"""  # noqa: E501
         if local_vars_configuration is None:
