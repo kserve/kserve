5c2324d070a3b345f9e2b61553fc118e68035410
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2024-05-27 20:56:28 +0530

    bug fix and formatting
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -210,6 +210,7 @@ var AutoscalerAllowedClassList = []AutoscalerClassType{
 var AutoscalerAllowedHPAMetricsList = []AutoscalerMetricsType{
 	AutoScalerMetricsCPU,
 	AutoScalerMetricsMemory,
+	AutoScalerMetricsPrometheus,
 }
 
 // Autoscaler KPA Metrics Allowed List
