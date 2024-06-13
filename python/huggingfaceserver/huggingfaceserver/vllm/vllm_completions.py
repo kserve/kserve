03eecb214211d7c73583bfdffde08f8be831ac52
Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>
2024-06-13 16:42:30 +0530

    Fix logprobs for vLLM
    Signed-off-by: Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>

--- python/huggingfaceserver/huggingfaceserver/vllm/vllm_completions.py
+++ python/huggingfaceserver/huggingfaceserver/vllm/vllm_completions.py
@@ -294,7 +294,9 @@ class OpenAIServingCompletion:
                     output_text = prompt_text
                 elif request.echo and request.max_tokens > 0:
                     token_ids = prompt_token_ids + output.token_ids
-                    top_logprobs = prompt_logprobs + output.logprobs
+                    top_logprobs = output.logprobs or prompt_logprobs
+                    if output.logprobs and prompt_logprobs:
+                        top_logprobs = prompt_logprobs + output.logprobs
                     output_text = prompt_text + output.text
                 else:
                     token_ids = output.token_ids
@@ -402,16 +404,16 @@ class OpenAIServingCompletion:
             step_top_logprobs = top_logprobs[i]
             if step_top_logprobs is not None:
                 token_logprob = step_top_logprobs[token_id].logprob
+                token = step_top_logprobs[token_id].decoded_token
+                logprobs.tokens.append(token)
+                last_token_len = len(token)
             else:
                 token_logprob = None
-            token = step_top_logprobs[token_id].decoded_token
-            logprobs.tokens.append(token)
             logprobs.token_logprobs.append(token_logprob)
             if len(logprobs.text_offset) == 0:
                 logprobs.text_offset.append(initial_text_offset)
             else:
                 logprobs.text_offset.append(logprobs.text_offset[-1] + last_token_len)
-            last_token_len = len(token)
 
             if num_output_top_logprobs:
                 logprobs.top_logprobs.append(
