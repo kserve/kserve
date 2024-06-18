91a9b6a0e7863086a0571b82d1c07c65f8bbab5a
caleb-imaige <caleb@imaige.com>
2024-06-18 13:05:57 -0600

    full working test suite, incl. addition of ssl certs for secure

--- python/kserve/test/test_server_grpc.py
+++ python/kserve/test/test_server_grpc.py
@@ -1,14 +1,10 @@
-import unittest
 import pytest
-import pytest_asyncio
 from typing import List, Union
-from concurrent import futures
-from concurrent.futures import ThreadPoolExecutor
-import threading
-import time
 import asyncio
 import kserve
 import multiprocessing
+import grpc
+import os
 
 # Import Kserve
 from typing import Dict, Union
@@ -40,8 +36,11 @@ class TestModel(Model):  # Test model
 
 
 # Function to run the model server
-async def run_model(secure_grpc_server, server_key, server_cert, ca_cert, models):
+async def run_model(secure_grpc_server, models):
     if secure_grpc_server:
+        server_key = open("./test/kserve_test_certs/server-key.pem", "rb").read()
+        server_cert = open("./test/kserve_test_certs/server-cert.pem", "rb").read()
+        ca_cert = open("./test/kserve_test_certs/ca-cert.pem", "rb").read()
         server = ModelServer(
             secure_grpc_server=secure_grpc_server,
             server_key=server_key,
@@ -54,10 +53,18 @@ async def run_model(secure_grpc_server, server_key, server_cert, ca_cert, models
         await server.start(models)
 
 
-async def grpc_infer_request(integer: int, port: str, ssl: bool, creds: List, channel_args: any, queue: multiprocessing.Queue):
-    await asyncio.sleep(1.5)
-    print("After sleep")
-    if channel_args:
+async def grpc_infer_request(ssl: bool, port: str, integer: int, queue: multiprocessing.Queue):
+    await asyncio.sleep(1)
+    if ssl:
+        channel_args = ('grpc.ssl_target_name_override', 'localhost')
+        client_key = open("./test/kserve_test_certs/client-key.pem", "rb").read()
+        client_cert = open("./test/kserve_test_certs/client-cert.pem", "rb").read()
+        ca_cert = open("./test/kserve_test_certs/ca-cert.pem", "rb").read()
+
+        channel_creds = grpc.ssl_channel_credentials(
+            root_certificates=ca_cert, private_key=client_key, certificate_chain=client_cert
+        )
+        creds = channel_creds
         client = InferenceServerClient(url=port,
                                        ssl=ssl,
                                        creds=creds,
@@ -65,40 +72,36 @@ async def grpc_infer_request(integer: int, port: str, ssl: bool, creds: List, ch
                                            # grpc.ssl_target_name_override must be set to match CN used in cert gen
                                            channel_args,)
                                        )
-    else:  # not channel_args or channel_args == []:
+    else:  # not ssl
         client = InferenceServerClient(url=port,
                                        ssl=ssl,
-                                       creds=creds)
+                                       )
     data = float(integer)
     infer_input = InferInput(name="input-0", shape=[1], datatype="FP32", data=[data])
     request = InferRequest(infer_inputs=[infer_input], model_name="test-model")
     res = client.infer(infer_request=request)
-    # assert res.outputs[0].contents.fp32_contents[0] == 2.0
+    print(f"res is: {res}")
     queue.put(res.outputs[0].contents.fp32_contents[0])
 
 
-def run_model_sync(secure_grpc_server, server_key, server_cert, ca_cert, models):
-    asyncio.run(run_model(secure_grpc_server, server_key, server_cert, ca_cert, models))
+def run_model_sync(secure_grpc_server, models):
+    asyncio.run(run_model(secure_grpc_server, models))
 
 
-def grpc_infer_request_sync(integer: int, port: str, ssl: bool, creds: List, channel_args: any, queue: multiprocessing.Queue):
-    return asyncio.run(grpc_infer_request(integer, port, ssl, creds, channel_args, queue))
+def grpc_infer_request_sync(ssl: bool, port: str, integer: int, queue: multiprocessing.Queue):
+    return asyncio.run(grpc_infer_request(ssl, port, integer, queue))
 
 
 class TestGrpcSecureServer:
-    def test_secure_server_returns(self):
-        # TODO: create better certs
-        server_key = "test"
-        server_cert = "test"
-        ca_cert = "test"
+    def test_insecure_grpc_server_returns(self):
         models = [TestModel("test-model")]
 
         queue = multiprocessing.Queue()
 
         server_process = multiprocessing.Process(target=run_model_sync,
-                                                 args=(False, server_key, server_cert, ca_cert, models))
+                                                 args=(False, models))
         client_process = multiprocessing.Process(target=grpc_infer_request_sync,
-                                                 args=(1, "localhost:8081", False, [], [], queue))
+                                                 args=(False, "localhost:8081", 1, queue))
 
         server_process.start()
         client_process.start()
@@ -106,8 +109,72 @@ class TestGrpcSecureServer:
         client_process.join()
 
         output = queue.get()
-        print(f"output value from queue is: {output}")
 
         server_process.terminate()
         assert output == 2.0
 
+    def test_secure_grpc_server_returns(self):
+        models = [TestModel("test-model")]
+
+        queue = multiprocessing.Queue()
+
+        server_process = multiprocessing.Process(target=run_model_sync,
+                                                 args=(True, models))
+        client_process = multiprocessing.Process(target=grpc_infer_request_sync,
+                                                 args=(True, "localhost:8081", 1, queue))
+
+        server_process.start()
+        client_process.start()
+
+        client_process.join()
+
+        output = queue.get()
+
+        server_process.terminate()
+        assert output == 2.0
+
+    def test_secure_grpc_server_insecure_client_fails(self):
+        models = [TestModel("test-model")]
+
+        queue = multiprocessing.Queue()
+
+        server_process = multiprocessing.Process(target=run_model_sync,
+                                                 args=(True, models))
+        client_process = multiprocessing.Process(target=grpc_infer_request_sync,
+                                                 args=(False, "localhost:8081", 1, queue))
+
+        server_process.start()
+        client_process.start()
+
+        client_process.join(timeout=5)
+
+        if not queue.empty():
+            output = queue.get()
+        else:
+            output = None
+
+        server_process.terminate()
+        assert output is None
+
+    def test_insecure_grpc_server_secure_client_fails(self):
+        models = [TestModel("test-model")]
+
+        queue = multiprocessing.Queue()
+
+        server_process = multiprocessing.Process(target=run_model_sync,
+                                                 args=(False, models))
+        client_process = multiprocessing.Process(target=grpc_infer_request_sync,
+                                                 args=(True, "localhost:8081", 1, queue))
+
+        server_process.start()
+        client_process.start()
+
+        client_process.join(timeout=5)
+
+        if not queue.empty():
+            output = queue.get()
+        else:
+            output = None
+
+        server_process.terminate()
+        assert output is None
