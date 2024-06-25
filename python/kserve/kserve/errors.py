f19600ac97fcc429c719675bf2484bfd3fc6d60f
Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>
2024-06-26 00:16:08 +0530

    Fix No model ready error in multi model serving
    - Fixes the regression introduced by #3275
    
    Signed-off-by: Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>

--- python/kserve/kserve/errors.py
+++ python/kserve/kserve/errors.py
@@ -141,7 +141,12 @@ class NoModelReady(RuntimeError):
 
     def __str__(self):
         model_name_list = [model.name for model in self.models]
-        self.error_msg = f"Models with name {','.join(model_name_list)} are not ready."
+        if len(model_name_list) == 1:
+            self.error_msg = f"Model with name {model_name_list[0]} is not ready."
+        else:
+            self.error_msg = (
+                f"Models with names {','.join(model_name_list)} are not ready."
+            )
         if self.detail:
             self.error_msg = self.error_msg + " " + self.detail
         return self.error_msg
