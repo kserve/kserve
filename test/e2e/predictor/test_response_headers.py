eeb6d83ce8e2b19748dc01f4aca2d930854662b4
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2024-07-01 16:17:45 +0530

    modifying the e2e test for rebase conflict
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- test/e2e/predictor/test_response_headers.py
+++ test/e2e/predictor/test_response_headers.py
@@ -1,6 +1,11 @@
 import os
 
 import pytest
+import requests
+import logging
+import time
+import json
+
 from kserve import (
     KServeClient,
     V1beta1InferenceService,
@@ -12,13 +17,15 @@ from kserve import (
 from kubernetes.client import V1ResourceRequirements
 from kubernetes import client
 from kubernetes.client import V1Container, V1ContainerPort
-from ..common.utils import KSERVE_TEST_NAMESPACE, predict
+from ..common.utils import KSERVE_TEST_NAMESPACE, get_isvc_endpoint
 
 
 @pytest.mark.transformer
 def test_predictor_headers_v1():
     service_name = "isvc-custom-model-transformer-v1"
     model_name = "custom-model"
+    protocol_version="v1"
+    input_json = "./data/custom_model_input_v1.json"
 
     predictor = V1beta1PredictorSpec(
         containers=[
@@ -65,15 +72,35 @@ def test_predictor_headers_v1():
     kserve_client.create(isvc)
     kserve_client.wait_isvc_ready(service_name, namespace=KSERVE_TEST_NAMESPACE)
 
-    res = predict(
+    isvc = kserve_client.get(
         service_name,
-        "./data/custom_model_input.json",
-        protocol_version="v1",
-        model_name=model_name,
+        namespace=KSERVE_TEST_NAMESPACE,
+        version=protocol_version,
     )
-    response_headers = res["headers"]
+    cluster_ip, host, path = get_isvc_endpoint(isvc)
+    headers = {"Host": host, "Content-Type": "application/json"}
+
+    if model_name is None:
+        model_name = service_name
+
+    url = f"http://{cluster_ip}{path}/v1/models/{model_name}:predict"
+
+    time.sleep(10)
+    with open(input_json) as json_file:
+        data = json.load(json_file)
+        response = requests.post(url, data, headers=headers)
+        logging.info(
+            "Got response code %s, content %s", response.status_code, response.content
+        )
+        if response.status_code == 200:
+            res_data = json.loads(response.content.decode("utf-8"))
+            res_data["headers"] = response.headers
+        else:
+            response.raise_for_status()
+    
+    response_headers = res_data["headers"]
     assert response_headers["my-header"] == "test_header"
-    points = ["%.3f" % (point) for point in list(res["predictions"])]
+    points = ["%.3f" % (point) for point in list(res_data["predictions"])]
     assert points == ["14.976", "14.037", "13.966", "12.252", "12.086"]
     kserve_client.delete(service_name, KSERVE_TEST_NAMESPACE)
 
@@ -82,6 +109,8 @@ def test_predictor_headers_v1():
 def test_predictor_headers_v2():
     service_name = "isvc-custom-model-transformer-v2"
     model_name = "custom-model"
+    protocol_version="v2"
+    input_json = "./data/custom_model_input_v2.json"
 
     predictor = V1beta1PredictorSpec(
         containers=[
@@ -128,14 +157,34 @@ def test_predictor_headers_v2():
     kserve_client.create(isvc)
     kserve_client.wait_isvc_ready(service_name, namespace=KSERVE_TEST_NAMESPACE)
 
-    res = predict(
+    isvc = kserve_client.get(
         service_name,
-        "./data/custom_model_input_v2.json",
-        protocol_version="v2",
-        model_name=model_name,
+        namespace=KSERVE_TEST_NAMESPACE,
+        version=protocol_version,
     )
-    response_headers = res["headers"]
+    cluster_ip, host, path = get_isvc_endpoint(isvc)
+    headers = {"Host": host, "Content-Type": "application/json"}
+
+    if model_name is None:
+        model_name = service_name
+
+    url = f"http://{cluster_ip}{path}/v2/models/{model_name}/infer"
+
+    time.sleep(10)
+    with open(input_json) as json_file:
+        data = json.load(json_file)
+        response = requests.post(url, data, headers=headers)
+        logging.info(
+            "Got response code %s, content %s", response.status_code, response.content
+        )
+        if response.status_code == 200:
+            res_data = json.loads(response.content.decode("utf-8"))
+            res_data["headers"] = response.headers
+        else:
+            response.raise_for_status()
+
+    response_headers = res_data["headers"]
     assert response_headers["my-header"] == "test_header"
-    points = ["%.3f" % (point) for point in list(res["outputs"][0]["data"])]
+    points = ["%.3f" % (point) for point in list(res_data["outputs"][0]["data"])]
     assert points == ["14.976", "14.037", "13.966", "12.252", "12.086"]
     kserve_client.delete(service_name, KSERVE_TEST_NAMESPACE)
