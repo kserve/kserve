f75f0298734d84b31c178de9a804c9a3ade19bf1
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2024-07-30 16:41:26 +0530

    bug fix on e2e test
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- python/kserve/kserve/model.py
+++ python/kserve/kserve/model.py
@@ -375,10 +375,8 @@ class Model(InferenceModel):
             model_name=self.name,
             data=payload,
             headers=predict_headers,
+            response_headers=response_headers,
         )
-
-        if response_headers is not None:
-            response_headers.update(response.headers)
 
         return response
 
