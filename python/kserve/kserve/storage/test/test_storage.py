f159d666e262bb3b8c60210584b44ebd813bca42
tjandy98 <3953059+tjandy98@users.noreply.github.com>
2024-08-06 22:03:41 +0800

    Add test cases for Azure Blob and File Share URI pattern matching
    Signed-off-by: tjandy98 <3953059+tjandy98@users.noreply.github.com>

--- python/kserve/kserve/storage/test/test_storage.py
+++ python/kserve/kserve/storage/test/test_storage.py
@@ -303,3 +303,31 @@ def test_unpack_zip_file():
     Storage._unpack_archive_file(tar_file, mimetype, out_dir)
     assert os.path.exists(os.path.join(out_dir, "model.pth"))
     os.remove(os.path.join(out_dir, "model.pth"))
+
+
+@mock.patch(STORAGE_MODULE + '.Storage._download_azure_blob')
+def test_download_azure_blob_called_with_matching_uri(mock_download_azure_blob):
+    azure_blob_uris = [
+        'https://accountname.blob.core.windows.net/container/some/blob/',
+        'https://accountname.z20.blob.storage.azure.net/container/some/blob/'
+    ]
+
+    for uri in azure_blob_uris:
+        Storage.download(uri, out_dir="dest_path")
+
+    expected_calls = [mock.call(uri, "dest_path") for uri in azure_blob_uris]
+    mock_download_azure_blob.assert_has_calls(expected_calls)
+
+
+@mock.patch(STORAGE_MODULE + '.Storage._download_azure_file_share')
+def test_download_azure_file_share_called_with_matching_uri(mock_download_azure_file_share):
+    azure_file_uris = [
+        'https://accountname.file.core.windows.net/container/some/blob',
+        'https://accountname.z20.file.storage.azure.net/container/some/blob'
+    ]
+
+    for uri in azure_file_uris:
+        Storage.download(uri, out_dir="dest_path")
+
+    expected_calls = [mock.call(uri, "dest_path") for uri in azure_file_uris]
+    mock_download_azure_file_share.assert_has_calls(expected_calls)
