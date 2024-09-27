a101881bcd5207c01cc1db626a94b246142f5868
jooho lee <jlee@redhat.com>
2024-09-26 21:57:26 -0400

    fix go linter
    Signed-off-by: jooho lee <jlee@redhat.com>

--- pkg/apis/serving/v1beta1/predictor.go
+++ pkg/apis/serving/v1beta1/predictor.go
@@ -69,9 +69,9 @@ type PredictorSpec struct {
 	ComponentExtensionSpec `json:",inline"`
 }
 
-type WorkerSpec struct{
+type WorkerSpec struct {
 	PodSpec `json:",inline"`
-	
+
 	// Configure the number of replicas in the worker set, each worker set represents the unit of scaling
 	// +optional
 	Size int `json:"size,omitempty"`
