fac8e314b9f63c124f69679662299f7570d067ab
Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>
2024-09-30 13:12:33 +0530

    Bump golang-lint to suupport go 1.22
    Signed-off-by: Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>

--- hack/verify-golint.sh
+++ hack/verify-golint.sh
@@ -23,7 +23,7 @@ golangci_lint_binary="$(go env GOPATH)/bin/golangci-lint"
 # Check if golangci-lint is already installed
 if ! command -v golangci-lint &> /dev/null; then
     echo "installing golangci-lint"
-    go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.55.2
+    go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.61
 	# Verify golangci-lint installation
 	$golangci_lint_binary --version
 fi
