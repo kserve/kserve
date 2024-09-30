3a7df66b0835ca829b906dc2c4f2df74cf76d79f
yxia216 <yxia216@bloomberg.net>
2024-09-30 13:52:31 -0400

    fix vLLMModel
    Signed-off-by: yxia216 <yxia216@bloomberg.net>

--- python/huggingfaceserver/huggingfaceserver/vllm/vllm_completions.py
+++ python/huggingfaceserver/huggingfaceserver/vllm/vllm_completions.py
@@ -359,9 +359,11 @@ class OpenAIServingCompletion:
     def apply_chat_template(
         self,
         messages: Iterable[ChatCompletionRequestMessage,],
+        chat_template: Optional[str] = None,
+
     ):
         return self.tokenizer.apply_chat_template(
-            conversation=messages, tokenize=False, add_generation_prompt=True
+            conversation=messages, chat_template=chat_template, tokenize=False, add_generation_prompt=True
         )
 
     async def _post_init(self):
