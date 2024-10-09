ace02f9b691a0db8f18fa87d7bdf5e383c8d5832
Jin Dong <greenmoon55@users.noreply.github.com>
2024-10-09 14:15:53 -0400

    Fix python lint
    Signed-off-by: Jin Dong <greenmoon55@users.noreply.github.com>

--- python/kserve/test/test_v1beta1_security_config.py
+++ python/kserve/test/test_v1beta1_security_config.py
@@ -33,6 +33,7 @@ import kserve
 from kserve.models.v1beta1_security_config import V1beta1SecurityConfig  # noqa: E501
 from kserve.rest import ApiException
 
+
 class TestV1beta1SecurityConfig(unittest.TestCase):
     """V1beta1SecurityConfig unit test stubs"""
 
@@ -44,18 +45,16 @@ class TestV1beta1SecurityConfig(unittest.TestCase):
 
     def make_instance(self, include_optional):
         """Test V1beta1SecurityConfig
-            include_option is a boolean, when False only required
-            params are included, when True both required and
-            optional params are included """
+        include_option is a boolean, when False only required
+        params are included, when True both required and
+        optional params are included"""
         # model = kserve.models.v1beta1_security_config.V1beta1SecurityConfig()  # noqa: E501
-        if include_optional :
+        if include_optional:
+            return V1beta1SecurityConfig(auto_mount_service_account_token=True)
+        else:
             return V1beta1SecurityConfig(
-                auto_mount_service_account_token = True
+                auto_mount_service_account_token=True,
             )
-        else :
-            return V1beta1SecurityConfig(
-                auto_mount_service_account_token = True,
-        )
 
     def testV1beta1SecurityConfig(self):
         """Test V1beta1SecurityConfig"""
@@ -63,5 +62,5 @@ class TestV1beta1SecurityConfig(unittest.TestCase):
         inst_req_and_optional = self.make_instance(include_optional=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
