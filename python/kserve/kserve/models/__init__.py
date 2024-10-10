854f47099239892249f4459a2dae4af71b4de890
Jin Dong <greenmoon55@users.noreply.github.com>
2024-10-10 12:45:26 -0400

    Add a flag for automount serviceaccount token (#3979)
    * Add a flag for automount serviceaccount
    
    Signed-off-by: Jin Dong <greenmoon55@users.noreply.github.com>
    
    * Set default to false
    
    Signed-off-by: Jin Dong <greenmoon55@users.noreply.github.com>
    
    * Default to true
    
    Signed-off-by: Jin Dong <greenmoon55@users.noreply.github.com>
    
    * Fix test error
    
    Signed-off-by: Jin Dong <greenmoon55@users.noreply.github.com>
    
    * Update openapi generated.go
    
    Signed-off-by: Jin Dong <greenmoon55@users.noreply.github.com>
    
    * Fix python lint
    
    Signed-off-by: Jin Dong <greenmoon55@users.noreply.github.com>
    
    * Fix config loading
    
    Signed-off-by: Jin Dong <greenmoon55@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Jin Dong <greenmoon55@users.noreply.github.com>

--- python/kserve/kserve/models/__init__.py
+++ python/kserve/kserve/models/__init__.py
@@ -93,6 +93,7 @@ from kserve.models.v1beta1_pod_spec import V1beta1PodSpec
 from kserve.models.v1beta1_predictor_extension_spec import V1beta1PredictorExtensionSpec
 from kserve.models.v1beta1_predictor_spec import V1beta1PredictorSpec
 from kserve.models.v1beta1_sk_learn_spec import V1beta1SKLearnSpec
+from kserve.models.v1beta1_security_config import V1beta1SecurityConfig
 from kserve.models.v1beta1_storage_spec import V1beta1StorageSpec
 from kserve.models.v1beta1_tf_serving_spec import V1beta1TFServingSpec
 from kserve.models.v1beta1_torch_serve_spec import V1beta1TorchServeSpec
