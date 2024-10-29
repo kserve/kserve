80764dc445fe458206d018055b508ddfec045e81
Arjun Bhalla <abhalla31@bloomberg.net>
2024-10-29 16:30:24 -0400

    Add guided decoding
    Signed-off-by: Arjun Bhalla <abhalla31@bloomberg.net>

--- python/huggingfaceserver/huggingfaceserver/vllm/vllm_completions.py
+++ python/huggingfaceserver/huggingfaceserver/vllm/vllm_completions.py
@@ -33,7 +33,7 @@ from vllm.entrypoints.logger import RequestLogger
 from vllm.inputs.parse import parse_and_batch_prompt
 from vllm.lora.request import LoRARequest
 from vllm.prompt_adapter.request import PromptAdapterRequest
-from vllm.sampling_params import SamplingParams
+from vllm.sampling_params import SamplingParams, GuidedDecodingParams
 from vllm.utils import random_uuid
 
 from vllm.outputs import RequestOutput
@@ -48,14 +48,15 @@ from kserve.protocol.rest.openai.types.openapi import (
     Choice as CompletionChoice,
     CompletionUsage,
     CreateCompletionRequest,
+    CreateChatCompletionRequest,
     CreateCompletionResponse as Completion,
     Logprobs,
 )
 from kserve.protocol.rest.openai.errors import OpenAIError, create_error_response
-from kserve.protocol.rest.openai import ChatCompletionRequestMessage, CompletionRequest
+from kserve.protocol.rest.openai import ChatCompletionRequestMessage, CompletionRequest, ChatCompletionRequest
 
 
-def to_sampling_params(request: CreateCompletionRequest):
+def to_sampling_params(request: CreateChatCompletionRequest | CreateCompletionRequest):
     echo_without_generation = request.echo and request.max_tokens == 0
 
     logits_processors = None
@@ -85,6 +86,7 @@ def to_sampling_params(request: CreateCompletionRequest):
         max_tokens=request.max_tokens if not echo_without_generation else 1,
         logits_processors=logits_processors,
         prompt_logprobs=request.logprobs if request.echo else None,
+        guided_decoding=GuidedDecodingParams.from_optional(request.tools.function) if "tools" in request else None# https://github.com/vllm-project/vllm/blob/ab6f981671c4e5035575f5e5ef6172f4df52e121/vllm/sampling_params.py#L29-L64
     )
 
 
@@ -111,7 +113,7 @@ class OpenAIServingCompletion:
             loop.run_until_complete(self._post_init())
             loop.close()
 
-    async def create_completion(self, completion_request: CompletionRequest):
+    async def create_completion(self, completion_request: CompletionRequest | ChatCompletionRequest):
         """Completion API similar to OpenAI's API.
 
         See https://platform.openai.com/docs/api-reference/completions/create
@@ -360,12 +362,14 @@ class OpenAIServingCompletion:
         self,
         messages: Iterable[ChatCompletionRequestMessage,],
         chat_template: Optional[str] = None,
+        tools: Optional[list[dict]] = None
     ):
         return self.tokenizer.apply_chat_template(
             conversation=messages,
             chat_template=chat_template,
             tokenize=False,
             add_generation_prompt=True,
+            tools=tools
         )
 
     async def _post_init(self):
