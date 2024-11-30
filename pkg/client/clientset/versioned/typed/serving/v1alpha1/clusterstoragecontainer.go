4f5ab4c663435472285caab813c6be0867e63a30
Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>
2024-11-30 13:58:37 +0530

    Bump Gateway API to v1.2.1
    Signed-off-by: Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>

--- pkg/client/clientset/versioned/typed/serving/v1alpha1/clusterstoragecontainer.go
+++ pkg/client/clientset/versioned/typed/serving/v1alpha1/clusterstoragecontainer.go
@@ -20,14 +20,13 @@ package v1alpha1
 
 import (
 	"context"
-	"time"
 
 	v1alpha1 "github.com/kserve/kserve/pkg/apis/serving/v1alpha1"
 	scheme "github.com/kserve/kserve/pkg/client/clientset/versioned/scheme"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
-	rest "k8s.io/client-go/rest"
+	gentype "k8s.io/client-go/gentype"
 )
 
 // ClusterStorageContainersGetter has a method to return a ClusterStorageContainerInterface.
@@ -51,128 +50,18 @@ type ClusterStorageContainerInterface interface {
 
 // clusterStorageContainers implements ClusterStorageContainerInterface
 type clusterStorageContainers struct {
-	client rest.Interface
-	ns     string
+	*gentype.ClientWithList[*v1alpha1.ClusterStorageContainer, *v1alpha1.ClusterStorageContainerList]
 }
 
 // newClusterStorageContainers returns a ClusterStorageContainers
 func newClusterStorageContainers(c *ServingV1alpha1Client, namespace string) *clusterStorageContainers {
 	return &clusterStorageContainers{
-		client: c.RESTClient(),
-		ns:     namespace,
+		gentype.NewClientWithList[*v1alpha1.ClusterStorageContainer, *v1alpha1.ClusterStorageContainerList](
+			"clusterstoragecontainers",
+			c.RESTClient(),
+			scheme.ParameterCodec,
+			namespace,
+			func() *v1alpha1.ClusterStorageContainer { return &v1alpha1.ClusterStorageContainer{} },
+			func() *v1alpha1.ClusterStorageContainerList { return &v1alpha1.ClusterStorageContainerList{} }),
 	}
-}
-
-// Get takes name of the clusterStorageContainer, and returns the corresponding clusterStorageContainer object, and an error if there is any.
-func (c *clusterStorageContainers) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha1.ClusterStorageContainer, err error) {
-	result = &v1alpha1.ClusterStorageContainer{}
-	err = c.client.Get().
-		Namespace(c.ns).
-		Resource("clusterstoragecontainers").
-		Name(name).
-		VersionedParams(&options, scheme.ParameterCodec).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// List takes label and field selectors, and returns the list of ClusterStorageContainers that match those selectors.
-func (c *clusterStorageContainers) List(ctx context.Context, opts v1.ListOptions) (result *v1alpha1.ClusterStorageContainerList, err error) {
-	var timeout time.Duration
-	if opts.TimeoutSeconds != nil {
-		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
-	}
-	result = &v1alpha1.ClusterStorageContainerList{}
-	err = c.client.Get().
-		Namespace(c.ns).
-		Resource("clusterstoragecontainers").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Watch returns a watch.Interface that watches the requested clusterStorageContainers.
-func (c *clusterStorageContainers) Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error) {
-	var timeout time.Duration
-	if opts.TimeoutSeconds != nil {
-		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
-	}
-	opts.Watch = true
-	return c.client.Get().
-		Namespace(c.ns).
-		Resource("clusterstoragecontainers").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Watch(ctx)
-}
-
-// Create takes the representation of a clusterStorageContainer and creates it.  Returns the server's representation of the clusterStorageContainer, and an error, if there is any.
-func (c *clusterStorageContainers) Create(ctx context.Context, clusterStorageContainer *v1alpha1.ClusterStorageContainer, opts v1.CreateOptions) (result *v1alpha1.ClusterStorageContainer, err error) {
-	result = &v1alpha1.ClusterStorageContainer{}
-	err = c.client.Post().
-		Namespace(c.ns).
-		Resource("clusterstoragecontainers").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(clusterStorageContainer).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Update takes the representation of a clusterStorageContainer and updates it. Returns the server's representation of the clusterStorageContainer, and an error, if there is any.
-func (c *clusterStorageContainers) Update(ctx context.Context, clusterStorageContainer *v1alpha1.ClusterStorageContainer, opts v1.UpdateOptions) (result *v1alpha1.ClusterStorageContainer, err error) {
-	result = &v1alpha1.ClusterStorageContainer{}
-	err = c.client.Put().
-		Namespace(c.ns).
-		Resource("clusterstoragecontainers").
-		Name(clusterStorageContainer.Name).
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(clusterStorageContainer).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Delete takes name of the clusterStorageContainer and deletes it. Returns an error if one occurs.
-func (c *clusterStorageContainers) Delete(ctx context.Context, name string, opts v1.DeleteOptions) error {
-	return c.client.Delete().
-		Namespace(c.ns).
-		Resource("clusterstoragecontainers").
-		Name(name).
-		Body(&opts).
-		Do(ctx).
-		Error()
-}
-
-// DeleteCollection deletes a collection of objects.
-func (c *clusterStorageContainers) DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error {
-	var timeout time.Duration
-	if listOpts.TimeoutSeconds != nil {
-		timeout = time.Duration(*listOpts.TimeoutSeconds) * time.Second
-	}
-	return c.client.Delete().
-		Namespace(c.ns).
-		Resource("clusterstoragecontainers").
-		VersionedParams(&listOpts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Body(&opts).
-		Do(ctx).
-		Error()
-}
-
-// Patch applies the patch and returns the patched clusterStorageContainer.
-func (c *clusterStorageContainers) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha1.ClusterStorageContainer, err error) {
-	result = &v1alpha1.ClusterStorageContainer{}
-	err = c.client.Patch(pt).
-		Namespace(c.ns).
-		Resource("clusterstoragecontainers").
-		Name(name).
-		SubResource(subresources...).
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(data).
-		Do(ctx).
-		Into(result)
-	return
 }
