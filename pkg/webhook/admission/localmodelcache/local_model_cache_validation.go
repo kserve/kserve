5668ccff8c63c3ebe4469edb737a03dea4ed7c40
Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>
2024-12-16 08:54:22 +0530

    Rebase
    Signed-off-by: Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>

--- pkg/webhook/admission/localmodelcache/local_model_cache_validation.go
+++ pkg/webhook/admission/localmodelcache/local_model_cache_validation.go
@@ -20,14 +20,15 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/kserve/kserve/pkg/apis/serving/v1alpha1"
-	"github.com/kserve/kserve/pkg/apis/serving/v1beta1"
-	"github.com/kserve/kserve/pkg/constants"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	"github.com/kserve/kserve/pkg/apis/serving/v1alpha1"
+	"github.com/kserve/kserve/pkg/apis/serving/v1beta1"
+	"github.com/kserve/kserve/pkg/constants"
 )
 
 var (
