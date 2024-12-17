ee8cc27871ea301b65a260be1267d9f77374958b
Sivanantham <90966311+sivanantha321@users.noreply.github.com>
2024-12-17 19:29:57 +0530

    Fix isvc role localmodelcache permission (#4131)
    * Fix localmodelcache permission for isvc
    
    Signed-off-by: Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>
    
    * Patch localmodelcache webhook for kubeflow overlay
    
    Signed-off-by: Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>
    
    ---------
    
    Signed-off-by: Sivanantham Chinnaiyan <sivanantham.chinnaiyan@ideas2it.com>
    Signed-off-by: Dan Sun <dsun20@bloomberg.net>

--- pkg/controller/v1beta1/inferenceservice/controller.go
+++ pkg/controller/v1beta1/inferenceservice/controller.go
@@ -57,7 +57,7 @@ import (
 // +kubebuilder:rbac:groups=serving.kserve.io,resources=clusterservingruntimes;clusterservingruntimes/finalizers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=serving.kserve.io,resources=clusterservingruntimes/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=serving.kserve.io,resources=clusterstoragecontainers,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=serving.kserve.io,resources=localmodelcaches,verbs=get;list
+// +kubebuilder:rbac:groups=serving.kserve.io,resources=localmodelcaches,verbs=get;list;watch
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=serving.kserve.io,resources=inferenceservices/status,verbs=get;update;patch
