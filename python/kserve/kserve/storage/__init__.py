cf035030ca67c9200fc6b01d5d85a7cdc8d3a4d5
Spolti <fspolti@redhat.com>
2025-01-02 18:26:10 -0300

    make it backwards compatible
    Signed-off-by: Spolti <fspolti@redhat.com>

---
+++ python/kserve/kserve/storage/__init__.py
@@ -0,0 +1,25 @@
+# Copyright 2025 The KServe Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# flake8: noqa
+
+# Keep backwards capability, try import storage package.
+# This way, existing projects using the Python SDK will not break, allowing
+# users to upgrade the SDK without changing their code.
+# We might need to inform users about the segregation of the storage package
+# so they have time to update the code and change the imports to the new package.
+try:
+    from storage import *
+except ImportError as e:
+    print("Please install the storage package")
+    raise ImportError("Failed to import storage package") from e
