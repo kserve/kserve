ab2e46aa5e5ffd7dff6fa862c47a833e9e1c3bab
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2025-01-23 12:19:39 +0530

    make generate
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

---
+++ python/kserve/docs/V1beta1MetricIdentifier.md
@@ -0,0 +1,12 @@
+# V1beta1MetricIdentifier
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**backend** | **str** | MetricsBackend defines the scaling metric type watched by autoscaler possible values are prometheus, graphite. | [optional] 
+**query** | **str** | Query to run to get metrics from MetricsBackend | [optional] 
+**server_address** | **str** | Address of MetricsBackend server. | [optional] 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
--- python/kserve/kserve/models/v1beta1_explainer_spec.py
+++ python/kserve/kserve/models/v1beta1_explainer_spec.py
@@ -51,6 +51,7 @@ class V1beta1ExplainerSpec(object):
         'affinity': 'V1Affinity',
         'annotations': 'dict(str, str)',
         'art': 'V1beta1ARTExplainerSpec',
+        'auto_scaling': 'list[V1beta1AutoScalingSpec]',
         'automount_service_account_token': 'bool',
         'batcher': 'V1beta1Batcher',
         'canary_traffic_percent': 'int',
@@ -87,7 +88,6 @@ class V1beta1ExplainerSpec(object):
         'scale_metric': 'str',
         'scale_metric_type': 'str',
         'scale_target': 'int',
-        'scaler': 'V1beta1ScalerSpec',
         'scheduler_name': 'str',
         'scheduling_gates': 'list[V1PodSchedulingGate]',
         'security_context': 'V1PodSecurityContext',
@@ -108,6 +108,7 @@ class V1beta1ExplainerSpec(object):
         'affinity': 'affinity',
         'annotations': 'annotations',
         'art': 'art',
+        'auto_scaling': 'autoScaling',
         'automount_service_account_token': 'automountServiceAccountToken',
         'batcher': 'batcher',
         'canary_traffic_percent': 'canaryTrafficPercent',
@@ -144,7 +145,6 @@ class V1beta1ExplainerSpec(object):
         'scale_metric': 'scaleMetric',
         'scale_metric_type': 'scaleMetricType',
         'scale_target': 'scaleTarget',
-        'scaler': 'scaler',
         'scheduler_name': 'schedulerName',
         'scheduling_gates': 'schedulingGates',
         'security_context': 'securityContext',
@@ -160,7 +160,7 @@ class V1beta1ExplainerSpec(object):
         'volumes': 'volumes'
     }
 
-    def __init__(self, active_deadline_seconds=None, affinity=None, annotations=None, art=None, automount_service_account_token=None, batcher=None, canary_traffic_percent=None, container_concurrency=None, containers=None, deployment_strategy=None, dns_config=None, dns_policy=None, enable_service_links=None, ephemeral_containers=None, host_aliases=None, host_ipc=None, host_network=None, host_pid=None, host_users=None, hostname=None, image_pull_secrets=None, init_containers=None, labels=None, logger=None, max_replicas=None, min_replicas=None, node_name=None, node_selector=None, os=None, overhead=None, preemption_policy=None, priority=None, priority_class_name=None, readiness_gates=None, resource_claims=None, restart_policy=None, runtime_class_name=None, scale_metric=None, scale_metric_type=None, scale_target=None, scaler=None, scheduler_name=None, scheduling_gates=None, security_context=None, service_account=None, service_account_name=None, set_hostname_as_fqdn=None, share_process_namespace=None, subdomain=None, termination_grace_period_seconds=None, timeout=None, tolerations=None, topology_spread_constraints=None, volumes=None, local_vars_configuration=None):  # noqa: E501
+    def __init__(self, active_deadline_seconds=None, affinity=None, annotations=None, art=None, auto_scaling=None, automount_service_account_token=None, batcher=None, canary_traffic_percent=None, container_concurrency=None, containers=None, deployment_strategy=None, dns_config=None, dns_policy=None, enable_service_links=None, ephemeral_containers=None, host_aliases=None, host_ipc=None, host_network=None, host_pid=None, host_users=None, hostname=None, image_pull_secrets=None, init_containers=None, labels=None, logger=None, max_replicas=None, min_replicas=None, node_name=None, node_selector=None, os=None, overhead=None, preemption_policy=None, priority=None, priority_class_name=None, readiness_gates=None, resource_claims=None, restart_policy=None, runtime_class_name=None, scale_metric=None, scale_metric_type=None, scale_target=None, scheduler_name=None, scheduling_gates=None, security_context=None, service_account=None, service_account_name=None, set_hostname_as_fqdn=None, share_process_namespace=None, subdomain=None, termination_grace_period_seconds=None, timeout=None, tolerations=None, topology_spread_constraints=None, volumes=None, local_vars_configuration=None):  # noqa: E501
         """V1beta1ExplainerSpec - a model defined in OpenAPI"""  # noqa: E501
         if local_vars_configuration is None:
             local_vars_configuration = Configuration()
@@ -170,6 +170,7 @@ class V1beta1ExplainerSpec(object):
         self._affinity = None
         self._annotations = None
         self._art = None
+        self._auto_scaling = None
         self._automount_service_account_token = None
         self._batcher = None
         self._canary_traffic_percent = None
@@ -206,7 +207,6 @@ class V1beta1ExplainerSpec(object):
         self._scale_metric = None
         self._scale_metric_type = None
         self._scale_target = None
-        self._scaler = None
         self._scheduler_name = None
         self._scheduling_gates = None
         self._security_context = None
@@ -230,6 +230,8 @@ class V1beta1ExplainerSpec(object):
             self.annotations = annotations
         if art is not None:
             self.art = art
+        if auto_scaling is not None:
+            self.auto_scaling = auto_scaling
         if automount_service_account_token is not None:
             self.automount_service_account_token = automount_service_account_token
         if batcher is not None:
@@ -302,8 +304,6 @@ class V1beta1ExplainerSpec(object):
             self.scale_metric_type = scale_metric_type
         if scale_target is not None:
             self.scale_target = scale_target
-        if scaler is not None:
-            self.scaler = scaler
         if scheduler_name is not None:
             self.scheduler_name = scheduler_name
         if scheduling_gates is not None:
@@ -418,6 +418,29 @@ class V1beta1ExplainerSpec(object):
         """
 
         self._art = art
+
+    @property
+    def auto_scaling(self):
+        """Gets the auto_scaling of this V1beta1ExplainerSpec.  # noqa: E501
+
+        AutoScaling to be used for autoscaling spec. Could be used for Keda autoscaling.  # noqa: E501
+
+        :return: The auto_scaling of this V1beta1ExplainerSpec.  # noqa: E501
+        :rtype: list[V1beta1AutoScalingSpec]
+        """
+        return self._auto_scaling
+
+    @auto_scaling.setter
+    def auto_scaling(self, auto_scaling):
+        """Sets the auto_scaling of this V1beta1ExplainerSpec.
+
+        AutoScaling to be used for autoscaling spec. Could be used for Keda autoscaling.  # noqa: E501
+
+        :param auto_scaling: The auto_scaling of this V1beta1ExplainerSpec.  # noqa: E501
+        :type: list[V1beta1AutoScalingSpec]
+        """
+
+        self._auto_scaling = auto_scaling
 
     @property
     def automount_service_account_token(self):
@@ -1236,27 +1259,6 @@ class V1beta1ExplainerSpec(object):
         """
 
         self._scale_target = scale_target
-
-    @property
-    def scaler(self):
-        """Gets the scaler of this V1beta1ExplainerSpec.  # noqa: E501
-
-
-        :return: The scaler of this V1beta1ExplainerSpec.  # noqa: E501
-        :rtype: V1beta1ScalerSpec
-        """
-        return self._scaler
-
-    @scaler.setter
-    def scaler(self, scaler):
-        """Sets the scaler of this V1beta1ExplainerSpec.
-
-
-        :param scaler: The scaler of this V1beta1ExplainerSpec.  # noqa: E501
-        :type: V1beta1ScalerSpec
-        """
-
-        self._scaler = scaler
 
     @property
     def scheduler_name(self):
