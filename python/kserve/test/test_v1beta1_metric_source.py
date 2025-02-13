910ee417d8c1aa17e95c2de3cb86d29e5bbdd6e9
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2025-02-13 12:43:58 +0530

    python lint fix
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- python/kserve/test/test_v1beta1_metric_source.py
+++ python/kserve/test/test_v1beta1_metric_source.py
@@ -33,6 +33,7 @@ import kserve
 from kserve.models.v1beta1_metric_source import V1beta1MetricSource  # noqa: E501
 from kserve.rest import ApiException
 
+
 class TestV1beta1MetricSource(unittest.TestCase):
     """V1beta1MetricSource unit test stubs"""
 
@@ -44,20 +45,16 @@ class TestV1beta1MetricSource(unittest.TestCase):
 
     def make_instance(self, include_optional):
         """Test V1beta1MetricSource
-            include_option is a boolean, when False only required
-            params are included, when True both required and
-            optional params are included """
+        include_option is a boolean, when False only required
+        params are included, when True both required and
+        optional params are included"""
         # model = kserve.models.v1beta1_metric_source.V1beta1MetricSource()  # noqa: E501
-        if include_optional :
+        if include_optional:
             return V1beta1MetricSource(
-                backend = '0', 
-                namespace = '0', 
-                query = '0', 
-                server_address = '0'
+                backend="0", namespace="0", query="0", server_address="0"
             )
-        else :
-            return V1beta1MetricSource(
-        )
+        else:
+            return V1beta1MetricSource()
 
     def testV1beta1MetricSource(self):
         """Test V1beta1MetricSource"""
@@ -65,5 +62,5 @@ class TestV1beta1MetricSource(unittest.TestCase):
         inst_req_and_optional = self.make_instance(include_optional=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
