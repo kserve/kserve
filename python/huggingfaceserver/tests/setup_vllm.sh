5a472aefb28445f92b15c93c874e2472f1fe706f
Abolfazl Shahbazi <12436063+ashahba@users.noreply.github.com>
2025-03-17 18:57:20 -0700

    Fix Python tests
    Signed-off-by: Abolfazl Shahbazi <12436063+ashahba@users.noreply.github.com>

--- python/huggingfaceserver/tests/setup_vllm.sh
+++ python/huggingfaceserver/tests/setup_vllm.sh
@@ -26,7 +26,16 @@ cd $VLLM_DIR
 
 git clone --branch $VLLM_VERSION --depth 1 https://github.com/vllm-project/vllm.git .
 pip install --upgrade pip
-pip install -r requirements-build.txt -r requirements-cpu.txt --extra-index-url ${TORCH_EXTRA_INDEX_URL}
+
+case $VLLM_TARGET_DEVICE in
+    cpu)
+        pip install -r requirements-build.txt -r requirements-cpu.txt --extra-index-url ${TORCH_EXTRA_INDEX_URL}
+        ;;
+    openvino)
+        pip install -r requirements-build.txt -r requirements-openvino.txt --extra-index-url ${TORCH_EXTRA_INDEX_URL}
+        ;;
+esac
+
 PIP_EXTRA_INDEX_URL=${TORCH_EXTRA_INDEX_URL} VLLM_TARGET_DEVICE=${VLLM_TARGET_DEVICE} python -m pip install -v .
 
 cd ..
