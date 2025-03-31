c3f5a7f1e5daf1c435f033b6ac2597d982e0bcd8
Andres Llausas <allausas@redhat.com>
2025-03-31 10:54:34 -0400

    [RHOAIENG-20369] Fix setup for e2e-graph tests (#542)
    * Work in progress. Fixed setup for local testing of E2E. Can now build images locally.
    
    * Still working e2e graph tests
    
    * e2e graph tests now fully passing
    
    Signed-off-by: Andres Llausas <allausas@redhat.com>
    
    * [RHOAIENG-20369] E2E graph tests now working
    
    Signed-off-by: Andres Llausas <allausas@redhat.com>
    
    [RHOAIENG-20369] E2E graph tests now working
    
    [RHOAIENG-20369] E2E graph tests now working
    
    Signed-off-by: Andres Llausas <allausas@redhat.com>
    
    ---------
    
    Signed-off-by: Andres Llausas <allausas@redhat.com>

--- test/scripts/gh-actions/build-graph-tests-images.sh
+++ test/scripts/gh-actions/build-graph-tests-images.sh
@@ -21,18 +21,42 @@ set -o nounset
 set -o pipefail
 
 echo "Github SHA ${GITHUB_SHA}"
-SUCCESS_200_ISVC_IMG_TAG=${DOCKER_REPO}/${SUCCESS_200_ISVC_IMG}:${GITHUB_SHA}
-ERROR_404_ISVC_IMG_TAG=${DOCKER_REPO}/${ERROR_404_ISVC_IMG}:${GITHUB_SHA}
+export DOCKER_REPO=kserve
+export SUCCESS_200_ISVC_IMG=success-200-isvc
+export ERROR_404_ISVC_IMG=error-404-isvc
+export DOCKER_IMAGES_PATH=/tmp/docker-images
+: "${QUAY_REPO:=}"
+SUCCESS_200_ISVC_IMG_TAG=${QUAY_REPO}/${DOCKER_REPO}/${SUCCESS_200_ISVC_IMG}:${GITHUB_SHA}
+ERROR_404_ISVC_IMG_TAG=${QUAY_REPO}/${DOCKER_REPO}/${ERROR_404_ISVC_IMG}:${GITHUB_SHA}
+if [ -z "${QUAY_REPO}" ]; then
+  SUCCESS_200_ISVC_IMG_TAG=${DOCKER_REPO}/${SUCCESS_200_ISVC_IMG}:${GITHUB_SHA}
+  ERROR_404_ISVC_IMG_TAG=${DOCKER_REPO}/${ERROR_404_ISVC_IMG}:${GITHUB_SHA}
+fi
+
+
+: "${BUILDER:=docker}"
+if [ $BUILDER == "docker" ]; then
+  BUILDER=docker
+  BUILDER_TYPE=docker
+  # docker buildx create --name mybuilder --driver docker-container --use
+  else 
+  BUILDER=podman
+  BUILDER_TYPE=local
+fi
 
 pushd python >/dev/null
 echo "Building success_200_isvc image"
-docker buildx build -t "${SUCCESS_200_ISVC_IMG_TAG}" -f success_200_isvc.Dockerfile \
-  -o type=docker,dest="${DOCKER_IMAGES_PATH}/${SUCCESS_200_ISVC_IMG}-${GITHUB_SHA}",compression-level=0 .
+$BUILDER buildx build -t "${SUCCESS_200_ISVC_IMG_TAG}" -f success_200_isvc.Dockerfile \
+  -o type=${BUILDER_TYPE} .
 echo "Done building success_200_isvc image"
 echo "Building error_404_isvc image"
-docker buildx build -t "${ERROR_404_ISVC_IMG_TAG}" -f error_404_isvc.Dockerfile \
-  -o type=docker,dest="${DOCKER_IMAGES_PATH}/${ERROR_404_ISVC_IMG}-${GITHUB_SHA}",compression-level=0 .
+$BUILDER buildx build -t "${ERROR_404_ISVC_IMG_TAG}" -f error_404_isvc.Dockerfile \
+  -o type=${BUILDER_TYPE} .
 echo "Done building error_404_isvc image"
+if $RUNNING_LOCAL; then
+  $BUILDER push ${SUCCESS_200_ISVC_IMG_TAG}
+  $BUILDER push ${ERROR_404_ISVC_IMG_TAG}
+fi 
 popd
 echo "Done building images"
 
--- test/scripts/openshift-ci/run-e2e-tests.sh
+++ test/scripts/openshift-ci/run-e2e-tests.sh
@@ -24,23 +24,30 @@ set -o pipefail
 
 MY_PATH=$(dirname "$0")
 PROJECT_ROOT=$MY_PATH/../../../
+export CI_USE_ISVC_HOST="1"
+export GITHUB_SHA=$(git rev-parse HEAD)
+: "${BUILD_GRAPH_IMAGES:=true}"
+: "${RUNNING_LOCAL:=false}"
+if $RUNNING_LOCAL; then
+  export CUSTOM_MODEL_GRPC_IMG_TAG=kserve/custom-model-grpc:latest
+  export IMAGE_TRANSFORMER_IMG_TAG=kserve/image-transformer:latest
+  export GITHUB_SHA=master
+  if [ "$1" = "graph" ] && [ "$BUILD_GRAPH_IMAGES" = "true" ]; then
+    pushd $PROJECT_ROOT >/dev/null
+    ./test/scripts/gh-actions/build-graph-tests-images.sh | tee 2>&1 ./test/scripts/openshift-ci/build-graph-tests-images.log
+    popd
+  fi
+fi
 
 : "${SETUP_E2E:=true}"
-
 if [ "$SETUP_E2E" = "true" ]; then
   echo "Installing on cluster"
   pushd $PROJECT_ROOT >/dev/null
-  ./test/scripts/openshift-ci/setup-e2e-tests.sh "$1"
+  ./test/scripts/openshift-ci/setup-e2e-tests.sh "$1" | tee 2>&1 ./test/scripts/openshift-ci/setup-e2e-tests-$1.log
   popd
 fi
 
 echo "Run E2E tests: $1"
 pushd $PROJECT_ROOT >/dev/null
-# Note: The following images are set by openshift-ci. Uncomment if you are running on your own machine.
-#export CUSTOM_MODEL_GRPC_IMG_TAG=kserve/custom-model-grpc:latest
-#export IMAGE_TRANSFORMER_IMG_TAG=kserve/image-transformer:latest
-
-export GITHUB_SHA=$(git rev-parse HEAD)
-export CI_USE_ISVC_HOST="1"
-./test/scripts/gh-actions/run-e2e-tests.sh "$1"
+./test/scripts/gh-actions/run-e2e-tests.sh "$1" | tee 2>&1 ./test/scripts/openshift-ci/run-e2e-tests-$1.log
 popd
