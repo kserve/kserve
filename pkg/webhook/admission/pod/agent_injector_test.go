41a3e239972cbf08cccce7d57fda7df592c7ea61
cory-johannsen <cjohannsen@cloudera.com>
2025-04-16 13:06:36 -0700

    cleanup: remove unused code and erroneous comments

--- pkg/webhook/admission/pod/agent_injector_test.go
+++ pkg/webhook/admission/pod/agent_injector_test.go
@@ -1652,6 +1652,7 @@ func TestAgentInjector(t *testing.T) {
 	}
 	// Run logger storage scenarios
 	for name, scenario := range scenariosLoggerStorage {
+		t.Logf("logger config: %v", loggerConfig)
 		injector := &AgentInjector{
 			credentialBuilder,
 			agentConfig,
