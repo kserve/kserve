e58f84596e65be31a9b3af2674a7f3fd349ad898
cory-johannsen <cjohannsen@cloudera.com>
2025-04-24 14:51:42 -0700

    cleanup: remove unused code
    Signed-off-by: cory-johannsen <cjohannsen@cloudera.com>

--- pkg/logger/store.go
+++ pkg/logger/store.go
@@ -20,7 +20,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/kserve/kserve/pkg/agent/storage"
-	"github.com/kserve/kserve/pkg/apis/serving/v1beta1"
 	"go.uber.org/zap"
 	"net/url"
 	"path"
@@ -71,45 +70,33 @@ func getMarshaller(format string) (Marshaller, error) {
 
 type Store interface {
 	Store(logUrl *url.URL, logRequest LogRequest) error
-	GetStorageSpec() *v1beta1.StorageSpec
 }
 
 type S3Store struct {
-	storeSpec  *v1beta1.StorageSpec
-	log        *zap.SugaredLogger
-	marshaller Marshaller
-	provider   storage.Provider
+	storePath   string
+	storeFormat string
+	log         *zap.SugaredLogger
+	marshaller  Marshaller
+	provider    storage.Provider
 }
 
 var _ Store = &S3Store{}
 
-func NewS3Store(storeSpec *v1beta1.StorageSpec, marshaller Marshaller, provider storage.Provider, log *zap.SugaredLogger) *S3Store {
+func NewS3Store(logStorePath string, logStoreFormat string, marshaller Marshaller, provider storage.Provider, log *zap.SugaredLogger) *S3Store {
 	return &S3Store{
-		storeSpec:  storeSpec,
-		marshaller: marshaller,
-		log:        log,
-		provider:   provider,
+		storePath:   logStorePath,
+		storeFormat: logStoreFormat,
+		marshaller:  marshaller,
+		log:         log,
+		provider:    provider,
 	}
 }
 
-func NewStoreForScheme(scheme string, storeSpec *v1beta1.StorageSpec, log *zap.SugaredLogger) (Store, error) {
-	if storeSpec == nil {
-		return nil, fmt.Errorf("storage spec is invalid")
+func NewStoreForScheme(scheme string, logStorePath string, logStoreFormat string, log *zap.SugaredLogger) (Store, error) {
+	if logStoreFormat == "" {
+		logStoreFormat = "json"
 	}
-	if storeSpec.StorageKey == nil {
-		return nil, fmt.Errorf("no storage key specified in storage spec")
-	}
-
-	if storeSpec.Parameters == nil {
-		return nil, fmt.Errorf("no parameters specified for store spec")
-	}
-	params := *storeSpec.Parameters
-	format := DefaultStoreFormat
-	if f, ok := params["format"]; ok {
-		format = f
-	}
-
-	marshaller, err := getMarshaller(format)
+	marshaller, err := getMarshaller(logStoreFormat)
 	if err != nil {
 		return nil, err
 	}
@@ -124,7 +111,7 @@ func NewStoreForScheme(scheme string, storeSpec *v1beta1.StorageSpec, log *zap.S
 		return nil, fmt.Errorf("failed to create S3 provider: %w", err)
 	}
 	if protocol == storage.S3 {
-		return NewS3Store(storeSpec, marshaller, provider, log), nil
+		return NewS3Store(logStorePath, logStoreFormat, marshaller, provider, log), nil
 	}
 	return nil, fmt.Errorf("unsupported protocol %s", protocol)
 }
@@ -165,10 +152,6 @@ func (s *S3Store) Store(logUrl *url.URL, logRequest LogRequest) error {
 	return nil
 }
 
-func (s *S3Store) GetStorageSpec() *v1beta1.StorageSpec {
-	return s.storeSpec
-}
-
 func (s *S3Store) getObjectPrefix(configPrefix string, request *LogRequest) (string, error) {
 	if request == nil {
 		return "", fmt.Errorf("log request is invalid")
@@ -188,8 +171,8 @@ func (s *S3Store) getObjectPrefix(configPrefix string, request *LogRequest) (str
 		parts = append(parts, request.Component)
 	}
 
-	if s.storeSpec != nil && s.storeSpec.Path != nil {
-		parts = append(parts, *s.storeSpec.Path)
+	if s.storePath != "" {
+		parts = append(parts, s.storePath)
 	}
 	return path.Join(parts...), nil
 }
@@ -210,10 +193,8 @@ func (s *S3Store) getObjectKey(configPrefix string, request *LogRequest) (string
 	}
 
 	reqType := request.ReqType[typeEnd+1:]
-	params := *s.storeSpec.Parameters
-	format := params["format"]
 
-	return fmt.Sprintf("%s/%s-%s.%s", prefix, request.Id, reqType, format), nil
+	return fmt.Sprintf("%s/%s-%s.%s", prefix, request.Id, reqType, s.storeFormat), nil
 }
 
 func parseS3URL(s3url string) (bucket, key string, err error) {
