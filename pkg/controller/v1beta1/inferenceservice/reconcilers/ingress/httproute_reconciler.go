271ee430b03447a0c7082e000738e122c0228f0a
Hannah DeFazio <h2defazio@gmail.com>
2025-05-12 23:34:36 -0400

    Formatting
    Signed-off-by: Hannah DeFazio <h2defazio@gmail.com>

--- pkg/controller/v1beta1/inferenceservice/reconcilers/ingress/httproute_reconciler.go
+++ pkg/controller/v1beta1/inferenceservice/reconcilers/ingress/httproute_reconciler.go
@@ -581,7 +581,6 @@ func (r *RawHTTPRouteReconciler) reconcilePredictorHTTPRoute(ctx context.Context
 					return err
 				}
 			}
-
 		} else if !apierr.IsNotFound(getExistingErr) {
 			return getExistingErr
 		}
@@ -731,7 +730,6 @@ func (r *RawHTTPRouteReconciler) reconcileTopLevelHTTPRoute(ctx context.Context,
 					return err
 				}
 			}
-
 		} else if !apierr.IsNotFound(getExistingErr) {
 			return getExistingErr
 		}
