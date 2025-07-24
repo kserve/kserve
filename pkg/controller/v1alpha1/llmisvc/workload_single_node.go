d44b9734fbb64e2d00f7eac670407eefe3e3c994
Andres Llausas <allausas@redhat.com>
2025-07-24 16:55:41 -0400

    Adding llmd scheduler and its dependencies.
    Co-authored-by: Bartosz Majsak <bartosz.majsak@gmail.com>
    Co-authored-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    Signed-off-by: Andres Llausas <allausas@redhat.com>

---
+++ pkg/controller/v1alpha1/llmisvc/workload_single_node.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2025 The KServe Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package llmisvc
+
+import (
+	"context"
+	"fmt"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/util/retry"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+func (r *LLMISVCReconciler) propagateDeploymentStatus(ctx context.Context, expected *appsv1.Deployment, ready func(), notReady func(reason, messageFormat string, messageA ...interface{})) error {
+	logger := log.FromContext(ctx)
+
+	curr := &appsv1.Deployment{}
+	err := retry.OnError(retry.DefaultRetry, apierrors.IsNotFound, func() error {
+		return r.Client.Get(ctx, client.ObjectKeyFromObject(expected), curr)
+	})
+	if err != nil {
+		return fmt.Errorf("failed to get current deployment %s/%s: %w", expected.GetNamespace(), expected.GetName(), err)
+	}
+	for _, cond := range curr.Status.Conditions {
+		if cond.Type == appsv1.DeploymentAvailable {
+			if cond.Status == corev1.ConditionTrue {
+				ready()
+			} else {
+				notReady(cond.Reason, cond.Message)
+			}
+			return nil
+		}
+	}
+	logger.Info("Deployment processed")
+	notReady(string(appsv1.DeploymentProgressing), "")
+	return nil
+}
+
+func semanticDeploymentIsEqual(expected *appsv1.Deployment, curr *appsv1.Deployment) bool {
+	return equality.Semantic.DeepDerivative(expected.Spec, curr.Spec) &&
+		equality.Semantic.DeepDerivative(expected.Labels, curr.Labels) &&
+		equality.Semantic.DeepDerivative(expected.Annotations, curr.Annotations)
+}
