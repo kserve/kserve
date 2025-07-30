ab778a4c0bdaed0247d02c7fce76823d16b129aa
Andres Llausas <allausas@redhat.com>
2025-07-30 11:34:16 -0400

    Address scheduler.go comments
    Signed-off-by: Andres Llausas <allausas@redhat.com>

--- pkg/apis/serving/v1alpha1/llm_inference_service_types_func.go
+++ pkg/apis/serving/v1alpha1/llm_inference_service_types_func.go
@@ -21,6 +21,22 @@ import (
 	"knative.dev/pkg/kmeta"
 )
 
+func (r *RouterSpec) HasSchedulerTemplate() bool {
+	return r != nil && r.Scheduler != nil && r.Scheduler.Template != nil
+}
+
+func InferenceModelName(llmSvc *LLMInferenceService) string {
+	return kmeta.ChildName(llmSvc.GetName(), "-inference-model")
+}
+
+func (s *SchedulerSpec) InferencePoolName(llmSvc *LLMInferenceService) string {
+	if s == nil || s.Pool == nil || !s.Pool.HasRef() {
+		// This default MUST match the default value set in the well-known presets.
+		return kmeta.ChildName(llmSvc.GetName(), "-inference-pool")
+	}
+	return s.Pool.Ref.Name
+}
+
 func (r *RouterSpec) EPPServiceName(llmSvc *LLMInferenceService) string {
 	if r == nil || r.Route == nil || r.Scheduler == nil || r.Scheduler.Pool == nil || !r.Scheduler.Pool.HasRef() || r.Scheduler.Pool.Spec == nil || r.Scheduler.Pool.Spec.ExtensionRef == nil {
 		return kmeta.ChildName(llmSvc.GetName(), "-epp-service")
