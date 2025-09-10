798813b337c100ba81320509e0db4e04cd02d5eb
Pierangelo Di Pilato <pierdipi@redhat.com>
2025-09-10 14:03:04 +0200

    Debug level logging for combined spec
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- pkg/controller/v1alpha1/llmisvc/controller.go
+++ pkg/controller/v1alpha1/llmisvc/controller.go
@@ -176,7 +176,7 @@ func (r *LLMISVCReconciler) reconcile(ctx context.Context, llmSvc *v1alpha1.LLMI
 	}
 	llmSvc.MarkPresetsCombinedReady()
 
-	logger.Info("Reconciling with combined base configurations", "combined.spec", baseCfg.Spec, "original.spec", llmSvc.Spec)
+	logger.V(2).Info("Reconciling with combined base configurations", "combined.spec", baseCfg.Spec, "original.spec", llmSvc.Spec)
 	// Replace the spec with the merged configuration for reconciliation
 	// We are only writing to status, so we can safely use the original object.
 	llmSvc.Spec = baseCfg.Spec
