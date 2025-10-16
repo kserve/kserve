76dba8a65506bd58300e6f2faff273a11ecdc653
Brett Thompson <196701379+brettmthompson@users.noreply.github.com>
2025-10-16 06:40:02 -0400

    Add Support for Configuring S3 Storage via Secret Data (#4727)
    Signed-off-by: Brett Thompson <196701379+brettmthompson@users.noreply.github.com>
    Co-authored-by: Dan Sun <dsun20@bloomberg.net>
    Signed-off-by: JunRyeol <wnsfuf0121@naver.com>

--- pkg/credentials/s3/utils_test.go
+++ pkg/credentials/s3/utils_test.go
@@ -25,8 +25,9 @@ import (
 
 func TestBuildS3EnvVars(t *testing.T) {
 	scenarios := map[string]struct {
-		config      S3Config
 		annotations map[string]string
+		secret      *map[string][]byte
+		config      S3Config
 		expected    []corev1.EnvVar
 	}{
 		"S3Endpoint": {
@@ -99,9 +100,249 @@ func TestBuildS3EnvVars(t *testing.T) {
 				},
 			},
 		},
+		"AllSecret": {
+			secret: &map[string][]byte{
+				S3UseHttps:             []byte("0"),
+				S3Endpoint:             []byte("s3.aws.com"),
+				S3VerifySSL:            []byte("0"),
+				AWSAnonymousCredential: []byte("true"),
+				AWSRegion:              []byte("us-east-2"),
+				S3UseVirtualBucket:     []byte("true"),
+				S3UseAccelerate:        []byte("true"),
+				AWSCABundle:            []byte("value"),
+				AWSCABundleConfigMap:   []byte("value"),
+			},
+			expected: []corev1.EnvVar{
+				{
+					Name:  S3UseHttps,
+					Value: "0",
+				},
+				{
+					Name:  S3Endpoint,
+					Value: "s3.aws.com",
+				},
+				{
+					Name:  AWSEndpointUrl,
+					Value: "http://s3.aws.com",
+				},
+				{
+					Name:  S3VerifySSL,
+					Value: "0",
+				},
+				{
+					Name:  AWSAnonymousCredential,
+					Value: "true",
+				},
+				{
+					Name:  AWSRegion,
+					Value: "us-east-2",
+				},
+				{
+					Name:  S3UseVirtualBucket,
+					Value: "true",
+				},
+				{
+					Name:  S3UseAccelerate,
+					Value: "true",
+				},
+				{
+					Name:  AWSCABundle,
+					Value: "value",
+				},
+				{
+					Name:  AWSCABundleConfigMap,
+					Value: "value",
+				},
+			},
+		},
+		"AllS3Config": {
+			config: S3Config{
+				S3Endpoint:               "s3.aws.com",
+				S3UseHttps:               "0",
+				S3Region:                 "us-east-2",
+				S3VerifySSL:              "0",
+				S3UseVirtualBucket:       "true",
+				S3UseAccelerate:          "true",
+				S3UseAnonymousCredential: "true",
+				S3CABundleConfigMap:      "value",
+				S3CABundle:               "value",
+			},
+			expected: []corev1.EnvVar{
+				{
+					Name:  S3UseHttps,
+					Value: "0",
+				},
+				{
+					Name:  S3Endpoint,
+					Value: "s3.aws.com",
+				},
+				{
+					Name:  AWSEndpointUrl,
+					Value: "http://s3.aws.com",
+				},
+				{
+					Name:  S3VerifySSL,
+					Value: "0",
+				},
+				{
+					Name:  AWSAnonymousCredential,
+					Value: "true",
+				},
+				{
+					Name:  AWSRegion,
+					Value: "us-east-2",
+				},
+				{
+					Name:  S3UseVirtualBucket,
+					Value: "true",
+				},
+				{
+					Name:  S3UseAccelerate,
+					Value: "true",
+				},
+				{
+					Name:  AWSCABundle,
+					Value: "value",
+				},
+				{
+					Name:  AWSCABundleConfigMap,
+					Value: "value",
+				},
+			},
+		},
+		"AnnotationsSecretAndS3Config": {
+			annotations: map[string]string{
+				InferenceServiceS3SecretEndpointAnnotation:    "s3.aws.com-annotation",
+				InferenceServiceS3SecretRegionAnnotation:      "us-east-2-annotation",
+				InferenceServiceS3SecretSSLAnnotation:         "0-annotation",
+				InferenceServiceS3UseVirtualBucketAnnotation:  "true-annotation",
+				InferenceServiceS3UseAccelerateAnnotation:     "true-annotation",
+				InferenceServiceS3UseAnonymousCredential:      "true-annotation",
+				InferenceServiceS3CABundleAnnotation:          "value-annotation",
+				InferenceServiceS3CABundleConfigMapAnnotation: "value-annotation",
+			},
+			secret: &map[string][]byte{
+				S3Endpoint:             []byte("s3.aws.com-secret"),
+				S3VerifySSL:            []byte("0-secret"),
+				AWSAnonymousCredential: []byte("true-secret"),
+				AWSRegion:              []byte("us-east-2-secret"),
+				S3UseVirtualBucket:     []byte("true-secret"),
+				S3UseAccelerate:        []byte("true-secret"),
+				AWSCABundle:            []byte("value-secret"),
+				AWSCABundleConfigMap:   []byte("value-secret"),
+			},
+			config: S3Config{
+				S3Endpoint:               "s3.aws.com-config",
+				S3Region:                 "us-east-2-config",
+				S3VerifySSL:              "0-config",
+				S3UseVirtualBucket:       "true-config",
+				S3UseAccelerate:          "true-config",
+				S3UseAnonymousCredential: "true-config",
+				S3CABundleConfigMap:      "value-config",
+				S3CABundle:               "value-config",
+			},
+			expected: []corev1.EnvVar{
+				{
+					Name:  S3Endpoint,
+					Value: "s3.aws.com-annotation",
+				},
+				{
+					Name:  AWSEndpointUrl,
+					Value: "https://s3.aws.com-annotation",
+				},
+				{
+					Name:  S3VerifySSL,
+					Value: "0-annotation",
+				},
+				{
+					Name:  AWSAnonymousCredential,
+					Value: "true-annotation",
+				},
+				{
+					Name:  AWSRegion,
+					Value: "us-east-2-annotation",
+				},
+				{
+					Name:  S3UseVirtualBucket,
+					Value: "true-annotation",
+				},
+				{
+					Name:  S3UseAccelerate,
+					Value: "true-annotation",
+				},
+				{
+					Name:  AWSCABundle,
+					Value: "value-annotation",
+				},
+				{
+					Name:  AWSCABundleConfigMap,
+					Value: "value-annotation",
+				},
+			},
+		},
+		"SecretAndS3Config": {
+			secret: &map[string][]byte{
+				S3Endpoint:             []byte("s3.aws.com-secret"),
+				S3VerifySSL:            []byte("0-secret"),
+				AWSAnonymousCredential: []byte("true-secret"),
+				AWSRegion:              []byte("us-east-2-secret"),
+				S3UseVirtualBucket:     []byte("true-secret"),
+				S3UseAccelerate:        []byte("true-secret"),
+				AWSCABundle:            []byte("value-secret"),
+				AWSCABundleConfigMap:   []byte("value-secret"),
+			},
+			config: S3Config{
+				S3Endpoint:               "s3.aws.com-config",
+				S3Region:                 "us-east-2-config",
+				S3VerifySSL:              "0-config",
+				S3UseVirtualBucket:       "true-config",
+				S3UseAccelerate:          "true-config",
+				S3UseAnonymousCredential: "true-config",
+				S3CABundleConfigMap:      "value-config",
+				S3CABundle:               "value-config",
+			},
+			expected: []corev1.EnvVar{
+				{
+					Name:  S3Endpoint,
+					Value: "s3.aws.com-secret",
+				},
+				{
+					Name:  AWSEndpointUrl,
+					Value: "https://s3.aws.com-secret",
+				},
+				{
+					Name:  S3VerifySSL,
+					Value: "0-secret",
+				},
+				{
+					Name:  AWSAnonymousCredential,
+					Value: "true-secret",
+				},
+				{
+					Name:  AWSRegion,
+					Value: "us-east-2-secret",
+				},
+				{
+					Name:  S3UseVirtualBucket,
+					Value: "true-secret",
+				},
+				{
+					Name:  S3UseAccelerate,
+					Value: "true-secret",
+				},
+				{
+					Name:  AWSCABundle,
+					Value: "value-secret",
+				},
+				{
+					Name:  AWSCABundleConfigMap,
+					Value: "value-secret",
+				},
+			},
+		},
 	}
 	for name, scenario := range scenarios {
-		envs := BuildS3EnvVars(scenario.annotations, &scenario.config)
+		envs := BuildS3EnvVars(scenario.annotations, scenario.secret, &scenario.config)
 
 		if diff := cmp.Diff(scenario.expected, envs); diff != "" {
 			t.Errorf("Test %q unexpected result (-want +got): %v", name, diff)
