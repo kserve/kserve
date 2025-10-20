8fc303d4d27a0905eb506aa3e283c64604853e27
Jooho Lee <jlee@redhat.com>
2025-10-20 09:23:11 -0400

    refactor: separate llmisvc manifests from default kustomization
    - Move llmisvc CRDs and webhooks to dedicated config/llmisvc/ directory
    - Create separate kustomizations for full/minimal CRD variants with llmisvc support
    - Update certmanager and webhook configurations for independent llmisvc deployment
    - Modify Makefile to support both default and llmisvc build targets
    
    Signed-off-by: Jooho Lee <jlee@redhat.com>

--- hack/minimal-crdgen.sh
+++ hack/minimal-crdgen.sh
@@ -10,3 +10,11 @@ find config/crd/full -name 'serving.kserve.io*.yaml' | while read -r file; do
   cp "$file" "$minimal"
   go run ./cmd/crd-gen removecrdvalidation "$minimal"
 done
+
+find config/crd/full/llmisvc -name 'serving.kserve.io*.yaml' | while read -r file; do
+  # create minimal
+  minimal="config/crd/minimal/llmisvc/$(basename "$file")"
+  echo "Creating minimal CRD file: ${minimal}"
+  cp "$file" "$minimal"
+  go run ./cmd/crd-gen removecrdvalidation "$minimal"
+done
