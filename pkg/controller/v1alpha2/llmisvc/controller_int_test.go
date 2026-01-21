b4434c26a9dfd6dda58d97bdd8b8010ce888dcaa
Killian Golds <kgolds@redhat.com>
2026-01-21 18:35:19 +0000

    test(llmisvc): mark v1alpha2 InferencePools ready in integration tests
    Update ensureRouterManagedResourcesAreReady() to also mark v1alpha2
    InferencePools as ready, supporting dual-pool migration testing.
    
    Signed-off-by: Killian Golds <kgolds@redhat.com>

--- pkg/controller/v1alpha2/llmisvc/controller_int_test.go
+++ pkg/controller/v1alpha2/llmisvc/controller_int_test.go
@@ -28,6 +28,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	igwapi "sigs.k8s.io/gateway-api-inference-extension/api/v1"
+	igwapiv1alpha2 "sigs.k8s.io/gateway-api-inference-extension/apix/v1alpha2"
 	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
 
 	"github.com/kserve/kserve/pkg/constants"
@@ -1494,10 +1495,23 @@ func ensureRouterManagedResourcesAreReady(ctx context.Context, c client.Client, 
 		if err != nil && !errors.IsNotFound(err) {
 			g.Expect(err).NotTo(gomega.HaveOccurred())
 		}
-		logf.FromContext(ctx).Info("Marking InferencePool resources ready", "inferencepools", infPools)
+		logf.FromContext(ctx).Info("Marking v1 InferencePool resources ready", "inferencepools", infPools)
 		for _, pool := range infPools.Items {
 			updatedPool := pool.DeepCopy()
 			WithInferencePoolReadyStatus()(updatedPool)
+			g.Expect(c.Status().Update(ctx, updatedPool)).To(gomega.Succeed())
+		}
+
+		// Also mark v1alpha2 InferencePools as ready (for dual-pool migration)
+		infPoolsV1Alpha2 := &igwapiv1alpha2.InferencePoolList{}
+		err = c.List(ctx, infPoolsV1Alpha2, infPoolsListOpts)
+		if err != nil && !errors.IsNotFound(err) {
+			g.Expect(err).NotTo(gomega.HaveOccurred())
+		}
+		logf.FromContext(ctx).Info("Marking v1alpha2 InferencePool resources ready", "inferencepools", infPoolsV1Alpha2)
+		for _, pool := range infPoolsV1Alpha2.Items {
+			updatedPool := pool.DeepCopy()
+			WithInferencePoolV1Alpha2ReadyStatus()(updatedPool)
 			g.Expect(c.Status().Update(ctx, updatedPool)).To(gomega.Succeed())
 		}
 
