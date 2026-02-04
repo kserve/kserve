dd4bca8dc7e1e0d761cb122836d52de9eefb3a32
Dhaval Khatri <khatri.dhaval20@gmail.com>
2026-02-04 13:48:56 -0500

    update: get svcname from spec
    Signed-off-by: Dhaval Khatri <khatri.dhaval20@gmail.com>

--- pkg/controller/v1beta1/inferenceservice/components/predictor.go
+++ pkg/controller/v1beta1/inferenceservice/components/predictor.go
@@ -153,8 +153,10 @@ func (p *Predictor) Reconcile(ctx context.Context, isvc *v1beta1.InferenceServic
 	}
 
 	// Add InferenceService name as environment variable to predictor container
-	if err := isvcutils.AddEnvVarToPodSpec(&podSpec, constants.InferenceServiceContainerName, constants.InferenceServiceNameEnvVarKey, isvc.Name); err != nil {
-		return ctrl.Result{}, errors.Wrapf(err, "failed to add INFERENCE_SERVICE_NAME environment variable to container %s", constants.InferenceServiceContainerName)
+	// Use the actual container name from the podSpec (first container)
+	predictorContainerName := podSpec.Containers[0].Name
+	if err := isvcutils.AddEnvVarToPodSpec(&podSpec, predictorContainerName, constants.InferenceServiceNameEnvVarKey, isvc.Name); err != nil {
+		return ctrl.Result{}, errors.Wrapf(err, "failed to add INFERENCE_SERVICE_NAME environment variable to container %s", predictorContainerName)
 	}
 
 	predictorName := constants.PredictorServiceName(isvc.Name)
