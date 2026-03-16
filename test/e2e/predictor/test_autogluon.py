1db26deb2ed45d1dd34f0be08f4283a29f692731
ZabinskiMichal <mzabinsk@redhat.com>
2026-03-16 15:45:47 +0100

    added proper s3 location for model artifact
    Signed-off-by: ZabinskiMichal <mzabinsk@redhat.com>

--- test/e2e/predictor/test_autogluon.py
+++ test/e2e/predictor/test_autogluon.py
@@ -16,9 +16,9 @@ from kserve import (
 from ..common.utils import KSERVE_TEST_NAMESPACE, predict_isvc
 
 AUTOGLOUON_STORAGE_URI = (
-    "s3://kserve-model-artifacts/predictor"
-)
+    "s3://kserve-autogluon-tests/predictor"
 
+)
 
 @pytest.mark.predictor
 @pytest.mark.asyncio(scope="session")
