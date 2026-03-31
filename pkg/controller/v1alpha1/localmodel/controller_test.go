ca3e3e6c771796fdc08d0575b7a7beb14b5cf5e6
Bartosz Majsak <bartosz.majsak@gmail.com>
2026-03-31 22:22:19 +0200

    fix(test): restore missing imports dropped during rebase conflict resolution
    The rebase conflict in controller_test.go was resolved in the function
    body but the import block was auto-merged incorrectly, dropping the
    k8s client-go and controller-runtime imports required by initializeManager.
    
    Signed-off-by: Bartosz Majsak <bartosz.majsak@gmail.com>

--- pkg/controller/v1alpha1/localmodel/controller_test.go
+++ pkg/controller/v1alpha1/localmodel/controller_test.go
@@ -28,7 +28,12 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/utils/ptr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	crconfig "sigs.k8s.io/controller-runtime/pkg/config"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	"github.com/kserve/kserve/pkg/apis/serving/v1alpha1"
 	"github.com/kserve/kserve/pkg/apis/serving/v1beta1"
