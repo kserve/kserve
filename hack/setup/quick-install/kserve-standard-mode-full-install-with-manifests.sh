3aedfcdd551f07d08e9a854bfe07f14dcde2d44e
sophieliu15 <sophieliu15@users.noreply.github.com>
2026-04-01 08:53:39 -0400

    chore: run make precommit to regenerate auto-generated files
    Signed-off-by: sophieliu15 <sophieliu15@users.noreply.github.com>

--- hack/setup/quick-install/kserve-standard-mode-full-install-with-manifests.sh
+++ hack/setup/quick-install/kserve-standard-mode-full-install-with-manifests.sh
@@ -37871,7 +37871,8 @@ data:
            "domainTemplate": "{{ .Name }}-{{ .Namespace }}.{{ .IngressDomain }}",
            "urlScheme": "http",
            "disableIstioVirtualHost": false,
-           "disableIngressCreation": false
+           "disableIngressCreation": false,
+           "disableHTTPRouteTimeout": false
        }
      ingress: |-
        {
@@ -37946,6 +37947,10 @@ data:
 
            # disableIngressCreation controls whether to disable ingress creation for raw deployment mode.
            "disableIngressCreation": false,
+
+           # disableHTTPRouteTimeout controls whether to omit the timeout field from HTTPRoute rules.
+           # Set to true for Gateway controllers (e.g. GKE Gateway) that do not support the optional timeouts field.
+           "disableHTTPRouteTimeout": false,
 
            # pathTemplate specifies the template for generating path based url for each inference service.
            # The following variables can be used in the template for generating url.
@@ -38266,7 +38271,8 @@ data:
         "domainTemplate": "{{ .Name }}-{{ .Namespace }}.{{ .IngressDomain }}",
         "urlScheme": "http",
         "disableIstioVirtualHost": false,
-        "disableIngressCreation": false
+        "disableIngressCreation": false,
+        "disableHTTPRouteTimeout": false
     }
   localModel: |-
     {
