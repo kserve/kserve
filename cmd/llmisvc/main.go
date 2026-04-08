f244077cef8a100acd68f696917163c4a8743c92
Bartosz Majsak <bartosz.majsak@gmail.com>
2026-04-08 09:34:13 +0200

    feat(llmisvc): add platform hooks for networking and manager cfg (#5240)
    Signed-off-by: Bartosz Majsak <bartosz.majsak@gmail.com>

--- cmd/llmisvc/main.go
+++ cmd/llmisvc/main.go
@@ -167,7 +167,7 @@ func main() {
 
 	llmSvcCacheSelector, _ := metav1.LabelSelectorAsSelector(&llmisvc.ChildResourcesLabelSelector)
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	mgrOpts := ctrl.Options{
 		Scheme:                 scheme,
 		Metrics:                metricsServerOptions,
 		WebhookServer:          webhook.NewServer(webhook.Options{Port: options.webhookPort, TLSOpts: tlsOpts}),
@@ -193,7 +193,13 @@ func main() {
 				},
 			},
 		},
-	})
+	}
+
+	if err := customizeManagerOptions(&mgrOpts); err != nil {
+		setupLog.Error(err, "failed to apply distribution-specific manager options")
+		os.Exit(1)
+	}
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), mgrOpts)
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
