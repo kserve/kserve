e5e63ef6c0e1a0e3fa9d68198298087fff2781e4
Spolti <fspolti@redhat.com>
2026-04-13 22:01:10 -0300

    fix(llmisvc): shorten webhook name to comply with 63-char label limit
    The LLMInferenceServiceConfig validating webhook name
    `llminferenceserviceconfig.kserve-webhook-server.v1alpha{1,2}.validator`
    was 66 characters, exceeding the Kubernetes 63-character label value
    limit. This caused OLM CSV installation to fail as it uses the webhook
    name as a generateName label value.
    
    Shortened to `llmisvcconfig.kserve-webhook-server.v1alpha{1,2}.validator`
    (54 chars).
    
    Signed-off-by: Spolti <fspolti@redhat.com>

--- pkg/apis/serving/v1alpha2/llm_inference_service_config_validation.go
+++ pkg/apis/serving/v1alpha2/llm_inference_service_config_validation.go
@@ -32,7 +32,7 @@ import (
 	"github.com/kserve/kserve/pkg/utils"
 )
 
-// +kubebuilder:webhook:path=/validate-serving-kserve-io-v1alpha2-llminferenceserviceconfig,mutating=false,failurePolicy=fail,sideEffects=None,groups=serving.kserve.io,resources=llminferenceserviceconfigs,verbs=create;update,versions=v1alpha2,name=llminferenceserviceconfig.kserve-webhook-server.v1alpha2.validator,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-serving-kserve-io-v1alpha2-llminferenceserviceconfig,mutating=false,failurePolicy=fail,sideEffects=None,groups=serving.kserve.io,resources=llminferenceserviceconfigs,verbs=create;update,versions=v1alpha2,name=llmisvcconfig.kserve-webhook-server.v1alpha2.validator,admissionReviewVersions=v1
 
 // LLMInferenceServiceConfigValidator is responsible for validating the LLMInferenceServiceConfig resource
 // when it is created, updated, or deleted.
