a9ef25085c17e19cbcc824bda96476038655bc7b
hfjxjjd123 <licbusiness21@gmail.com>
2026-04-26 13:27:55 +0900

    fix: rbac changes for list, watch
    Signed-off-by: hfjxjjd123 <licbusiness21@gmail.com>

--- pkg/controller/v1beta1/inferenceservice/controller.go
+++ pkg/controller/v1beta1/inferenceservice/controller.go
@@ -84,7 +84,7 @@ import (
 // +kubebuilder:rbac:groups=autoscaling,resources=horizontalpodautoscalers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=serviceaccounts,verbs=get
-// +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;create
+// +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update
 // +kubebuilder:rbac:groups=core,resources=secrets,verbs=get
 // +kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch
 // +kubebuilder:rbac:groups=core,resources=events,verbs=get;list;watch;create;update;patch;delete
