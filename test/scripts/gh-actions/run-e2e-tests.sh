b50e28e90fd1bec83aa5ea90316c4aa5bb387aa2
Filippe Spolti <fspolti@redhat.com>
2026-05-14 16:16:29 -0300

    ci: add network-layer parameter support to e2e tests infrastructure (#5506)
    Signed-off-by: Spolti <fspolti@redhat.com>

--- test/scripts/gh-actions/run-e2e-tests.sh
+++ test/scripts/gh-actions/run-e2e-tests.sh
@@ -31,7 +31,10 @@ export GITHUB_SHA="${TAG:-latest}"
 echo "Starting E2E functional tests ..."
 MARKER="${1}"
 PARALLELISM="${2:-1}"
-NETWORK_LAYER="${3:-'istio'}"
+NETWORK_LAYER="${3:-istio}"
+
+: "${SKIP_DELETION_ON_FAILURE:=true}"
+export SKIP_DELETION_ON_FAILURE
 
 echo "Parallelism requested for pytest is ${PARALLELISM}"
 
