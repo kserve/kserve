2368fdbca9c00da80c9daa65ffc599a7a1d8198c
Tessa Pham <tepham@redhat.com>
2026-06-11 18:00:18 -0400

    fix formatting
    Signed-off-by: Tessa Pham <tepham@redhat.com>

--- test/e2e/llmisvc/test_llm_inference_service.py
+++ test/e2e/llmisvc/test_llm_inference_service.py
@@ -242,7 +242,6 @@ def chat_completions_payload(test_case: TestCase) -> Dict[str, Any]:
     }
 
 
-
 @pytest.mark.llminferenceservice
 @pytest.mark.asyncio(loop_scope="session")
 @pytest.mark.parametrize(
