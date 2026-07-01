4be3f0655ea20b813f0779a671b4340b7aa22571
Tessa Pham <tepham@redhat.com>
2026-07-01 15:38:07 -0400

    add GIE to CRD dirs list
    Signed-off-by: Tessa Pham <tepham@redhat.com>

--- hack/setup/scripts/install-script-generator/pkg/manifest_builder.py
+++ hack/setup/scripts/install-script-generator/pkg/manifest_builder.py
@@ -182,6 +182,7 @@ def select_kserve_directories(
         crd_dirs = [
             repo_root / "config/crd/full/clusterstoragecontainer",
             repo_root / "config/crd/full/llmisvc",
+            repo_root / "config/crd/full/gie",
         ]
         config_dirs = [repo_root / "config/overlays/standalone/llmisvc"]
     elif localmodel == "true":
