90265cf383e797c0c2c33e5958f18349c64b169b
Harsh Gupta <harshgupta93198@gmail.com>
2026-07-28 15:12:26 +0530

    test: drop unused mock import in test_cpu_count
    Signed-off-by: Harsh Gupta <harshgupta93198@gmail.com>

--- python/kserve/test/test_cpu_count.py
+++ python/kserve/test/test_cpu_count.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from unittest import mock
-
 import psutil
 
 from kserve.utils.utils import cpu_count
