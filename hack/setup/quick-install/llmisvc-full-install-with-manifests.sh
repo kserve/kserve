0d9d3b0c157fae8a661663ea21b08c36263b2593
xrwang8 <68765051+xrwang8@users.noreply.github.com>
2026-08-15 01:32:46 +0800

    feat: add ModelScope storage download support (ms:// URI scheme) (#5330)
    Signed-off-by: xrwang8 <xrwang8@gmail.com>

--- hack/setup/quick-install/llmisvc-full-install-with-manifests.sh
+++ hack/setup/quick-install/llmisvc-full-install-with-manifests.sh
@@ -109293,6 +109293,7 @@ spec:
   - prefix: s3://
   - prefix: hdfs://
   - prefix: hf://
+  - prefix: modelscope://
   - prefix: webhdfs://
   - regex: https://(.+?).blob.core.windows.net/(.+)
   - regex: https://(.+?).file.core.windows.net/(.+)
